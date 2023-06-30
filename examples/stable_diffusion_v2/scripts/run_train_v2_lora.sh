2fc8410816b04fe0e2b2fa34372ec662adeea5fd
Samit <285365963@qq.com>
2023-07-01 00:25:33 +0800

    Improve LoRA finetune, update trainer & saver, and fix bugs (#26)
    * Support save/load LoRA param only. Support LoRA distributed finetune. Add grad clip and ema (trainer & saver updated). Impr pokemon results. Fix bugs in ms1.9
    
    * fix bug and impr training recipe
    
    * add lora finetune tutorial

--- examples/stable_diffusion_v2/scripts/run_train_v2_lora.sh
+++ examples/stable_diffusion_v2/scripts/run_train_v2_lora.sh
@@ -3,26 +3,30 @@ export HCCL_CONNECT_TIMEOUT=600
 export ASCEND_GLOBAL_LOG_LEVEL=3
 export ASCEND_SLOG_PRINT_TO_STDOUT=0
 
-export SD_VERSION="2.0" # TODO: parse by args. or fix to 2.0 later
+device_id=7
 
-device_id=4
-
+# modify to your local data path
+data_path=./datasets/pokemon_blip/train
+#data_path=/home/yx/datasets/diffusion/pokemon
 output_path=output/lora_pokemon
+
 task_name=txt2img
-data_path=/home/yx/datasets/diffusion/pokemon
 pretrained_model_path=models/
 pretrained_model_file=stablediffusionv2_512.ckpt
 train_config_file=configs/train_config_v2.json
+image_filter_size=200
 image_size=512
-train_batch_size=4      # bs=1, grad_accu=4, in diffuser
-start_learning_rate=1e-4 #lr=1e-4, lr_min =0. in diffuser
+train_batch_size=4
+lora_rank=128
+start_learning_rate=1e-4
 end_learning_rate=0
-warmup_steps=516 # ~3 epoch. diffuser 0
-epochs=18 #15000 steps=>18 epochs in diffuser
-save_checkpoint_steps=516  # save every two epochs
-
-
-max_grad_norm=1. # TODO: NOT making effect. 1.0 in duffuser. But we don't support gard_norm or grad_accumulate currently.
+warmup_steps=0
+epochs=72
+use_ema=True
+clip_grad=True
+lora_fp16=True
+max_grad_norm=1.
+ckpt_save_interval=5
 
 # uncomment the following two lines to finetune on 768x768 resolution.
 #image_size=768 # v2-base 512, v2.1 768
@@ -30,10 +34,12 @@ max_grad_norm=1. # TODO: NOT making effect. 1.0 in duffuser. But we don't suppor
 
 rm -rf ${output_path:?}/${task_name:?}
 mkdir -p ${output_path:?}/${task_name:?}
-export RANK_SIZE=1;export DEVICE_ID=$device_id;export MS_COMPILER_CACHE_PATH=${output_path:?}/${task_name:?}; \
+export RANK_SIZE=1;export DEVICE_ID=$device_id;
+#export MS_COMPILER_CACHE_PATH=${output_path:?}/${task_name:?}; 
 #nohup python -u run_train.py \
 python train_text_to_image.py \
     --data_path=$data_path \
+    --image_filter_size=$image_filter_size \
     --train_config=$train_config_file \
     --output_path=$output_path/$task_name \
     --use_parallel=False \
@@ -43,8 +49,13 @@ python train_text_to_image.py \
     --train_batch_size=$train_batch_size \
     --epochs=$epochs \
     --use_lora=True \
+    --lora_rank=$lora_rank \
+    --lora_fp16=$lora_fp16 \
     --start_learning_rate=$start_learning_rate \
     --end_learning_rate=$end_learning_rate \
     --warmup_steps=$warmup_steps \
-    --save_checkpoint_steps=$save_checkpoint_steps \
+    --ckpt_save_interval=$ckpt_save_interval\
+    --use_ema=$use_ema \
+    --clip_grad=$clip_grad \
+    --max_grad_norm=$max_grad_norm \
 #    > $output_path/$task_name/log_train_v2 2>&1 &
