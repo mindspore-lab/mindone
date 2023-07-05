66c88fb7ad5ddf1ac16b42c8d4264708b857157a
heepengpeng <122354463+heepengpeng@users.noreply.github.com>
2023-07-05 11:59:32 +0800

    [feat] mv lora_rank into ckpt & add fiter to load_param_into_net (#30)
    * [feat] mv lora_rank into ckpt & add fiter to load_param_into_net
    
    * [feat] mr from main
    
    ---------
    
    Co-authored-by: Samit <285365963@qq.com>

--- examples/stable_diffusion_v2/ldm/modules/train/checkpoint.py
+++ examples/stable_diffusion_v2/ldm/modules/train/checkpoint.py
@@ -49,25 +49,30 @@ class CheckpointManager:
         except ValueError:
             print(f"ValueError, failed to remove the older ckpt file {file_name}.")
 
-    def save_top_k(self, network, perf, ckpt_name, verbose=True):
+    def save_top_k(self, network, perf, ckpt_name, verbose=True, lora_rank=None):
         """Save and return Top K checkpoint address and accuracy."""
         self.ckpt_queue.append((perf, ckpt_name))
         self.ckpt_queue = sorted(
             self.ckpt_queue, key=lambda x: x[0], reverse=not self.prefer_low_perf
         )  # by default, reverse is True for descending order
+        append_dict = {"lora_rank": lora_rank} if lora_rank is not None else None
         if len(self.ckpt_queue) > self.k:
             to_del = self.ckpt_queue.pop(-1)
             # save if the perf is better than the minimum in the heap
             if to_del[1] != ckpt_name:
-                ms.save_checkpoint(network, os.path.join(self.ckpt_save_dir, ckpt_name), integrated_save=False)
+                ms.save_checkpoint(network, os.path.join(self.ckpt_save_dir, ckpt_name), integrated_save=False,
+                                   append_dict=append_dict)
                 # del minimum
                 self.remove_ckpt_file(os.path.join(self.ckpt_save_dir, to_del[1]))
         else:
-            ms.save_checkpoint(network, os.path.join(self.ckpt_save_dir, ckpt_name))
+            ms.save_checkpoint(network, os.path.join(self.ckpt_save_dir, ckpt_name),
+                               append_dict=append_dict)
 
-    def save_latest_k(self, network, ckpt_name):
+    def save_latest_k(self, network, ckpt_name, lora_rank):
+        append_dict = {"lora_rank": lora_rank} if lora_rank is not None else None
         """Save latest K checkpoint."""
-        ms.save_checkpoint(network, os.path.join(self.ckpt_save_dir, ckpt_name), integrated_save=False)
+        ms.save_checkpoint(network, os.path.join(self.ckpt_save_dir, ckpt_name), integrated_save=False,
+                           append_dict=append_dict)
 
         print('Checkpoint saved in ', os.path.join(self.ckpt_save_dir, ckpt_name))
         self.ckpt_queue.append(ckpt_name)
@@ -76,19 +81,21 @@ class CheckpointManager:
             if self.del_past:
                 self.remove_ckpt_file(os.path.join(self.ckpt_save_dir, to_del))
 
-    def save(self, network, perf=None, ckpt_name=None):
+    def save(self, network, perf=None, ckpt_name=None, lora_rank=None):
         """Save checkpoint according to different save strategy."""
+        append_dict = {"lora_rank": lora_rank} if lora_rank is not None else None
         if self.ckpt_save_policy is None:
-            ms.save_checkpoint(network, os.path.join(self.ckpt_save_dir, ckpt_name), integrated_save=False)
+            ms.save_checkpoint(network, os.path.join(self.ckpt_save_dir, ckpt_name), integrated_save=False,
+                               append_dict=append_dict)
         elif self.ckpt_save_policy == "top_k":
             if perf is None:
                 raise ValueError(
                     "Evaluation performance is None, but `top_k` ckpt save policy requires evaluation performance"
                 )
-            self.save_top_k(network, perf, ckpt_name)
+            self.save_top_k(network, perf, ckpt_name, lora_rank)
             return self.ckpt_queue
         elif self.ckpt_save_policy == "latest_k":
-            self.save_latest_k(network, ckpt_name)
+            self.save_latest_k(network, ckpt_name, lora_rank)
             return self.ckpt_queue
         else:
             raise ValueError(
