6af2295f09454e711117314fc63a41c2058a8952
congw729 <wangcong.970723@gmail.com>
2023-08-17 17:35:37 +0800

    Add convert scripts for weight of controlnet (except cond_stage model).

--- examples/stable_diffusion_v2/tools/model_conversion/convert_weights.py
+++ examples/stable_diffusion_v2/tools/model_conversion/convert_weights.py
@@ -10,7 +10,7 @@ MINDSPORE = "ms"
 PYTORCH = "pt"
 STABLE_DIFFUSION_V1 = "sdv1"
 STABLE_DIFFUSION_V2 = "sdv2"
-CONTROLNET_V2 = "controlnet"
+CONTROLNET_V1 = "controlnet"
 DIFFUSERS_V1 = "diffusersv1"
 DIFFUSERS_V2 = "diffusersv2"
 
@@ -40,7 +40,7 @@ parser.add_argument(
     "--model",
     "-m",
     type=str,
-    choices=[STABLE_DIFFUSION_V1, STABLE_DIFFUSION_V2, CONTROLNET_V2, DIFFUSERS_V2],
+    choices=[STABLE_DIFFUSION_V1, STABLE_DIFFUSION_V2, CONTROLNET_V1, DIFFUSERS_V2],
     help="version of stable diffusion",
     default=STABLE_DIFFUSION_V2,
 )
@@ -77,6 +77,45 @@ def MINDSPORE_PYTORCH_STABLE_DIFFUSION_V2():
         _source_data = source_data[_name_ms].asnumpy()
         target_data[_name_pt] = torch.tensor(_source_data)
     torch.save(target_data, args.target)
+
+
+def PYTORCH_MINDSPORE_CONTROLNET_V1():
+    with open("tools/model_conversion/ms_exceptcond_v1.txt") as file_ms:
+        lines_ms = file_ms.readlines()
+    with open("tools/model_conversion/pt_exceptcond_v1.txt") as file_pt:
+        lines_pt = file_pt.readlines()
+
+    # no need for ["state_dict"] due to using model.state_dict() in export script
+    source_data = torch.load(args.source, map_location="cpu")
+    target_data = []
+    i = j = 0
+    while i < len(lines_ms):
+        line_ms = lines_ms[i]
+        _name_ms, _, _ = line_ms.strip().split("#")
+        if "attn.attn.in_proj" not in line_ms:
+            line_pt = lines_pt[j]
+            _name_pt, _, _ = line_pt.strip().split("#")
+            target_data.append({"name": _name_ms, "data": ms.Tensor(source_data[_name_pt].cpu().detach().numpy())})
+            i += 1
+            j += 1
+        else:
+            w, b = [], []
+            for k in range(6):
+                line_pt = lines_pt[j]
+                _name_pt, _, _ = line_pt.strip().split("#")
+                j += 1
+                if "weight" in _name_pt:
+                    w.append(source_data[_name_pt].cpu().detach().numpy())
+                else:
+                    b.append(source_data[_name_pt].cpu().detach().numpy())
+            target_data.append({"name": _name_ms, "data": ms.Tensor(np.concatenate([b[1], b[0], b[2]]))})
+            i += 1
+            line_ms = lines_ms[i]
+            _name_ms, _, _ = line_ms.strip().split("#")
+            target_data.append({"name": _name_ms, "data": ms.Tensor(np.concatenate([w[1], w[0], w[2]]))})
+            i += 1
+
+    ms.save_checkpoint(target_data, args.target)
 
 
 def _load_v1_and_split_qkv(source_data, lines_ms, lines_pt):
@@ -204,6 +243,7 @@ SUPPORTED_CONVERSIONS = {
     (MINDSPORE, PYTORCH, STABLE_DIFFUSION_V2, MINDSPORE_PYTORCH_STABLE_DIFFUSION_V2),
     (MINDSPORE, PYTORCH, DIFFUSERS_V2, MINDSPORE_PYTORCH_DIFFUSERS_V2),
     (MINDSPORE, PYTORCH, DIFFUSERS_V1, MINDSPORE_PYTORCH_DIFFUSERS_V1),
+    (PYTORCH, MINDSPORE, CONTROLNET_V1, PYTORCH_MINDSPORE_CONTROLNET_V1),
 }
 
 
