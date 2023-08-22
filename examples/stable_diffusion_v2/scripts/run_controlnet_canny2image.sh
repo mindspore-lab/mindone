a60f0acba895d2a0cdb57bd24a678e87e36b453c
Alicia <115451386+congw729@users.noreply.github.com>
2023-08-22 15:35:32 +0800

    controlnet  canny2img (#76)
    * ControlNet canny2image under development.
    
    * ControlNet canny2image inference done [Ascend/Pynative/MS2.0]
    
    * controlnet canny2image pynative/ms2.0/Ascend done.
    
    * Reformat codes.
    
    * Fix CI.

---
+++ examples/stable_diffusion_v2/scripts/run_controlnet_canny2image.sh
@@ -0,0 +1,18 @@
+export GLOG_v=3
+export ASCEND_GLOBAL_LOG_LEVEL=3
+export ASCEND_SLOG_PRINT_TO_STDOUT=0
+
+export DEVICE_ID=1
+export INPUT_FILE_NAME="dog2"
+#dog bird cyber dog2 house toy
+export TASK_NAME="0822-canny2image"
+
+export DATA_DIR="/home/mindspore/congw/data/"
+
+nohup python image2image.py \
+    --model_config "./configs/v1_inference_contorlnet.yaml" \
+    --model_ckpt $DATA_DIR"control_sd15_canny_ms.ckpt" \
+    --input_image $DATA_DIR"test_imgs/"$INPUT_FILE_NAME".png" \
+    --prompt "cute toy" \
+    --task_name $TASK_NAME \
+    2>&1 > "output/"$TASK_NAME"_logms_id"$DEVICE_ID"_"$INPUT_FILE_NAME".txt" &
