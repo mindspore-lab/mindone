7213de0401a02c96c5a3fc693c34d6a4774f00fa
Samit <285365963@qq.com>
2023-09-04 11:03:34 +0800

    Add text-guided depth-to-image pipeline based on sd-v2-depth model (#103)

--- examples/stable_diffusion_v2/ldm/modules/diffusionmodules/openaimodel.py
+++ examples/stable_diffusion_v2/ldm/modules/diffusionmodules/openaimodel.py
@@ -615,7 +615,6 @@ class UNetModel(nn.Cell):
         :param y: an [N] Tensor of labels, if class-conditional.
         :return: an [N x C x ...] Tensor of outputs.
         """
-
         assert (y is not None) == (
             self.num_classes is not None
         ), "must specify y if and only if the model is class-conditional"
