1fdd49a02e0013853065c167a8c50d7f06b5bf5b
zhaoting <37293445+CaitinZhao@users.noreply.github.com>
2023-09-05 16:44:18 +0800

    reconstruct inference (#93)
    * add add inference
    
    * update
    
    * add strength for img2img task
    
    * update
    
    * update
    
    * update
    
    ---------
    
    Co-authored-by: zhaoting <zhaoting23@huawei.com>

---
+++ examples/stable_diffusion_v2/inference/libs/scheduler/ddim_schedule.py
@@ -0,0 +1,265 @@
+import math
+
+import numpy as np
+
+import mindspore as ms
+from mindspore import nn, ops
+
+
+def betas_for_alpha_bar(num_diffusion_timesteps, max_beta=0.999, alpha_transform_type="cosine"):
+    if alpha_transform_type == "cosine":
+
+        def alpha_bar_fn(t):
+            return math.cos((t + 0.008) / 1.008 * math.pi / 2) ** 2
+
+    elif alpha_transform_type == "exp":
+
+        def alpha_bar_fn(t):
+            return math.exp(t * -12.0)
+
+    else:
+        raise ValueError(f"Unsupported alpha_tranform_type: {alpha_transform_type}")
+
+    betas = []
+    for i in range(num_diffusion_timesteps):
+        t1 = i / num_diffusion_timesteps
+        t2 = (i + 1) / num_diffusion_timesteps
+        betas.append(min(1 - alpha_bar_fn(t2) / alpha_bar_fn(t1), max_beta))
+    return ms.Tensor(betas, ms.float32)
+
+
+def rescale_zero_terminal_snr(betas):
+    """
+    Rescales betas to have zero terminal SNR Based on https://arxiv.org/pdf/2305.08891.pdf (Algorithm 1)
+
+
+    Args:
+        betas (`torch.FloatTensor`):
+            the betas that the scheduler is being initialized with.
+
+    Returns:
+        `torch.FloatTensor`: rescaled betas with zero terminal SNR
+    """
+    # Convert betas to alphas_bar_sqrt
+    alphas = 1.0 - betas
+    alphas_cumprod = ms.Tensor(np.cumprod(alphas.asnumpy(), axis=0))
+    alphas_bar_sqrt = alphas_cumprod.sqrt()
+
+    # Store old values.
+    alphas_bar_sqrt_0 = alphas_bar_sqrt[0].clone()
+    alphas_bar_sqrt_T = alphas_bar_sqrt[-1].clone()
+
+    # Shift so the last timestep is zero.
+    alphas_bar_sqrt -= alphas_bar_sqrt_T
+
+    # Scale so the first timestep is back to the old value.
+    alphas_bar_sqrt *= alphas_bar_sqrt_0 / (alphas_bar_sqrt_0 - alphas_bar_sqrt_T)
+
+    # Convert alphas_bar_sqrt to betas
+    alphas_bar = alphas_bar_sqrt**2  # Revert sqrt
+    alphas = alphas_bar[1:] / alphas_bar[:-1]  # Revert cumprod
+    alphas = ops.concat([alphas_bar[0:1], alphas], axis=0)
+    betas = 1 - alphas
+
+    return betas
+
+
+class DDIMScheduler(nn.Cell):
+    def __init__(
+        self,
+        num_train_timesteps=1000,
+        beta_start=0.0001,
+        beta_end=0.02,
+        beta_schedule="scaled_linear",
+        trained_betas=None,
+        clip_sample=False,
+        set_alpha_to_one=True,
+        steps_offset=1,
+        prediction_type="epsilon",
+        thresholding=False,
+        dynamic_thresholding_ratio=0.995,
+        clip_sample_range=1.0,
+        sample_max_value=1.0,
+        timestep_spacing="leading",
+        rescale_betas_zero_snr=False,
+        eta=0.0,
+        use_clipped_model_output=False,
+        variance_noise=None,
+        with_mask=False,
+    ):
+        super(DDIMScheduler, self).__init__()
+        trained_betas = trained_betas if trained_betas != "None" else None
+        if trained_betas is not None:
+            self.betas = ms.Tensor(trained_betas, ms.float32)
+        elif beta_schedule == "linear":
+            self.betas = ms.Tensor(np.linspace(beta_start, beta_end, num_train_timesteps), ms.float32)
+        elif beta_schedule == "scaled_linear":
+            # this schedule is very specific to the latent diffusion model.
+            self.betas = ms.Tensor(
+                np.linspace(beta_start**0.5, beta_end**0.5, num_train_timesteps, dtype=np.float32) ** 2, ms.float32
+            )
+        elif beta_schedule == "squaredcos_cap_v2":
+            # Glide cosine schedule
+            self.betas = betas_for_alpha_bar(num_train_timesteps)
+        else:
+            raise NotImplementedError(f"{beta_schedule} does is not implemented for {self.__class__}")
+
+        # Rescale for zero SNR
+        if rescale_betas_zero_snr:
+            self.betas = rescale_zero_terminal_snr(self.betas)
+
+        self.alphas = 1.0 - self.betas
+        self.alphas_cumprod = ms.Tensor(np.cumprod(self.alphas.asnumpy(), axis=0), ms.float16)
+
+        # At every step in ddim, we are looking into the previous alphas_cumprod
+        # For the final step, there is no previous alphas_cumprod because we are already at 0
+        # `set_alpha_to_one` decides whether we set this parameter simply to one or
+        # whether we use the final alpha of the "non-previous" one.
+        self.final_alpha_cumprod = ms.Tensor(1.0) if set_alpha_to_one else self.alphas_cumprod[0]
+
+        # standard deviation of the initial noise distribution
+        self.init_noise_sigma = 1.0
+
+        # setable values
+        self.num_inference_steps = None
+        self.timesteps = ms.Tensor(np.arange(0, num_train_timesteps)[::-1].copy().astype(np.int64))
+        self.num_train_timesteps = num_train_timesteps
+        self.beta_start = beta_start
+        self.beta_end = beta_end
+        self.beta_schedule = beta_schedule
+        self.trained_betas = trained_betas
+        self.clip_sample = clip_sample
+        self.set_alpha_to_one = set_alpha_to_one
+        self.steps_offset = steps_offset
+        self.prediction_type = prediction_type
+        self.thresholding = thresholding
+        self.dynamic_thresholding_ratio = dynamic_thresholding_ratio
+        self.clip_sample_range = clip_sample_range
+        self.sample_max_value = sample_max_value
+        self.timestep_spacing = timestep_spacing
+        self.rescale_betas_zero_snr = rescale_betas_zero_snr
+        self.eta = eta
+        self.use_clipped_model_output = use_clipped_model_output
+        self.variance_noise = variance_noise if variance_noise != "None" else None
+        self.with_mask = with_mask
+        self.order = 1
+
+    def set_timesteps(self, num_inference_steps):
+        """
+        Sets the discrete timesteps used for the diffusion chain (to be run before inference).
+
+        Args:
+            num_inference_steps (`int`):
+                The number of diffusion steps used when generating samples with a pre-trained model.
+        """
+
+        if num_inference_steps > self.num_train_timesteps:
+            raise ValueError(
+                f"`num_inference_steps`: {num_inference_steps} cannot be larger than `self.train_timesteps`:"
+                f" {self.num_train_timesteps} as the unet model trained with this scheduler can only handle"
+                f" maximal {self.num_train_timesteps} timesteps."
+            )
+
+        self.num_inference_steps = num_inference_steps
+
+        # "linspace", "leading", "trailing" corresponds to annotation of Table 2. of https://arxiv.org/abs/2305.08891
+        if self.timestep_spacing == "linspace":
+            timesteps = (
+                np.linspace(0, self.num_train_timesteps - 1, num_inference_steps).round()[::-1].copy().astype(np.int64)
+            )
+        elif self.timestep_spacing == "leading":
+            step_ratio = self.num_train_timesteps // self.num_inference_steps
+            # creates integer timesteps by multiplying by ratio
+            # casting to int to avoid issues when num_inference_step is power of 3
+            timesteps = (np.arange(0, num_inference_steps) * step_ratio).round()[::-1].copy().astype(np.int64)
+            timesteps += self.steps_offset
+        elif self.timestep_spacing == "trailing":
+            step_ratio = self.num_train_timesteps / self.num_inference_steps
+            # creates integer timesteps by multiplying by ratio
+            # casting to int to avoid issues when num_inference_step is power of 3
+            timesteps = np.round(np.arange(self.num_train_timesteps, 0, -step_ratio)).astype(np.int64)
+            timesteps -= 1
+        else:
+            raise ValueError(
+                f"{self.timestep_spacing} is not supported. Please make sure to choose one of 'leading' or 'trailing'."
+            )
+
+        self.timesteps = timesteps.tolist()
+        return self.timesteps
+
+    def _get_variance(self, alpha_prod_t, alpha_prod_t_prev):
+        beta_prod_t = 1 - alpha_prod_t
+        beta_prod_t_prev = 1 - alpha_prod_t_prev
+        variance = (beta_prod_t_prev / beta_prod_t) * (1 - alpha_prod_t / alpha_prod_t_prev)
+        return variance
+
+    def construct(self, model_output, timestep, sample, num_inference_steps, mask=None):
+        # See formulas (12) and (16) of DDIM paper https://arxiv.org/pdf/2010.02502.pdf
+        # Ideally, read DDIM paper in-detail understanding
+
+        # Notation (<variable name> -> <name in paper>
+        # - pred_noise_t -> e_theta(x_t, t)
+        # - pred_original_sample -> f_theta(x_t, t) or x_0
+        # - std_dev_t -> sigma_t
+        # - eta -> η
+        # - pred_sample_direction -> "direction pointing to x_t"
+        # - pred_prev_sample -> "x_t-1"
+        # 1. get previous step value (=t-1)
+        timestep = timestep.astype(ms.int32)
+        prev_timestep = ops.maximum(timestep - self.num_train_timesteps // num_inference_steps, 0)
+        # 2. compute alphas, betas
+        alpha_prod_t = self.alphas_cumprod[timestep]
+        alpha_prod_t_prev = self.alphas_cumprod[prev_timestep]
+
+        beta_prod_t = 1 - alpha_prod_t
+
+        # 3. compute predicted original sample from predicted noise also called
+        # "predicted x_0" of formula (12) from https://arxiv.org/pdf/2010.02502.pdf
+        if self.prediction_type == "epsilon":
+            pred_original_sample = (sample - beta_prod_t.sqrt() * model_output) / alpha_prod_t.sqrt()
+            pred_epsilon = model_output
+        elif self.prediction_type == "sample":
+            pred_original_sample = model_output
+            pred_epsilon = (sample - alpha_prod_t.sqrt() * pred_original_sample) / beta_prod_t.sqrt()
+        elif self.prediction_type == "v_prediction":
+            pred_original_sample = (alpha_prod_t.sqrt()) * sample - (beta_prod_t.sqrt()) * model_output
+            pred_epsilon = (alpha_prod_t.sqrt()) * model_output + (beta_prod_t.sqrt()) * sample
+        else:
+            raise ValueError(
+                f"prediction_type given as {self.prediction_type} must be one of `epsilon`, `sample`, or"
+                " `v_prediction`"
+            )
+        # 4. Clip or threshold "predicted x_0"
+        if self.thresholding:
+            pred_original_sample = self._threshold_sample(pred_original_sample)
+        elif self.clip_sample:
+            pred_original_sample = pred_original_sample.clamp(-self.clip_sample_range, self.clip_sample_range)
+        # 5. compute variance: "sigma_t(η)" -> see formula (16)
+        variance = self._get_variance(alpha_prod_t, alpha_prod_t_prev)
+        std_dev_t = self.eta * variance.sqrt()
+        if self.use_clipped_model_output:
+            # the pred_epsilon is always re-derived from the clipped x_0 in Glide
+            pred_epsilon = (sample - alpha_prod_t.sqrt() * pred_original_sample) / beta_prod_t.sqrt()
+
+        # 6. compute "direction pointing to x_t" of formula (12) from https://arxiv.org/pdf/2010.02502.pdf
+        pred_sample_direction = (1 - alpha_prod_t_prev - std_dev_t * std_dev_t).sqrt() * pred_epsilon
+        # 7. compute x_t without "random noise" of formula (12) from https://arxiv.org/pdf/2010.02502.pdf
+        prev_sample = alpha_prod_t_prev.sqrt() * pred_original_sample + pred_sample_direction
+
+        if self.eta > 0:
+            variance_noise = self.variance_noise
+            if variance_noise is None:
+                variance_noise = ops.standard_normal(model_output.shape).astype(model_output.dtype)
+            variance = std_dev_t * variance_noise
+            prev_sample = prev_sample + variance
+        return prev_sample
+
+    def scale_model_input(self, latents, t):
+        return latents + t * 0  # If t is not used, lite will eliminate the second input
+
+    def add_noise(self, original_samples, noise, alphas_ts):
+        # Make sure alphas_cumprod and timestep have same device and dtype as original_samples
+        sqrt_alpha_prod = alphas_ts.sqrt()
+        sqrt_one_minus_alpha_prod = (1 - alphas_ts).sqrt()
+        noisy_samples = sqrt_alpha_prod * original_samples + sqrt_one_minus_alpha_prod * noise
+        return noisy_samples
---
+++ examples/stable_diffusion_v2/inference/libs/sd_models.py
@@ -0,0 +1,202 @@
+from abc import ABC, abstractmethod
+
+from tqdm import tqdm
+
+import mindspore as ms
+from mindspore import ops
+
+
+class SDInfer(ABC):
+    """
+    Stable Diffusion inference.
+
+    Args:
+        text_encoder (nn.Cell): Frozen text-encoder.
+        unet (nn.Cell): A `UNet2DConditionModel` to denoise the encoded image latents.
+        vae (nn.Cell): Variational Auto-Encoder (VAE) Model to encode and decode images to and from latent representations.
+        scheduler (nn.Cell): A scheduler to be used in combination with `unet` to denoise the encoded image latents.
+        scale_factor (float): scale_factor for vae.
+        guidance_rescale (float): A higher guidance scale value for noise rescale.
+        num_inference_steps: (int): The number of denoising steps.
+    """
+
+    def __init__(
+        self,
+        text_encoder,
+        unet,
+        vae,
+        scheduler,
+        scale_factor=1.0,
+        guidance_rescale=0.0,
+        num_inference_steps=50,
+    ):
+        super().__init__()
+        self.text_encoder = text_encoder
+        self.unet = unet
+        self.vae = vae
+        self.scheduler = scheduler
+        self.scale_factor = scale_factor
+        self.guidance_rescale = guidance_rescale
+        self.num_inference_steps = ms.Tensor(num_inference_steps, ms.int32)
+        self.alphas_cumprod = scheduler.alphas_cumprod
+
+    @ms.jit
+    def vae_encode(self, x):
+        image_latents = self.vae.encode(x)
+        image_latents = image_latents * self.scale_factor
+        return image_latents.astype(ms.float16)
+
+    @ms.jit
+    def vae_decode(self, x):
+        y = self.vae.decode(x / self.scale_factor)
+        y = ops.clip_by_value((y + 1.0) / 2.0, clip_value_min=0.0, clip_value_max=1.0)
+        return y
+
+    @ms.jit
+    def prompt_embed(self, prompt_data, negative_prompt_data):
+        pos_prompt_embeds = self.text_encoder(prompt_data)
+        negative_prompt_embeds = self.text_encoder(negative_prompt_data)
+        prompt_embeds = ops.concat([negative_prompt_embeds, pos_prompt_embeds], axis=0)
+        return prompt_embeds
+
+    @ms.jit
+    def latents_add_noise(self, image_latents, noise, ts):
+        latents = self.scheduler.add_noise(image_latents, noise, self.alphas_cumprod[ts])
+        return latents
+
+    @ms.jit
+    def scale_model_input(self, latents, t):
+        return self.scheduler.scale_model_input(latents, t)
+
+    @ms.jit
+    def predict_noise(self, x, t_continuous, c_crossattn, guidance_scale, c_concat=None):
+        """
+        The noise predicition model function that is used for DPM-Solver.
+        """
+        t_continuous = ops.tile(t_continuous.reshape(1), (x.shape[0],))
+        x_in = ops.concat([x] * 2, axis=0)
+        t_in = ops.concat([t_continuous] * 2, axis=0)
+        if c_concat is not None:
+            c_concat = ops.concat([c_concat] * 2, axis=0)
+        noise_pred = self.unet(x_in, t_in, c_concat=c_concat, c_crossattn=c_crossattn)
+        noise_pred_uncond, noise_pred_text = ops.split(noise_pred, split_size_or_sections=noise_pred.shape[0] // 2)
+        noise_pred = noise_pred_uncond + guidance_scale * (noise_pred_text - noise_pred_uncond)
+        if self.guidance_rescale > 0:
+            noise_pred = self.rescale_noise_cfg(noise_pred, noise_pred_text)
+        return noise_pred
+
+    def rescale_noise_cfg(self, noise_pred, noise_pred_text):
+        """
+        Rescale `noise_pred` according to `guidance_rescale`. Based on findings of [Common Diffusion Noise Schedules and
+        Sample Steps are Flawed](https://arxiv.org/pdf/2305.08891.pdf). See Section 3.4
+        """
+        std_text = ops.std(noise_pred_text, axis=tuple(range(1, len(noise_pred_text.shape))), keepdims=True)
+        std_cfg = ops.std(noise_pred, axis=tuple(range(1, len(noise_pred.shape))), keepdims=True)
+        # rescale the results from guidance (fixes overexposure)
+        noise_pred_rescaled = noise_pred * (std_text / std_cfg)
+        # mix with the original results from guidance by factor guidance_rescale to avoid "plain looking" images
+        noise_pred = self.guidance_rescale * noise_pred_rescaled + (1 - self.guidance_rescale) * noise_pred
+        return noise_pred
+
+    @abstractmethod
+    def data_prepare(self, inputs):
+        pass
+
+    def __call__(self, inputs):
+        latents, c_crossattn, c_concat = self.data_prepare(inputs)
+        timesteps = inputs["timesteps"]
+        iterator = tqdm(timesteps, desc="Stable Diffusion Sampling", total=len(timesteps))
+        for i, t in enumerate(iterator):
+            ts = ms.Tensor(t, ms.int32)
+            latents = self.scale_model_input(latents, ts)
+            noise_pred = self.predict_noise(latents, ts, c_crossattn, inputs["scale"], c_concat)
+            latents = self.scheduler(noise_pred, ts, latents, self.num_inference_steps)
+        image = self.vae_decode(latents)
+        return image
+
+
+class SDText2Img(SDInfer):
+    def __init__(
+        self,
+        text_encoder,
+        unet,
+        vae,
+        scheduler,
+        scale_factor=1.0,
+        guidance_rescale=0.0,
+        num_inference_steps=50,
+    ):
+        super(SDText2Img, self).__init__(
+            text_encoder,
+            unet,
+            vae,
+            scheduler,
+            scale_factor=scale_factor,
+            guidance_rescale=guidance_rescale,
+            num_inference_steps=num_inference_steps,
+        )
+
+    def data_prepare(self, inputs):
+        latents = inputs["noise"]
+        c_crossattn = self.prompt_embed(inputs["prompt_data"], inputs["negative_prompt_data"])
+        return latents, c_crossattn, None
+
+
+class SDImg2Img(SDInfer):
+    def __init__(
+        self,
+        text_encoder,
+        unet,
+        vae,
+        scheduler,
+        scale_factor=1.0,
+        guidance_rescale=0.0,
+        num_inference_steps=50,
+    ):
+        super(SDImg2Img, self).__init__(
+            text_encoder,
+            unet,
+            vae,
+            scheduler,
+            scale_factor=scale_factor,
+            guidance_rescale=guidance_rescale,
+            num_inference_steps=num_inference_steps,
+        )
+
+    def data_prepare(self, inputs):
+        timesteps = inputs["timesteps"]
+        t0 = ms.Tensor(timesteps[0], ms.int32)
+        image_latents = self.vae_encode(inputs["img"])
+        latents = self.latents_add_noise(image_latents, inputs["noise"], t0)
+        c_crossattn = self.prompt_embed(inputs["prompt_data"], inputs["negative_prompt_data"])
+        return latents, c_crossattn, None
+
+
+class SDInpaint(SDInfer):
+    def __init__(
+        self,
+        text_encoder,
+        unet,
+        vae,
+        scheduler,
+        scale_factor=1.0,
+        guidance_rescale=0.0,
+        num_inference_steps=50,
+    ):
+        super(SDInpaint, self).__init__(
+            text_encoder,
+            unet,
+            vae,
+            scheduler,
+            scale_factor=scale_factor,
+            guidance_rescale=guidance_rescale,
+            num_inference_steps=num_inference_steps,
+        )
+
+    def data_prepare(self, inputs):
+        masked_image = self.vae_encode(inputs["masked_image"])
+        mask = ops.ResizeNearestNeighbor(masked_image.shape[2:])(inputs["mask"])
+        c_concat = ops.concat((mask, masked_image), axis=1)
+        latents = inputs["noise"]
+        c_crossattn = self.prompt_embed(inputs["prompt_data"], inputs["negative_prompt_data"])
+        return latents, c_crossattn, c_concat
