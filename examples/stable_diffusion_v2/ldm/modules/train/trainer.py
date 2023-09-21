Allow gradient accumulation. Fix 910B bugs. Fix ckpt save name.

examples/stable_diffusion_v2/ldm/modules/train/trainer.py
@@ -154,8 +154,8 @@ class TrainOneStepWrapper(nn.TrainOneStepWithLossScaleCell):
             # 8.ema
             if self.ema is not None:
                 self.ema.ema_update()
-        else:
-            print("WARNING: Gradient overflow! update skipped.")
-            pass
+        #else:
+        #    print("WARNING: Gradient overflow! update skipped.") # TODO: recover it after 910B in-graph print issue fixed
+        #    pass
 
         return loss, cond, scaling_sens