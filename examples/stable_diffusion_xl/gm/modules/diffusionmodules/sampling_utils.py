16f4354cad6a862aeb59a8f273d334b5c6b04638
Fzilan <33061146+Fzilan@users.noreply.github.com>
2023-10-09 20:22:41 +0800

    feat: add sdxl sampler (#174)
    * add HeunEDMSampler, DPMPP2MSampler
    
    * complete samplers of streamlit
    
    ---------
    
    Co-authored-by: Fzilan <Fzilan@users.noreply.github.com>

--- examples/stable_diffusion_xl/gm/modules/diffusionmodules/sampling_utils.py
+++ examples/stable_diffusion_xl/gm/modules/diffusionmodules/sampling_utils.py
@@ -10,3 +10,11 @@ class NoDynamicThresholding:
 
 def to_d(x, sigma, denoised):
     return (x - denoised) / append_dims(sigma, x.ndim)
+
+
+def to_neg_log_sigma(sigma):
+    return sigma.log().neg()
+
+
+def to_sigma(neg_log_sigma):
+    return neg_log_sigma.neg().exp()
