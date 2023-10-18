b3eeb5bab49140777612d0c61a7163b7b25cb84f
Yang Chen <isyangc2@gmail.com>
2023-10-18 20:53:56 +0800

    Add score clip and fid on SDXL

---
+++ examples/stable_diffusion_xl/tools/_common/utils.py
@@ -0,0 +1,36 @@
+"common utility functions and classes"
+import os
+from functools import partial
+
+
+from PIL import Image
+
+from mindspore import ops
+
+# get L2 norm operator
+L2_norm_ops = partial(ops.norm, ord=2, dim=1, keepdim=True)
+
+
+def load_images(paths, resize=True):
+    # load images
+    images = []
+    if os.path.isdir(paths) and os.path.exists(paths):
+        paths = [
+            os.path.join(root, file)
+            for root, _, file_list in os.walk(os.path.join(paths))
+            for file in file_list
+            if file.endswith(".jpg")
+            or file.endswith(".png")
+            or file.endswith(".jpeg")
+            or file.endswith(".JPEG")
+            or file.endswith("bmp")
+        ]
+        paths.sort()
+        images = [Image.open(p) for p in paths]
+        paths = paths
+    else:
+        images = [Image.open(paths)]
+        paths = [paths]
+    if resize:
+        images = [image.resize((224, 224)) for image in images]
+    return images, paths
