409d115d35cb3c395cfa879f9120ca5ae3cb94c1
Rustam Khadipash <16683750+hadipash@users.noreply.github.com>
2023-10-19 16:45:18 +0800

    VideoLDM v0.1

--- examples/video_ldm/modules/unet3d.py
+++ examples/video_ldm/modules/unet3d.py
@@ -1,5 +1,15 @@
 from mindspore import nn, Parameter, Tensor
 from ldm.modules.diffusionmodules.openaimodel import UNetModel
+from ldm.modules.attention import BasicTransformerBlock
+
+
+class GroupNorm3D(nn.GroupNorm):
+    """
+    MindSpore supports (N, C, H, W) input only
+    """
+
+    def construct(self, x: Tensor) -> Tensor:
+        return super().construct(x.view(x.shape[0], x.shape[1], x.shape[2], -1)).view(x.shape)
 
 
 class Conv3DLayer(nn.Cell):
@@ -8,7 +18,7 @@ class Conv3DLayer(nn.Cell):
         self._num_frames = num_frames
 
         self.conv3d = nn.SequentialCell(
-            nn.GroupNorm(32, in_channels),
+            GroupNorm3D(32, in_channels),
             nn.SiLU(),
             nn.Conv3d(
                 in_channels,
@@ -18,7 +28,7 @@ class Conv3DLayer(nn.Cell):
                 pad_mode="pad",
                 has_bias=True,
             ),
-            nn.GroupNorm(32, out_channels),
+            GroupNorm3D(32, out_channels),
             nn.SiLU(),
             nn.Conv3d(
                 out_channels,
@@ -32,63 +42,77 @@ class Conv3DLayer(nn.Cell):
 
         self.alpha = Parameter(1.0)
 
-    def construct(self, x: Tensor) -> Tensor:
-        h = x.reshape(-1, self._num_frames, x.shape[1], x.shape[2], x.shape[3])  # (b t) c h w -> b c t h w
+    def construct(self, x: Tensor, *args) -> Tensor:
+        # (b t) c h w -> b c t h w
+        h = x.reshape(-1, self._num_frames, x.shape[1], x.shape[2], x.shape[3]).swapaxes(1, 2)
         h = self.conv3d(h)
-        h = h.reshape(-1, x.shape[1], x.shape[2], x.shape[3])  # b c t h w -> (b t) c h w
+        h = h.swapaxes(1, 2).reshape(-1, x.shape[1], x.shape[2], x.shape[3])  # b c t h w -> (b t) c h w
 
         # TODO: limit alpha with no grad
         return self.alpha * x + (1 - self.alpha) * h
 
 
-class TemporalAttentionLayer(nn.Cell):
-    # TODO: replace with ldm.modules.attention.CrossAttention
-    def __init__(self, dim, num_frames: int, num_heads: int = 8, kv_dim=None):
+class TemporalTransformer(nn.Cell):
+    def __init__(
+        self,
+        in_channels: int,
+        num_frames: int,
+        n_heads: int = 16,
+        d_head: int = 88,
+        depth: int = 1,
+        dropout: float = 0.0,
+        context_dim: int = None,
+        enable_flash_attention=False,
+    ):
         super().__init__()
-        self.n_frames = num_frames
-        self.n_heads = num_heads
+        self._num_frames = num_frames
+        self.in_channels = in_channels
+        inner_dim = n_heads * d_head
 
-        head_dim = dim // num_heads
-        proj_dim = head_dim * num_heads
-        self.q_proj = nn.Dense(dim, proj_dim, has_bias=False)
+        self.norm = GroupNorm3D(32, in_channels, eps=1e-6)
+        self.proj_in = nn.Dense(in_channels, inner_dim)
 
-        kv_dim = kv_dim or dim
-        self.k_proj = nn.Dense(kv_dim, proj_dim, has_bias=False)
-        self.v_proj = nn.Dense(kv_dim, proj_dim, has_bias=False)
-        self.o_proj = nn.Dense(proj_dim, dim, has_bias=False)
+        # 3. Define transformers blocks
+        self.transformer_blocks = nn.CellList(
+            [
+                BasicTransformerBlock(
+                    inner_dim,
+                    n_heads,
+                    d_head,
+                    dropout=dropout,
+                    context_dim=context_dim,
+                    enable_flash_attention=enable_flash_attention,
+                )
+                for _ in range(depth)
+            ]
+        )
 
-        # self.attention = nn.MultiheadAttention(dim, num_heads, batch_first=True, has_bias=True)
-        # TODO: self.pos_enc = PositionalEncoding(dim)
+        self.proj_out = nn.Dense(inner_dim, in_channels)
         self.alpha = Parameter(1.0)
 
-    def forward(self, q: Tensor, kv=None, mask=None):
-        skip = q
+    def construct(self, x: Tensor, emb=None, context=None):
+        residual = x
 
-        bt, c, h, w = q.shape
-        # (b t) c h w -> b (h w) t c
-        q = q.reshape(-1, self.n_frames, c, h, w)
-        q = q.traspose(0, 3, 4, 1, 2).reshape(q.shape[0], h * w, q.shape[1], q.shape[2])
+        # 1. Input
+        _, c, h, w = x.shape
 
-        # q = q + self.pos_enc(self.n_frames)
+        x = x.reshape(-1, self._num_frames, c, h, w).swapaxes(1, 2)     # (b t) c h w -> b c t h w
+        x = self.norm(x)
+        x = x.transpose(0, 3, 4, 2, 1).reshape(-1, self._num_frames, c)  # b c t h w -> (b h w) t c
+        x = self.proj_in(x)
 
-        kv = kv[:: self.n_frames] if kv is not None else q
-        q = self.q_proj(q)
-        k = self.k_proj(kv)
-        v = self.v_proj(kv)
+        # 2. Blocks
+        for block in self.transformer_blocks:
+            x = block(x, context=context)
 
-        # q = rearrange(q, "b hw t (heads d) -> b hw heads t d", heads=self.n_heads)
-        # k = rearrange(k, "b s (heads d) -> b heads s d", heads=self.n_heads)
-        # v = rearrange(v, "b s (heads d) -> b heads s d", heads=self.n_heads)
+        # 3. Output
+        x = self.proj_out(x)
 
-        # out = F.scaled_dot_product_attention(q, k, v, mask)
-        # out = rearrange(out, "b hw heads t d -> b hw t (heads d)")
-        # out = self.o_proj(out)
-
-        # out = rearrange(out, "b (h w) t c -> (b t) c h w", h=h, w=w)
+        x = x.reshape(-1, h, w, self._num_frames, c).permute(0, 3, 4, 1, 2)  # (b h w) t c -> b t c h w
+        x = x.reshape(-1, c, h, w)  # b t c h w -> (b t) c h w
 
         # TODO: limit alpha with no grad
-        out = q
-        return self.alpha * skip + (1 - self.alpha) * out
+        return self.alpha * residual + (1 - self.alpha) * x
 
 
 class VideoLDMUNetModel(UNetModel):
@@ -102,4 +126,15 @@ class VideoLDMUNetModel(UNetModel):
                 if "ResBlock" in names and "SpatialTransformer" in names:
                     out_channels = blocks[i][0].out_channels
                     blocks[i].insert(1, Conv3DLayer(out_channels, out_channels, num_frames))
-                    blocks[i].append(TemporalAttentionLayer(out_channels, num_frames, kv_dim=kwargs["context_dim"]))
+                    blocks[i].append(
+                        TemporalTransformer(
+                            out_channels,
+                            num_frames,
+                            n_heads=8,
+                            d_head=out_channels // 8,
+                            dropout=1.0,
+                            context_dim=kwargs["context_dim"],
+                            depth=2,
+                            enable_flash_attention=kwargs["enable_flash_attention"],
+                        )
+                    )
