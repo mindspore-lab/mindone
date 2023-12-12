b1acf1110104f264e6e4d3ceb195d614be5e0bb1
samithuang <285365963@qq.com>
2023-12-12 09:55:53 +0800

    tmp save

--- examples/stable_diffusion_xl/scripts/run_train_lora.sh
+++ examples/stable_diffusion_xl/scripts/run_train_lora.sh
@@ -1,23 +1,24 @@
 export DEVICE_ID=$1
 export MS_PYNATIVE_GE=1
-export MS_ASCEND_CHECK_OVERFLOW_MODE="INFNAN_MODE"
-#export MS_ASCEND_CHECK_OVERFLOW_MODE=1 # "INFNAN_MODE"
+# export MS_ASCEND_CHECK_OVERFLOW_MODE="INFNAN_MODE"
+# export MS_ASCEND_CHECK_OVERFLOW_MODE=1 # "INFNAN_MODE"
 export PYTHONPATH=$(pwd):$PYTHONPATH
 
 #data_path=datasets/chinese_art_blip/train
 data_path=datasets/pokemon_blip/train
-task_name=train_lora_pokemon_r4_InfNan
+task_name=train_lora_pokemon_r4_fixedko
 save_path=outputs/$task_name
 
 rm -rf $save_path
 mkdir -p $save_path
 
 # train
-python train_lora.py \
+python train.py \
     --config configs/training/sd_xl_base_finetune_lora_910b.yaml \
     --data_path $data_path \
     --weight models/sd_xl_base_1.0_ms.ckpt \
     --save_path $save_path \
+    --gradient_accumulation_steps 4 \
     > $save_path/train.log 2>&1 &
 
 # infer
