9da8605d21b2875576a19a7c287464d2e2d4c205
Samit <285365963@qq.com>
2023-12-19 17:46:37 +0800

    support webdataset with wids random access

--- examples/stable_diffusion_xl/gm/data/dataset_wds.py
+++ examples/stable_diffusion_xl/gm/data/dataset_wds.py
@@ -8,6 +8,7 @@ import numpy as np
 import pandas as pd
 from PIL import Image
 import webdataset as wds
+import wids
 from itertools import islice
 import io
 import json
@@ -28,12 +29,35 @@ def get_tar_file_list(data_dir):
 
     return tar_files
 
+def get_tar_nsample(tar_file):
+    # didn't check content completeness.
+    wds_iterator = wds.WebDataset(tar_file)
+    n = 0
+    for cur in wds_iterator:
+        n += 1
+    return n
 
-class T2I_RandWebdataset:
+def generate_sharlist(data_dir):
+    tar_files = get_tar_file_list(data_dir)
+    out = { "__kind__": "wids-shard-index-v1",
+            "wids_version": 1,
+            "shardlist": [],
+           }
+    for tf in tar_files:
+        nsamples = get_tar_nsample(tf)
+        out['shardlist'].append({"url": tf, "nsamples": nsamples})
+    save_fp = os.path.join(data_dir, 'data_info.json')
+    with open(save_fp, 'w') as fp:
+        json.dump(out, fp)
+
+    return save_fp
+
+
+class T2I_BaseDataset:
     def __init__(
         self,
         data_path,
-        num_samples=None, # required for webdataset. If None, it will spend time to count
+        num_samples=None, # need for webdataset to get data len
         target_size=(1024, 1024),
         transforms=None,
         batched_transforms=None,
@@ -45,6 +69,7 @@ class T2I_RandWebdataset:
         multi_aspect=None,  # for multi_aspect
         seed=42,  # for multi_aspect
         per_batch_size=1,  # for multi_aspect
+        caption_key='caption',
     ):
         super().__init__()
         self.tokenizer = tokenizer
@@ -61,21 +86,13 @@ class T2I_RandWebdataset:
         self.target_size = [target_size, target_size] if isinstance(target_size, int) else target_size
         self.random_crop = random_crop
         self.filter_small_size = filter_small_size
+        assert not filter_small_size, 'filter small size is not supported'
 
         self.multi_aspect = list(multi_aspect) if multi_aspect is not None else None
         self.seed = seed
         self.per_batch_size = per_batch_size
 
-        # create webdataset iterator for tar files
-        tar_files = get_tar_file_list(data_path)
-        print(f"Get {len(tar_files)} tar files")
-        self.wds_iterator = wds.WebDataset(tar_files, cache_dir=None)
-        if num_samples is None:
-            print("WARNING: For webdataset, it's recommended to specify `num_samples` to save time to iterate all samples for counting")
-            self.num_samples = self.count_sample_num(self.wds_iterator)
-            print(f"Total number of samples: {self.num_samples} in all tar files")
-        else:
-            self.num_samples = num_samples
+        self.caption_key = caption_key
 
         # ds = ds.shuffle(1000) # uncomment for local shuffle. no needed if shuffle in generator dataset
         # ds = ds.decode("rgb8").to_tuple("jpg;png", "json") # will do in getitem to save time
@@ -98,23 +115,11 @@ class T2I_RandWebdataset:
                     f"Adding batch mapper {bs_trans.__class__.__name__} as batch transform #{i} " f"to the datapipeline"
                 )
 
-    def __getitem__(self, idx):
-        # images preprocess
-        '''
-        image_path = self.local_images[idx]
-        image = Image.open(image_path)
+    def preprocess(self, image, caption: str):
+        # preprocess image and caption
         if not image.mode == "RGB":
             image = image.convert("RGB")
         image = np.array(image).astype(np.uint8)
-        '''
-        raw = next(islice(self.wds_iterator, idx, idx+1))
-        image = Image.open(io.BytesIO(raw['jpg']))
-        if not image.mode == "RGB":
-            image = image.convert("RGB")
-        image = np.array(image).astype(np.uint8)
-
-        annot = json.load(io.BytesIO(raw['json']))
-        caption = annot['caption']
 
         # caption preprocess
         caption = np.array(caption)
@@ -175,28 +180,104 @@ class T2I_RandWebdataset:
 
         return cnt
 
-    @staticmethod
-    def filter_small_image(all_images, all_captions, image_filter_size):
-        filted_images = []
-        filted_captions = []
-        for image, caption in zip(all_images, all_captions):
-            w, h = imagesize.get(image)
-            if min(w, h) < image_filter_size:
-                print(f"The size of image {image}: {w}x{h} < `image_filter_size` and excluded from training.")
-                continue
+class T2I_Webdataset(T2I_BaseDataset):
+    # sequential reading
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        data_path = kwargs.get('data_path')
+        num_samples = kwargs.get('num_samples')
+
+        tar_files = get_tar_file_list(data_path)
+        print(f"Get {len(tar_files)} tar files")
+
+        self.wds_iterator = wds.WebDataset(tar_files, cache_dir=None)
+
+        if num_samples is None:
+            print("WARNING: For webdataset, it's recommended to specify `num_samples` to save time to iterate all samples for counting")
+            self.num_samples = self.count_sample_num(self.wds_iterator)
+            print(f"Total number of samples: {self.num_samples} in all tar files")
+        else:
+            self.num_samples = num_samples
+
+    def parse_raw_data(self, raw_data):
+        if 'jpg' in raw_data:
+            image = Image.open(io.BytesIO(raw_data['jpg']))
+        else:
+            image = Image.open(io.BytesIO(raw_data['png']))
+
+        annot = json.load(io.BytesIO(raw_data['json']))
+
+        caption = annot[caption_key]
+
+        return image, caption
+
+    def __iter__(self):
+        # images preprocess
+        for raw in self.wds_iterator:
+            try:
+                image, caption = self.parse_raw_data(raw)
+                sample = self.preprocess(image, caption)
+                yield sample
+            except StopIteration:
+                raise StopIteration
+
+
+class T2I_Webdataset_RndAcs(T2I_BaseDataset):
+    # random access
+    def __init__(self, shardlist_desc=None, *args, **kwargs):
+        # shardlist_desc: path to a json file describing sample num for each tar
+        # shardlist_desc =kwargs.pop('shardlist_desc')
+        super().__init__(*args, **kwargs)
+        if shardlist_desc is None:
+            data_path = kwargs.get('data_path')
+            if not os.path.exists(os.path.join(data_path, 'data_info.json')):
+                print("Scanning tar files to get sample nums...")
+                shardlist_desc = generate_sharlist(data_path)
+                print("=> Saved shardlist json file in ", shardlist_desc)
             else:
-                filted_images.append(image)
-                filted_captions.append(caption)
-        return filted_images, filted_captions
+                shardlist_desc = os.path.join(data_path, 'data_info.json')
+        print("Loading sharlist description from: ", shardlist_desc)
+
+        with open(shardlist_desc, 'r') as fp:
+            shardlist = json.load(fp)['shardlist']
+        self.dataset = wids.ShardListDataset(shardlist)
+
+    def parse_raw_data(self, raw_data):
+        # parse webdataset reading result
+        if '.jpg' in raw_data:
+            image = raw_data['.jpg']
+        elif 'png' in raw_data:
+            image = raw_data['.png']
+        else:
+            raise ValueError('Missing jpg/png image, only get keys: {}'.format(raw_data.keys()))
+
+        caption = raw_data['.json'][self.caption_key]
+
+        return image, caption
+
+    def __getitem__(self, idx):
+        # images preprocess
+        # raw = next(islice(self.wds_iterator, idx, idx+1))
+        raw = self.dataset[idx]
+        image, caption = self.parse_raw_data(raw)
+        sample = self.preprocess(image, caption)
+
+        return sample
+
+    def __len__(self):
+        return len(self.dataset)
 
 
 if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser(description="check dataset")
-    parser.add_argument("--target", type=str, default="T2I_RandWebdataset")
+    parser.add_argument("--target", type=str, default="T2I_Webdataset_RndAcs")
     # for Text2ImageDataset
     parser.add_argument("--data_path", type=str, default="")
+    parser.add_argument("--shardlist_desc", type=str, default=None)
+    parser.add_argument("--caption_key", type=str, default='caption')
     # for Text2ImageDatasetDreamBooth
     args, _ = parser.parse_known_args()
     transforms = [
@@ -205,19 +286,25 @@ if __name__ == "__main__":
         {"target": "gm.data.mappers.AddOriginalImageSizeAsTupleAndCropToSquare"},
     ]
     print('loading..')
-    if args.target == "T2I_RandWebdataset":
-        dataset = T2I_RandWebdataset(data_path=args.data_path, target_size=1024, transforms=transforms)
-        dataset_size = len(dataset)
-        print(f"dataset size: {dataset_size}")
-
-        s_time = time.time()
-        for i, data in enumerate(dataset):
-            if i > 9:
-                break
-            print(
-                f"{i}/{dataset_size}, image shape: {data.pop('image')}, {data}, "
-                f"time cost: {(time.time()-s_time) * 1000} ms"
-            )
-            s_time = time.time()
+    if args.target == "T2I_Webdataset":
+        dataset = T2I_Webdataset(data_path=args.data_path, target_size=1024, transforms=transforms, caption_key=args.caption_key)
+    elif args.target == "T2I_Webdataset_RndAcs":
+        dataset = T2I_Webdataset_RndAcs(data_path=args.data_path, shardlist_desc=args.shardlist_desc, target_size=1024, transforms=transforms, caption_key=args.caption_key)
     else:
         raise ValueError("Unknown dataset target")
+
+    dataset_size = len(dataset)
+    print(f"dataset size: {dataset_size}")
+
+    s_time = time.time()
+    tot_time = 0
+    n_read = len(dataset)
+    for i, data in enumerate(dataset):
+        if i > n_read:
+            break
+        tot_time += time.time() - s_time
+        # print(f"{i}/{dataset_size}, image shape: {data.pop('image')}, {data}")
+        print(f"{i+1}/{dataset_size}, time cost: {(time.time()-s_time) * 1000} ms")
+        print(data['txt'])
+        s_time = time.time()
+    print("Total read time: ", tot_time)
