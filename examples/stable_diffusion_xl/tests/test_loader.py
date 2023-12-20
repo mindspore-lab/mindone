082379eeacdf48e05d73888e8e48f3f031f65198
Samit <285365963@qq.com>
2023-12-20 15:59:48 +0800

    add doc

--- examples/stable_diffusion_xl/tests/test_loader.py
+++ examples/stable_diffusion_xl/tests/test_loader.py
@@ -1,37 +1,42 @@
-from omegaconf import OmegaConf
 import time
-import mindspore as ms
+
 from gm.data.loader import create_loader
+from omegaconf import OmegaConf
+
+import mindspore as ms
+
 
 def test_loader():
-    data_path = 'datasets/custom'
-    shardlist_desc = 'datasets/custom/data_info.json'
+    data_path = "datasets/custom"
+    # shardlist_desc = "datasets/custom/data_info.json"
 
     rank = 0
     rank_size = 1
-    batch_size = 1
+    # batch_size = 1
 
-    config_file = 'configs/training/sd_xl_base_finetune_910b_wds.yaml'
+    config_file = "configs/training/sd_xl_base_finetune_910b_wds.yaml"
     config = OmegaConf.load(config_file)
 
     dataloader = create_loader(
-            data_path=data_path,
-            rank=rank,
-            rank_size=rank_size,
-            tokenizer=None,
-            token_nums=None,
-            **config.data,
-        )
+        data_path=data_path,
+        rank=rank,
+        rank_size=rank_size,
+        tokenizer=None,
+        token_nums=None,
+        **config.data,
+    )
 
     ms.set_context(mode=0)
 
-    num_batches = dataloader.get_dataset_size()
+    # num_batches = dataloader.get_dataset_size()
     start = time.time()
     iterator = dataloader.create_dict_iterator()
     tot = 0
-    num_steps = config.data['total_step']
+    num_steps = config.data["total_step"]
     run_steps = 0
     for i, batch in enumerate(iterator):
+        if i >= num_steps:
+            break
         dur = time.time() - start
         tot += dur
         run_steps += 1
@@ -46,5 +51,5 @@ def test_loader():
     print("Avg batch loading time: ", mean)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     test_loader()
