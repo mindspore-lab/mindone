454b6a726618ddf764ac1a55d332d4b761e07da9
samithuang <285365963@qq.com>
2023-12-22 17:59:10 +0800

    add inference

--- examples/stable_diffusion_v2/tests/st/test_overfit.py
+++ examples/stable_diffusion_v2/tests/st/test_overfit.py
@@ -1,3 +1,9 @@
+'''
+Vanilla, unet randomly initialized, loss change 1.0 ->  0.1
+LoRA, loss not stable, but 0.5 is a safe threshold 
+Dreambooth, unet randomly initialized, loss  change 2.0 -> 0.5
+'''
+
 import os
 import shutil
 import subprocess
@@ -16,7 +22,7 @@ def create_dataset(n=1):
 
     img_fn = "sunflower.png"
     img_path = __dir__ + f"/../../../videocomposer/demo_video/{img_fn}"
-    caption = '"a photo of sun flowers under blue sky, vivid color"'
+    caption = '"a photo of sunflowers under blue sky, vivid color"'
 
     shutil.copy(img_path, data_dir + f"/{img_fn}")
 
@@ -32,19 +38,26 @@ def create_dataset(n=1):
     return data_dir
 
 
-@pytest.mark.parametrize("use_lora", [True, False])  # 'db'
-def test_vanilla_lora(use_lora):
-    # model_version = "sd1.5"
-    # seed = 42
+@pytest.mark.parametrize("use_lora", [True, False])  # lora or vanilla
+@pytest.mark.parametrize("version", ['1.5', '2.0']) 
+def test_vanilla_lora(use_lora, version):
+    expected_loss = 0.1 if not use_lora else 0.5
 
     # 1. create dummpy data
     data_dir = create_dataset(1)
 
     # 2. init vae clip with pretrained weight, init UNet randomly
     # by pop out the unet parameter from sd checkpoint
-
-    model_config = __dir__ + "/../../configs/v1-train.yaml"
-    pretrained_model_path = __dir__ + "/../../models/sd_v1.5-d0ab7146.ckpt"
+    if version == '1.5':
+        model_config = __dir__ + "/../../configs/v1-train.yaml"
+        pretrained_model_path = __dir__ + "/../../models/sd_v1.5-d0ab7146.ckpt"
+        infer_config = __dir__ + "/../../configs/v1-inference.yaml"
+    elif version=='2.0':
+        model_config = __dir__ + "/../../configs/v2-train.yaml"
+        pretrained_model_path = __dir__ + "/../../models/sd_v2_base-57526ee4.ckpt"
+        infer_config = __dir__ + "/../../configs/v2-inference.yaml"
+    else:
+        raise ValueError(f'SD {version} not included in test')
 
     output_path = __dir__
     if use_lora:
@@ -57,12 +70,13 @@ def test_vanilla_lora(use_lora):
     os.makedirs(output_path, exist_ok=True)
 
     # export MS_ASCEND_CHECK_OVERFLOW_MODE="INFNAN_MODE"
-    os.environ["MS_ASCEND_CHECK_OVERFLOW_MODE"] = "INFNAN_MODE"
+    os.environ["MS_ASCEND_CHECK_OVERFLOW_MODE"] = "INFNAN_MODE" # It depends on MS version
+    epochs = 1000
 
     cmd = (
         f"python train_text_to_image.py --data_path={data_dir} --model_config={model_config} "
         f"--pretrained_model_path={pretrained_model_path} --weight_decay=0.01 --image_size=512 --dataset_sink_mode=True "
-        f"--epochs=1000 --ckpt_save_interval=1000 --start_learning_rate=0.00001 --train_batch_size=1 --init_loss_scale=65536 "
+        f"--epochs={epochs} --ckpt_save_interval={epochs} --start_learning_rate=0.00001 --train_batch_size=1 --init_loss_scale=65536 "
         f"--use_lora={use_lora} --output_path={output_path} --warmup_steps=10 --use_ema=False --clip_grad=True "
         f"--unet_initialize_random={unet_initialize_random} "
     )
@@ -72,30 +86,48 @@ def test_vanilla_lora(use_lora):
     assert ret == 0, "Training fails"
 
     # check ending loss
-
     result_log = os.path.join(output_path, "ckpt/result.log")
     df = pd.read_csv(result_log, sep="\t")  # , lineterminator='\r')
     converge_loss = np.mean(df["loss"][-100:])
 
-    expected_loss = 0.1 if not use_lora else 0.3
     print("converge_loss: ", converge_loss)
     assert converge_loss < expected_loss
+    # test inference
+    end_ckpt = os.path.join(output_path, "ckpt", f"sd-{epochs}.ckpt")
+    if not use_lora:
+        lora_ckpt_path = None
+        ckpt_path = end_ckpt
+    else:
+        lora_ckpt_path = end_ckpt
+        ckpt_path = pretrained_model_path 
+    cmd = (
+        f"python text_to_image.py --config={infer_config} --n_iter=1 --n_samples=2 "
+        f"--output_path={output_path} --lora_ckpt_path={lora_ckpt_path} --use_lora={use_lora} "
+        f"--ckpt_path={ckpt_path} --negative_prompt='sunflowers' "
+    )
+    print(f"Running command: \n{cmd}")
+    ret = subprocess.call(cmd.split(), stdout=sys.stdout, stderr=sys.stderr)
+    assert ret == 0, "run text_to_image.py fails"
 
 
-def test_db():
-    # model_version = "sd1.5"
+@pytest.mark.parametrize("version", ['1.5', '2.0']) 
+def test_db(version):
     # seed = 42
 
     # 1. create dummpy data
     data_dir = create_dataset(1)
     # data_dir = __dir__ + "/../../datasets/dog"
+    if version=='1.5':
+        train_config = __dir__ + "/config/train_config_dreambooth_v1.yaml"
+        pretrained_model_path = __dir__ + "/../../models/sd_v1.5-d0ab7146.ckpt"
+        infer_config = __dir__ + "/../../configs/v1-inference.yaml"
+    elif version=='2.0': 
+        train_config = __dir__ + "/config/train_config_dreambooth_v2.yaml"
+        pretrained_model_path = __dir__ + "/../../models/sd_v2_base-57526ee4.ckpt"
+        infer_config = __dir__ + "/../../configs/v2-inference.yaml"
+    else:
+        raise ValueError(f'SD {version} not included in test')
 
-    train_config = __dir__ + "/config/train_config_dreambooth_v1.yaml"
-    # train_config = __dir__ + "/../../configs/train/train_config_dreambooth_v1.yaml"
-    pretrained_model_path = __dir__ + "/../../models/sd_v1.5-d0ab7146.ckpt"
-    # instance_prompt = "a photo of sks sunflower"
-    # class_prompt = "a photo of a sunflower"
-    # class_data_dir = "temp_class_images/dog"
     class_data_dir = "temp_class_images/sunflower"
 
     output_path = __dir__ + "/db"
@@ -125,26 +157,35 @@ def test_db():
     ending = max(1, epochs // 10)
     converge_loss = np.mean(df["loss"][-ending:])  # 200 steps
 
-    expected_loss = 1.0
+    expected_loss = 0.5
     print("converge_loss: ", converge_loss)  #
     assert converge_loss < expected_loss
 
+    # test inference
+    end_ckpt = os.path.join(output_path, "ckpt/rank_0", f"sd-{epochs}.ckpt")
+    cmd = (
+        f"python text_to_image.py --config={infer_config} --n_iter=1 --n_samples=2 "
+        f"--output_path={output_path} --ckpt_path={end_ckpt} --negative_prompt='sunflowers' "
+    )
+    print(f"Running command: \n{cmd}")
+    ret = subprocess.call(cmd.split(), stdout=sys.stdout, stderr=sys.stderr)
+    assert ret == 0, "run text_to_image.py fails"
 
-def run_task(task="vanilla"):
+
+def run_task(task="vanilla", version='1.5'):
     if task == "vanilla":
-        test_vanilla_lora(use_lora=False)
+        test_vanilla_lora(use_lora=False, version=version)
     elif task == "lora":
-        test_vanilla_lora(use_lora=True)
+        test_vanilla_lora(use_lora=True, version=version)
     elif task == "db":
-        test_db()
+        test_db(version=version)
     else:
         raise ValueError
 
 
 if __name__ == "__main__":
-    from fire import Fire
-
-    Fire(run_task)
-
-    # test_vanilla_lora(False)
-    # test_db()
+    # from fire import Fire
+    # Fire(run_task)
+    test_vanilla_lora(False, "1.5")
+    test_vanilla_lora(True, "1.5")
+    test_db("1.5")
