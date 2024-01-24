60ab0991f037bef1c0230220fe8f28ce7a2d1ec4
Samit <285365963@qq.com>
2024-01-24 14:46:06 +0800

    rm files

--- examples/animatediff/run_mmv2_train.sh
+++
@@ -1,8 +0,0 @@
-export MS_ASCEND_CHECK_OVERFLOW_MODE="INFNAN_MODE"
-# For best performance, set to a SD1.5 checkpoint trained from image-finetuning in 256x256 resolution
-# pretrained_model_path=models/stable_diffusion/sd_v1.5-d0ab7146.ckpt
-# pretrained_model_path=tmp_outputs/sdgen_overfit/2024-01-09T18-40-59/ckpt/sd-1000.ckpt
-
-python train.py --config configs/training/mmv2_train.yaml
-
-# --pretrained_model_path=$pretrained_model_path \
