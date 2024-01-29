f0847d986aab0d202b1261c74459285bfb9d96f5
Rustam Khadipash <16683750+hadipash@users.noreply.github.com>
2024-01-29 10:58:09 +0800

    add `device_target`, remove `ParallelConfig`

--- mindone/env/init.py
+++ mindone/env/init.py
@@ -2,17 +2,22 @@ import logging
 import os
 from typing import Optional, Tuple
 
+try:
+    from typing import Literal
+except ImportError:
+    from typing_extensions import Literal  # FIXME: python 3.7
+
 import mindspore as ms
 from mindspore.communication import get_group_size, get_rank, init
 
 from ..utils.seed import set_random_seed
-from .parallel_config import ParallelConfig
 
 _logger = logging.getLogger(__name__)
 
 
 def init_train_env(
     mode: int = ms.GRAPH_MODE,
+    device_target: Literal["Ascend", "GPU"] = "Ascend",
     debug: bool = False,
     seed: int = 42,
     cache_graph: bool = False,
@@ -25,7 +30,8 @@ def init_train_env(
     Initialize MindSpore training environment.
 
     Args:
-        mode: MindSpore execution mode. Default is 0 (ms.GRAPH_MODE).
+        mode: MindSpore execution mode. Options: 0 (ms.GRAPH_MODE), 1 (ms.PYNATIVE_MODE). Default is 0 (ms.GRAPH_MODE).
+        device_target: The target execution device. Options: "Ascend", "GPU". Default is "Ascend".
         debug: Whether to enable debug mode (forces PyNative mode). Default is False.
         seed: The seed value for reproducibility. Default is 42.
         cache_graph: (Experimental) Save or load the saved computation graph to significantly reduce the graph
@@ -51,13 +57,12 @@ def init_train_env(
         device_id = int(os.getenv("DEVICE_ID"))
         ms.set_context(
             mode=mode,
-            device_target="Ascend",
+            device_target=device_target,
             device_id=device_id,
             ascend_config={"precision_mode": "allow_fp32_to_fp16"},  # Only effective on Ascend 910*
         )
         init()
         device_num = get_group_size()
-        ParallelConfig.dp = device_num
         rank_id = get_rank()
         _logger.debug(f"Device_id: {device_id}, rank_id: {rank_id}, device_num: {device_num}")
         ms.reset_auto_parallel_context()
@@ -67,7 +72,7 @@ def init_train_env(
             device_num=device_num,
         )
         var_info = ["device_num", "rank_id", "device_num / 8", "rank_id / 8"]
-        var_value = [device_num, rank_id, int(device_num / 8), int(rank_id / 8)]
+        var_value = [device_num, rank_id, device_num // 8, rank_id // 8]
         _logger.info(dict(zip(var_info, var_value)))
 
         if enable_modelarts:
@@ -79,7 +84,7 @@ def init_train_env(
         rank_id = 0
         ms.set_context(
             mode=mode,
-            device_target="Ascend",
+            device_target=device_target,
             device_id=device_id,
             ascend_config={"precision_mode": "allow_fp32_to_fp16"},  # Only effective on Ascend 910*
             pynative_synchronize=debug,
