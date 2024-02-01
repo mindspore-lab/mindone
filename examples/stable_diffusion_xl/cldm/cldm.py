61f1415a9e2a88e842f07bed818059262f0f6963
Mike Cheung <zhtmike@gmail.com>
2024-02-01 16:58:57 +0800

    enable recompute

--- examples/stable_diffusion_xl/cldm/cldm.py
+++ examples/stable_diffusion_xl/cldm/cldm.py
@@ -1,5 +1,3 @@
-import logging
-
 from gm.modules.attention import SpatialTransformer
 from gm.modules.diffusionmodules.openaimodel import (
     AttentionBlock,
@@ -10,20 +8,18 @@ from gm.modules.diffusionmodules.openaimodel import (
     UNetModel,
 )
 from gm.modules.diffusionmodules.util import conv_nd, linear, timestep_embedding, zero_module
-from gm.util import exists, instantiate_from_config
+from gm.util import default, exists, instantiate_from_config
 
-import mindspore as ms
 import mindspore.nn as nn
 import mindspore.ops as ops
-
-_logger = logging.getLogger(__name__)
 
 
 class ControlnetUnetModel(UNetModel):
     def __init__(self, control_stage_config, guess_mode=False, strength=1.0, sd_locked=True, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        if sd_locked:
+        self.sd_locked = sd_locked
+        if self.sd_locked:
             for param in self.get_parameters():
                 param.requires_grad = False
 
@@ -33,12 +29,6 @@ class ControlnetUnetModel(UNetModel):
         )
 
     def construct(self, x, timesteps=None, context=None, y=None, control=None, only_mid_control=False, **kwargs):
-        """
-        x: latent image in shape [bs, z, H//4, W//4]
-        timesteps: in shape [bs]
-        context: text embedding [bs, seq_len, f]
-        control: control signal [bs, 3, H, W]
-        """
         hs = []
 
         t_emb = timestep_embedding(timesteps, self.model_channels, repeat_only=False)
@@ -66,7 +56,6 @@ class ControlnetUnetModel(UNetModel):
         ):
             if control is not None:
                 h_c = c_celllist(h_c, emb_c, context)
-                # add encoded hint with latent image encoded projected with conv2d
                 if guided_hint is not None:
                     h_c += guided_hint
                     guided_hint = None
@@ -79,6 +68,10 @@ class ControlnetUnetModel(UNetModel):
             h_c = self.controlnet.middle_block(h_c, emb_c, context)
 
         h = self.middle_block(h, emb, context)
+
+        if self.sd_locked:
+            # may save some computation time
+            h = ops.stop_gradient(h)
 
         if control is not None:
             control_list.append(self.controlnet.middle_block_out(h_c, emb_c, context))
@@ -110,12 +103,11 @@ class ControlNet(nn.Cell):
         hint_channels,
         num_res_blocks,
         attention_resolutions,
-        dropout=0,
+        dropout=0.0,
         channel_mult=(1, 2, 4, 8),
         conv_resample=True,
         dims=2,
         num_classes=None,
-        use_fp16=False,
         num_heads=-1,
         num_head_channels=-1,
         num_heads_upsample=-1,
@@ -127,11 +119,17 @@ class ControlNet(nn.Cell):
         context_dim=None,  # custom transformer support
         n_embed=None,  # custom support for prediction of discrete ids into codebook of first stage vq model
         legacy=True,
+        disable_self_attentions=None,
         num_attention_blocks=None,
+        disable_middle_self_attn=False,
         use_linear_in_transformer=False,
+        spatial_transformer_attn_type="vanilla",
         adm_in_channels=None,
+        transformer_depth_middle=None,
+        use_recompute=False,
     ):
         super().__init__()
+        from omegaconf.listconfig import ListConfig
 
         if use_spatial_transformer:
             assert (
@@ -142,8 +140,6 @@ class ControlNet(nn.Cell):
             assert (
                 use_spatial_transformer
             ), "Fool!! You forgot to use the spatial transformer for your cross-attention conditioning..."
-            from omegaconf.listconfig import ListConfig
-
             if type(context_dim) == ListConfig:
                 context_dim = list(context_dim)
 
@@ -156,26 +152,62 @@ class ControlNet(nn.Cell):
         if num_head_channels == -1:
             assert num_heads != -1, "Either num_heads or num_head_channels has to be set"
 
-        self.dims = dims
         self.in_channels = in_channels
         self.model_channels = model_channels
-        self.num_res_blocks = num_res_blocks
+        self.hint_channels = hint_channels
+        if isinstance(transformer_depth, int):
+            transformer_depth = len(channel_mult) * [transformer_depth]
+        elif isinstance(transformer_depth, ListConfig):
+            transformer_depth = list(transformer_depth)
+        transformer_depth_middle = default(transformer_depth_middle, transformer_depth[-1])
+
+        if isinstance(num_res_blocks, int):
+            self.num_res_blocks = len(channel_mult) * [num_res_blocks]
+        else:
+            if len(num_res_blocks) != len(channel_mult):
+                raise ValueError(
+                    "provide num_res_blocks either as an int (globally constant) or "
+                    "as a list/tuple (per-level) with the same length as channel_mult"
+                )
+            self.num_res_blocks = num_res_blocks
+        # self.num_res_blocks = num_res_blocks
+        if disable_self_attentions is not None:
+            # should be a list of booleans, indicating whether to disable self-attention in TransformerBlocks or not
+            assert len(disable_self_attentions) == len(channel_mult)
+        if num_attention_blocks is not None:
+            assert len(num_attention_blocks) == len(self.num_res_blocks)
+            assert all(
+                map(
+                    lambda i: self.num_res_blocks[i] >= num_attention_blocks[i],
+                    range(len(num_attention_blocks)),
+                )
+            )
+            print(
+                f"Constructor of UNetModel received num_attention_blocks={num_attention_blocks}. "
+                f"This option has LESS priority than attention_resolutions {attention_resolutions}, "
+                f"i.e., in cases where num_attention_blocks[i] > 0 but 2**i not in attention_resolutions, "
+                f"attention will still not be set."
+            )  # todo: convert to warning
+
         self.attention_resolutions = attention_resolutions
         self.dropout = dropout
         self.channel_mult = channel_mult
         self.conv_resample = conv_resample
-        self.dtype = ms.float16 if use_fp16 else ms.float32
+        self.num_classes = num_classes
+        self.dims = dims
         self.num_heads = num_heads
         self.num_head_channels = num_head_channels
         self.num_heads_upsample = num_heads_upsample
         self.predict_codebook_ids = n_embed is not None
-        self.num_classes = num_classes
 
         time_embed_dim = model_channels * 4
+
         self.time_embed = nn.SequentialCell(
-            linear(model_channels, time_embed_dim, dtype=self.dtype),
-            nn.SiLU().to_float(self.dtype),
-            linear(time_embed_dim, time_embed_dim, dtype=self.dtype),
+            [
+                linear(model_channels, time_embed_dim),
+                nn.SiLU(),
+                linear(time_embed_dim, time_embed_dim),
+            ]
         )
 
         if self.num_classes is not None:
@@ -191,7 +223,7 @@ class ControlNet(nn.Cell):
                         nn.SequentialCell(
                             [
                                 linear(model_channels, time_embed_dim),
-                                nn.SiLU().to_float(self.dtype),
+                                nn.SiLU(),
                                 linear(time_embed_dim, time_embed_dim),
                             ]
                         ),
@@ -204,7 +236,7 @@ class ControlNet(nn.Cell):
                         nn.SequentialCell(
                             [
                                 linear(adm_in_channels, time_embed_dim),
-                                nn.SiLU().to_float(self.dtype),
+                                nn.SiLU(),
                                 linear(time_embed_dim, time_embed_dim),
                             ]
                         )
@@ -214,58 +246,46 @@ class ControlNet(nn.Cell):
                 raise ValueError()
 
         self.input_blocks = nn.CellList(
-            [
-                TimestepEmbedSequential(
-                    conv_nd(dims, in_channels, model_channels, 3, padding=1, has_bias=True, pad_mode="pad").to_float(
-                        self.dtype
-                    )
-                )
-            ]
+            [TimestepEmbedSequential(conv_nd(dims, in_channels, model_channels, 3, padding=1, pad_mode="pad"))]
         )
 
         self.zero_convs = nn.CellList([self.make_zero_conv(model_channels)])
 
         self.input_hint_block = nn.CellList(
             [
-                conv_nd(dims, hint_channels, 16, 3, padding=1, has_bias=True, pad_mode="pad").to_float(self.dtype),
-                nn.SiLU().to_float(self.dtype),
-                conv_nd(dims, 16, 16, 3, padding=1, has_bias=True, pad_mode="pad").to_float(self.dtype),
-                nn.SiLU().to_float(self.dtype),
-                conv_nd(dims, 16, 32, 3, padding=1, stride=2, has_bias=True, pad_mode="pad").to_float(self.dtype),
-                nn.SiLU().to_float(self.dtype),
-                conv_nd(dims, 32, 32, 3, padding=1, has_bias=True, pad_mode="pad").to_float(self.dtype),
-                nn.SiLU().to_float(self.dtype),
-                conv_nd(dims, 32, 96, 3, padding=1, stride=2, has_bias=True, pad_mode="pad").to_float(self.dtype),
-                nn.SiLU().to_float(self.dtype),
-                conv_nd(dims, 96, 96, 3, padding=1, has_bias=True, pad_mode="pad").to_float(self.dtype),
-                nn.SiLU().to_float(self.dtype),
-                conv_nd(dims, 96, 256, 3, padding=1, stride=2, has_bias=True, pad_mode="pad").to_float(self.dtype),
-                nn.SiLU().to_float(self.dtype),
-                zero_module(
-                    conv_nd(dims, 256, model_channels, 3, padding=1, has_bias=True, pad_mode="pad").to_float(self.dtype)
-                ),
+                conv_nd(dims, hint_channels, 16, 3, padding=1, has_bias=True, pad_mode="pad"),
+                nn.SiLU(),
+                conv_nd(dims, 16, 16, 3, padding=1, has_bias=True, pad_mode="pad"),
+                nn.SiLU(),
+                conv_nd(dims, 16, 32, 3, padding=1, stride=2, has_bias=True, pad_mode="pad"),
+                nn.SiLU(),
+                conv_nd(dims, 32, 32, 3, padding=1, has_bias=True, pad_mode="pad"),
+                nn.SiLU(),
+                conv_nd(dims, 32, 96, 3, padding=1, stride=2, has_bias=True, pad_mode="pad"),
+                nn.SiLU(),
+                conv_nd(dims, 96, 96, 3, padding=1, has_bias=True, pad_mode="pad"),
+                nn.SiLU(),
+                conv_nd(dims, 96, 256, 3, padding=1, stride=2, has_bias=True, pad_mode="pad"),
+                nn.SiLU(),
+                zero_module(conv_nd(dims, 256, model_channels, 3, padding=1, has_bias=True, pad_mode="pad")),
             ]
         )
-
         self._feature_size = model_channels
         input_block_chans = [model_channels]
         ch = model_channels
         ds = 1
-
         for level, mult in enumerate(channel_mult):
-            for _ in range(num_res_blocks):
-                layers = nn.CellList(
-                    [
-                        ResBlock(
-                            ch,
-                            time_embed_dim,
-                            self.dropout,
-                            out_channels=mult * model_channels,
-                            dims=dims,
-                            use_scale_shift_norm=use_scale_shift_norm,
-                        )
-                    ]
-                )
+            for nr in range(self.num_res_blocks[level]):
+                layers = [
+                    ResBlock(
+                        ch,
+                        time_embed_dim,
+                        dropout,
+                        out_channels=mult * model_channels,
+                        dims=dims,
+                        use_scale_shift_norm=use_scale_shift_norm,
+                    )
+                ]
                 ch = mult * model_channels
                 if ds in attention_resolutions:
                     if num_head_channels == -1:
@@ -274,9 +294,14 @@ class ControlNet(nn.Cell):
                         num_heads = ch // num_head_channels
                         dim_head = num_head_channels
                     if legacy:
+                        # num_heads = 1
                         dim_head = ch // num_heads if use_spatial_transformer else num_head_channels
+                    if exists(disable_self_attentions):
+                        disabled_sa = disable_self_attentions[level]
+                    else:
+                        disabled_sa = False
 
-                    if not exists(num_attention_blocks) or _ < num_attention_blocks[level]:
+                    if not exists(num_attention_blocks) or nr < num_attention_blocks[level]:
                         layers.append(
                             AttentionBlock(
                                 ch,
@@ -291,8 +316,9 @@ class ControlNet(nn.Cell):
                                 dim_head,
                                 depth=transformer_depth[level],
                                 context_dim=context_dim,
-                                dropout=self.dropout,
+                                disable_self_attn=disabled_sa,
                                 use_linear=use_linear_in_transformer,
+                                attn_type=spatial_transformer_attn_type,
                             )
                         )
                 self.input_blocks.append(TimestepEmbedSequential(*layers))
@@ -306,7 +332,7 @@ class ControlNet(nn.Cell):
                         ResBlock(
                             ch,
                             time_embed_dim,
-                            self.dropout,
+                            dropout,
                             out_channels=out_ch,
                             dims=dims,
                             use_scale_shift_norm=use_scale_shift_norm,
@@ -328,19 +354,13 @@ class ControlNet(nn.Cell):
             num_heads = ch // num_head_channels
             dim_head = num_head_channels
         if legacy:
+            # num_heads = 1
             dim_head = ch // num_heads if use_spatial_transformer else num_head_channels
-
-        _logger.debug(
-            "Attention: output_channels={}, num_heads={}, num_head_channels:{}, dim_head={}".format(
-                ch, num_heads, num_head_channels, dim_head
-            )
-        )
-
         self.middle_block = TimestepEmbedSequential(
             ResBlock(
                 ch,
                 time_embed_dim,
-                self.dropout,
+                dropout,
                 dims=dims,
                 use_scale_shift_norm=use_scale_shift_norm,
             ),
@@ -351,19 +371,20 @@ class ControlNet(nn.Cell):
                 use_new_attention_order=use_new_attention_order,
             )
             if not use_spatial_transformer
-            else SpatialTransformer(
+            else SpatialTransformer(  # always uses a self-attn
                 ch,
                 num_heads,
                 dim_head,
-                depth=transformer_depth[level],
+                depth=transformer_depth_middle,
                 context_dim=context_dim,
-                dropout=self.dropout,
+                disable_self_attn=disable_middle_self_attn,
                 use_linear=use_linear_in_transformer,
+                attn_type=spatial_transformer_attn_type,
             ),
             ResBlock(
                 ch,
                 time_embed_dim,
-                self.dropout,
+                dropout,
                 dims=dims,
                 use_scale_shift_norm=use_scale_shift_norm,
             ),
@@ -371,9 +392,51 @@ class ControlNet(nn.Cell):
         self.middle_block_out = self.make_zero_conv(ch)
         self._feature_size += ch
 
+        if use_recompute:
+            self.recompute_strategy_v1()
+
     def make_zero_conv(self, channels):
         return zero_module(
-            TimestepEmbedSequential(
-                conv_nd(self.dims, channels, channels, 1, padding=0, has_bias=True, pad_mode="pad").to_float(self.dtype)
-            )
+            TimestepEmbedSequential(conv_nd(self.dims, channels, channels, 1, padding=0, has_bias=True, pad_mode="pad"))
         )
+
+    def recompute_strategy_v1(self):
+        # embed
+        self.time_embed.recompute()
+        self.label_emb.recompute()
+
+        # input blocks
+        self.input_blocks[4][0].recompute()  # 4
+        self.input_blocks[5][0].recompute()  # 5
+        self.input_blocks[7][0].recompute()  # 7
+        self.input_blocks[8][0].recompute()  # 8
+
+        # middle block
+        self.middle_block[0].recompute()
+        self.middle_block[1].recompute()
+
+        print("Turn on recompute with StrategyV1.")
+
+    def construct(self, x, hint, timesteps, context, **kwargs):
+        t_emb = timestep_embedding(timesteps, self.model_channels, repeat_only=False)
+        emb = self.time_embed(t_emb)
+        guided_hint = hint
+        for cell in self.input_hint_block:
+            guided_hint = cell(guided_hint)
+
+        outs = []
+
+        h = x
+        for celllist, zero_conv in zip(self.input_blocks, self.zero_convs):
+            for cell in celllist:
+                h = cell(h, emb, context)
+            if guided_hint is not None:
+                h += guided_hint
+                guided_hint = None
+            outs.append(zero_conv(h, emb, context))
+        for module in self.middle_block:
+            h = module(h, emb, context)
+
+        outs.append(self.middle_block_out(h, emb, context))
+
+        return outs
