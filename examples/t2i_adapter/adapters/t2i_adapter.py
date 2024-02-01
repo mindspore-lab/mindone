e2e8188057e5c9a792dac5004d6392303bd43d43
Rustam Khadipash <16683750+hadipash@users.noreply.github.com>
2024-02-01 11:24:58 +0800

    fixes to T2I Adapter (#330)
    * T2I Adapter fix
    
    * fix linting

--- examples/t2i_adapter/adapters/t2i_adapter.py
+++ examples/t2i_adapter/adapters/t2i_adapter.py
@@ -328,9 +328,7 @@ class CombinedAdapter(nn.Cell):
                     feature_map[j] + feat * self._weights[i] for j, feat in enumerate(self._adapters[i](conds[i]))
                 ]
 
-            # TODO: in the original implementation adapter features are applied to both
-            #  unconditional and conditional guidance
-            feature_map = [ops.cat((feat, feat)).astype(self._out_cast) for feat in feature_map]
+            feature_map = [feat.astype(self._out_cast) for feat in feature_map]
 
         if self._style_ids:
             feature_seq = [self._adapters[i](conds[i]) * self._weights[i] for i in self._style_ids]
