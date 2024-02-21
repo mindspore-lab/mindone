1048d0a75ab4bfbcbdbc2fde280e3f23a640c423
townwish4git <tanweixi1@huawei.com>
2024-02-21 15:22:41 +0800

    SDXL: record crop_coords_top_left in `CenterCrop`

--- examples/stable_diffusion_xl/gm/data/dataset.py
+++ examples/stable_diffusion_xl/gm/data/dataset.py
@@ -202,7 +202,7 @@ class Text2ImageDataset:
             h //= 2
             w //= 2
             need_shrink = True
-            
+
         image = image.resize(size=(w, h)) if need_shrink else image
 
         return image
