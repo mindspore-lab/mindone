70c3fc5f6abcbdbac6161ec273ac2ec87c5776e3
Yang Chen <isyangc2@gmail.com>
2024-02-23 15:20:26 +0800

    convert origin sdxl to hf bug fix

--- examples/stable_diffusion_xl/tools/model_conversion/convert_original_sdxl_to_diffusers.py
+++ examples/stable_diffusion_xl/tools/model_conversion/convert_original_sdxl_to_diffusers.py
@@ -1,12 +1,13 @@
 import argparse
+import os.path as osp
 
 import torch
-from safetensors.torch import  save_file
+from safetensors.torch import load_file, save_file
 
 
-def get_map(key_file,value_file):
+def get_map(key_file, value_file):
     key_map = {}
-    with open(key_file, 'r') as key_file, open(value_file, 'r') as value_file:
+    with open(key_file, "r") as key_file, open(value_file, "r") as value_file:
         keys = key_file.readlines()
         values = value_file.readlines()
 
@@ -17,33 +18,43 @@ def get_map(key_file,value_file):
     return key_map
 
 
-
-def convert(state_dict,module_map):
+def convert(state_dict, module_map):
     mapping = module_map
-    new_state_dict = {hf_name: state_dict[sd_name] for sd_name,hf_name in mapping.items()}
+    new_state_dict = {}
+    for sd_name, hf_name in mapping.items():
+        if sd_name in state_dict:
+            new_state_dict[hf_name] = state_dict[sd_name]
     return new_state_dict
-
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--model_path", default=None, type=str, required=True, help="Path to the model to convert.")
-    parser.add_argument("--checkpoint_path", default=None, type=str, required=True, help="Path to the output model.")
+    parser.add_argument("--output_path", default=None, type=str, required=True, help="Path to the output model.")
     parser.add_argument("--use_safetensors", action="store_true", help="Save weights use safetensors, default is ckpt.")
     parser.add_argument("--half", action="store_true", help="Save weights in half precision.")
     args = parser.parse_args()
 
-    key_file = 'torch_key_unet.yaml'
-    value_file = 'hf_key_unet.yaml'
-    unet_map = get_map('torch_key_unet.yaml', 'hf_key_unet.yaml')
-    vae_map = get_map('torch_key_vae.yaml', 'hf_key_vae.yaml')
-    text1_map = get_map('torch_key_text1.yaml', 'hf_key_text1.yaml')
-    text2_map = get_map('torch_key_text2.yaml', 'hf_key_text2.yaml')
+    key_file = "diffuser_yaml/torch_key_unet.yaml"
+    value_file = "diffuser_yaml/hf_key_unet.yaml"
+    unet_map = get_map("diffuser_yaml/torch_key_unet.yaml", "diffuser_yaml/hf_key_unet.yaml")
+    vae_map = get_map("diffuser_yaml/torch_key_vae.yaml", "diffuser_yaml/hf_key_vae.yaml")
+    text1_map = get_map("diffuser_yaml/torch_key_text1.yaml", "diffuser_yaml/hf_key_text1.yaml")
+    text2_map = get_map("diffuser_yaml/torch_key_text2.yaml", "diffuser_yaml/hf_key_text2.yaml")
 
-    unet = convert(args.model_path,unet_map)
-    vae = convert(args.model_path,vae_map)
-    text1 = convert(args.model_path,text1_map)
-    text2 = convert(args.model_path,text2_map)
+    if osp.exists(args.model_path):
+        if args.model_path.endswith(".pth"):
+            state_dict = torch.load(args.model_path, map_location="cpu")
+        else:
+            state_dict = load_file(args.model_path, device="cpu")
+        print("load model from model_path ", args.model_path)
+    else:
+        print("model_path is not valid, please double-check it!")
+
+    unet = convert(state_dict, unet_map)
+    vae = convert(state_dict, vae_map)
+    text1 = convert(state_dict, text1_map)
+    text2 = convert(state_dict, text2_map)
 
     if args.half:
         unet = {k: v.half() for k, v in unet.items()}
@@ -52,12 +63,12 @@ if __name__ == "__main__":
         text2 = {k: v.half() for k, v in text2.items()}
 
     if args.use_safetensors:
-        save_file(unet, args.output_path+"/unet.safetensors")
+        save_file(unet, args.output_path + "/unet.safetensors")
         save_file(vae, args.output_path + "/vae.safetensors")
         save_file(text1, args.output_path + "/text1.safetensors")
         save_file(text2, args.output_path + "/text2.safetensors")
     else:
-        torch.save({"state_dict": unet}, args.output_path+"/unet.pth")
+        torch.save({"state_dict": unet}, args.output_path + "/unet.pth")
         torch.save({"state_dict": vae}, args.output_path + "/vae.pth")
         torch.save({"state_dict": text1}, args.output_path + "/text1.pth")
         torch.save({"state_dict": text2}, args.output_path + "/text2.pth")
