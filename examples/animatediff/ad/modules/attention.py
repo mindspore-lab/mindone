improve mixed precision and fix FA. Add prompt dropout for training etc

examples/animatediff/ad/modules/attention.py
@@ -189,8 +189,8 @@ class CrossAttention(nn.Cell):
         head_dim = q.shape[-1] // self.heads
 
         if (
-            self.enable_flash_attention and q_n % 16 == 0 and k_n % 16 == 0 and head_dim <= 256
-        ):  # TODO: why restrict head_dim?
+            self.enable_flash_attention and q_n % 16 == 0 and k_n % 16 == 0 and head_dim <= 128 
+        ):  # FIXME: now restrict head_dim to 128 to avoid 160 bug. revert to 256 once FA bug is fixed.
             # reshape qkv shape ((b n h*d) -> (b h n d))and mask dtype for FA input format
             q = q.view(q_b, q_n, h, -1).transpose(0, 2, 1, 3)
             k = k.view(k_b, k_n, h, -1).transpose(0, 2, 1, 3)
@@ -307,9 +307,11 @@ class Attention(nn.Cell):
             mask = mask.repeat(self.heads, axis=0)
             mask = ops.expand_dims(mask, axis=1)
             sim.masked_fill(mask, max_neg_value)
-
+        
+        # TODO: testing use fp16 instead
         # use fp32 for exponential inside
-        attn = self.softmax(sim.astype(ms.float32)).astype(v.dtype)
+        # attn = self.softmax(sim.astype(ms.float32)).astype(v.dtype)
+        attn = self.softmax(sim)
 
         out = ops.matmul(attn, v)
 

examples/animatediff/ad/modules/diffusionmodules/unet3d.py
@@ -115,6 +115,7 @@ class UNet3DModel(nn.Cell):
         unet_chunk_size=2,
         adm_in_channels=None,
         use_recompute=False,
+        recompute_strategy='down_up',
         # Additional
         use_inflated_groupnorm=True,  # diff, default is to use in mm-v2, which is more reasonable.
         use_motion_module=False,
@@ -500,13 +501,22 @@ class UNet3DModel(nn.Cell):
 
         # TODO: optimize where to recompute & fix bug on cell list.
         if use_recompute:
-            print("D--: recompute: ", use_recompute)
-            for iblock in self.input_blocks:
-                self.recompute(iblock)
-                # mblock.recompute()
-            for oblock in self.output_blocks:
-                self.recompute(oblock)
-                # oblock.recompute()
+            # print("D--: recompute strategy: ", recompute_strategy)
+            if recompute_strategy == 'down_mm':
+                for iblock in self.input_blocks:
+                    for cell in iblock:
+                        if isinstance(cell, VanillaTemporalModule):
+                            self.recompute(cell)
+            elif recompute_strategy == 'up_mm':
+                for oblock in self.output_blocks:
+                    for cell in oblock:
+                        if isinstance(cell, VanillaTemporalModule):
+                            self.recompute(cell)
+            else: # up and down
+                for iblock in self.input_blocks:
+                    self.recompute(iblock)
+                for oblock in self.output_blocks:
+                    self.recompute(oblock)
 
     def recompute(self, b):
         if not b._has_config_recompute:

examples/animatediff/ad/modules/diffusionmodules/util.py
@@ -104,8 +104,10 @@ class SiLU(nn.Cell):
         self.sigmoid = ops.Sigmoid()
 
     def construct(self, x):
+        # TODO: test use fp16 instead
         # force sigmoid to use fp32
-        return x * self.sigmoid(x.astype(ms.float32)).astype(x.dtype)
+        # return x * self.sigmoid(x.astype(ms.float32)).astype(x.dtype)
+        return x * self.sigmoid(x)
 
 
 class GroupNorm32(nn.GroupNorm):