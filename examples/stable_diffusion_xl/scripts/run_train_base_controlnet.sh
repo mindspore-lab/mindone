17f430345f44636915f612b9905f136ed816a2ae
HaoyangLI <417493727@qq.com>
2024-02-27 11:21:08 +0800

    Update run_train_base_controlnet.sh

--- examples/stable_diffusion_xl/scripts/run_train_base_controlnet.sh
+++ examples/stable_diffusion_xl/scripts/run_train_base_controlnet.sh
@@ -1,4 +1,4 @@
-nohup mpirun -n 8 --allow-run-as-root python train_controlnet.py \
+mpirun -n 8 --allow-run-as-root python train_controlnet.py \
     --data_path DATA_PATH \
     --weight PATH TO/sd_xl_base_1.0_ms_controlnet_init.ckpt \
     --config configs/training/sd_xl_base_finetune_controlnet_910b.yaml \
@@ -7,4 +7,3 @@ nohup mpirun -n 8 --allow-run-as-root python train_controlnet.py \
     --group_lr_scaler 10.0 \
     --save_ckpt_interval 10000 \
     --max_num_ckpt 5 \
-    > train.log 2>&1 &
