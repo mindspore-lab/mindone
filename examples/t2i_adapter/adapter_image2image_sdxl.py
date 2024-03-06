ecf425b7f2ed92f75709967b2a65f686a0125476
Rustam Khadipash <16683750+hadipash@users.noreply.github.com>
2024-03-06 10:31:04 +0800

    fix linting

--- examples/t2i_adapter/adapter_image2image_sdxl.py
+++ examples/t2i_adapter/adapter_image2image_sdxl.py
@@ -12,8 +12,6 @@ from omegaconf import OmegaConf
 from PIL import Image
 from t2i_utils import read_images
 
-from mindspore import ops
-
 sys.path.append("../../")  # FIXME: remove in future when mindone is ready for install
 from mindone.env import init_train_env
 from mindone.utils import set_logger
