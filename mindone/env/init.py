4904a56313ed1a7cc2506293c8e085332e937d52
Rustam Khadipash <16683750+hadipash@users.noreply.github.com>
2024-03-07 10:08:44 +0800

    extract environment initialization (#315)
    * extract environment initialization
    
    * add `device_target`, remove `ParallelConfig`
    
    * Extract `ascend_config` argument
    
    * fix linting

--- mindone/env/init.py
+++ mindone/env/init.py
@@ -2,9 +2,10 @@ import logging
 import os
 from typing import Optional, Tuple
 
-from ldm.data.dataset_dist import split_and_sync_data
-from ldm.modules.train.parallel_config import ParallelConfig
-from ldm.modules.train.tools import set_random_seed
+try:
+    from typing import Literal
+except ImportError:
+    from typing_extensions import Literal  # FIXME: python 3.7
 
 import mindspore as ms
 from mindspore.communication import get_group_size, get_rank, init
@@ -12,24 +13,33 @@ from mindspore.communication import get_group_size, get_rank, init
 _logger = logging.getLogger(__name__)
 
 
-def init_env(
+def init_train_env(
     mode: int = ms.GRAPH_MODE,
+    device_target: Literal["Ascend", "GPU"] = "Ascend",
     debug: bool = False,
     seed: int = 42,
+    cache_graph: bool = False,
+    cache_path: str = "./cache",
     distributed: bool = False,
-    device_target: Optional[str] = "Ascend",
+    ascend_config: Optional[dict] = None,
     enable_modelarts: bool = False,
     num_workers: int = 1,
     json_data_path: Optional[str] = None,
 ) -> Tuple[int, int, int]:
     """
-    Initialize MindSpore environment.
+    Initialize MindSpore training environment.
 
     Args:
-        mode: MindSpore execution mode. Default is 0 (ms.GRAPH_MODE).
+        mode: MindSpore execution mode. Options: 0 (ms.GRAPH_MODE), 1 (ms.PYNATIVE_MODE). Default is 0 (ms.GRAPH_MODE).
+        device_target: The target execution device. Options: "Ascend", "GPU". Default is "Ascend".
         debug: Whether to enable debug mode (forces PyNative mode). Default is False.
         seed: The seed value for reproducibility. Default is 42.
+        cache_graph: (Experimental) Save or load the saved computation graph to significantly reduce the graph
+                     compilation time during the first epoch. Use this feature with great caution, as any changes to the
+                     Python scripts may cause inconsistencies in the results.
+        cache_path: The path to save or load the saved computation graph.
         distributed: Whether to enable distributed training. Default is False.
+        ascend_config: Parameters specific to the Ascend hardware platform.
         enable_modelarts: Whether to enable modelarts (OpenI) support. Default is False.
         num_workers: The number of modelarts workers. Used only when `enable_modelarts` is True. Default is 1.
         json_data_path: The path of num_samples.json containing a dictionary with 64 parts. Each part is a large
@@ -39,7 +49,7 @@ def init_env(
     Returns:
         A tuple containing the device ID, rank ID and number of devices.
     """
-    set_random_seed(seed)
+    ms.set_seed(seed)
 
     if debug and mode == ms.GRAPH_MODE:  # force PyNative mode when debugging
         _logger.warning("Debug mode is on, switching execution mode to PyNative.")
@@ -47,15 +57,9 @@ def init_env(
 
     if distributed:
         device_id = int(os.getenv("DEVICE_ID"))
-        ms.set_context(
-            mode=mode,
-            device_target=device_target,
-            device_id=device_id,
-            ascend_config={"precision_mode": "allow_fp32_to_fp16"},  # Only effective on Ascend 910*
-        )
+        ms.set_context(mode=mode, device_target=device_target, device_id=device_id, ascend_config=ascend_config or {})
         init()
         device_num = get_group_size()
-        ParallelConfig.dp = device_num
         rank_id = get_rank()
         _logger.debug(f"Device_id: {device_id}, rank_id: {rank_id}, device_num: {device_num}")
         ms.reset_auto_parallel_context()
@@ -65,11 +69,12 @@ def init_env(
             device_num=device_num,
         )
         var_info = ["device_num", "rank_id", "device_num / 8", "rank_id / 8"]
-        var_value = [device_num, rank_id, int(device_num / 8), int(rank_id / 8)]
+        var_value = [device_num, rank_id, device_num // 8, rank_id // 8]
         _logger.info(dict(zip(var_info, var_value)))
 
         if enable_modelarts:
-            split_and_sync_data(json_data_path, num_workers, device_num, rank_id)
+            # split_and_sync_data(json_data_path, num_workers, device_num, rank_id)
+            raise NotImplementedError("ModelArts is not supported yet.")
     else:
         device_num = 1
         device_id = int(os.getenv("DEVICE_ID", 0))
@@ -78,8 +83,10 @@ def init_env(
             mode=mode,
             device_target=device_target,
             device_id=device_id,
-            ascend_config={"precision_mode": "allow_fp32_to_fp16"},  # Only effective on Ascend 910*
+            ascend_config=ascend_config or {},
             pynative_synchronize=debug,
+            enable_compile_cache=cache_graph,
+            compile_cache_path=cache_path,
         )
 
     return device_id, rank_id, device_num
