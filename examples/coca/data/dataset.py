673e3bca553fdea259419839c78bfcdcae3cbc89
songyuanwei <song.yuanwei@huawei.com>
2024-03-14 10:18:14 +0800

    update

--- examples/coca/data/dataset.py
+++ examples/coca/data/dataset.py
@@ -4,8 +4,10 @@ from mindspore.dataset.vision import CenterCrop, Inter, Normalize, Resize, ToTen
 MEAN = (0.48145466, 0.4578275, 0.40821073)
 STD = (0.26862954, 0.26130258, 0.27577711)
 
+
 def _convert_to_rgb(image):
-    return image.convert('RGB')
+    return image.convert("RGB")
+
 
 def create_transforms(image_size=224, interpolation="bicubic"):
     mapping = {"bilinear": Inter.BILINEAR, "bicubic": Inter.BICUBIC}
@@ -17,7 +19,7 @@ def create_transforms(image_size=224, interpolation="bicubic"):
                 CenterCrop(image_size),
                 _convert_to_rgb,
                 ToTensor(),
-                Normalize(mean=MEAN, std=STD, is_hwc=False)
+                Normalize(mean=MEAN, std=STD, is_hwc=False),
             ]
         )
-    return  transforms
+    return transforms
