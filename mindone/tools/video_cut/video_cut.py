77b908cd8269c8ab5b08c1e35575f411f3dfd215
songyuanwei <song.yuanwei@huawei.com>
2024-03-21 16:49:05 +0800

    add mutil processing

--- mindone/tools/video_cut/video_cut.py
+++ mindone/tools/video_cut/video_cut.py
@@ -1,27 +1,13 @@
 import argparse
 import os
+from multiprocessing import Pool
 
 import av
 from scenedetect import SceneManager, open_video
 from scenedetect.detectors import AdaptiveDetector, ContentDetector, ThresholdDetector
 from scenedetect.video_splitter import split_video_ffmpeg
 from tqdm import tqdm
-
-
-def get_video_path(paths):
-    if os.path.isdir(paths) and os.path.exists(paths):
-        paths = [
-            os.path.join(root, file)
-            for root, _, file_list in os.walk(os.path.join(paths))
-            for file in file_list
-            if file.endswith(".mp4")
-        ]
-        paths.sort()
-        paths = paths
-    else:
-        paths = [paths]
-
-    return paths
+from utils import check_video_integrity, get_video_path
 
 
 class KeyFrameExtractUtils:
@@ -61,9 +47,11 @@ class SceneCutUtils:
             - ThresholdDetector: Detects transitions below a set pixel intensity (cuts or fades to black).
     """
 
-    def __init__(self, video_data_path, Detectortype="ContentDetector"):
+    def __init__(self, video_data_path, Detectortype="ContentDetector", save_dir=None, num_processing=1):
         self.video_data_path = video_data_path
         self.Detectortype = Detectortype
+        self.num_processing = num_processing
+        self.save_dir = save_dir
         if self.Detectortype == "ContentDetector":
             self.Detector = ContentDetector()
         elif self.Detectortype == "AdaptiveDetector":
@@ -73,10 +61,35 @@ class SceneCutUtils:
         else:
             raise ValueError(f"Unsupported Detector type: {self.Detectortype}.")
 
-    def save_scene(self):
-        vdieo_dir = get_video_path(self.video_data_path)
-        for video_path in vdieo_dir:
+    def split_list(self, lst, n):
+        length = len(lst) // n + (1 if len(lst) % n != 0 else 0)
+        result = []
+        for i in range(n):
+            start = i * length
+            end = start + length
+            sublist = lst[start:end]
+            result.append(sublist)
+
+        return result
+
+    def scenes_detect(self):
+        video_dir = get_video_path(self.video_data_path)
+        if self.save_dir is None:
+            self.save_dir = os.path.join(os.path.splitext(self.video_data_path)[0])
+            if not os.path.exists(self.save_dir):
+                os.makedirs(self.save_dir)
+        if self.num_processing > 1:
+            video_list = self.split_list(video_dir, self.num_processing)
+            with Pool(processes=self.num_processing) as pool:
+                pool.starmap(self.save_scene, [(video, self.save_dir) for video in video_list])
+        else:
+            self.save_scene(video_dir, self.save_dir)
+
+    def save_scene(self, video_dir, save_dir):
+        for video_path in video_dir:
             # Create our video & scene managers, then add the detector.
+            if not check_video_integrity(video_path):
+                continue
             video_manager = open_video(video_path)
             scene_manager = SceneManager()
             scene_manager.add_detector(self.Detector)
@@ -86,7 +99,7 @@ class SceneCutUtils:
             # save scene
             scene_list = scene_manager.get_scene_list()
             if len(scene_list):
-                split_video_ffmpeg(video_path, scene_list, show_progress=False)
+                split_video_ffmpeg(video_path, scene_list, output_dir=save_dir, show_progress=False)
                 for index, scene in enumerate(scene_list):
                     print(
                         f"Scene {index + 1}: Start {scene[0].get_timecode()} / Frame {scene[0].get_frames()}",
@@ -100,10 +113,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--video_data_path", type=str, default=None, help="path to video data." "Default: None")
     parser.add_argument(
-        "--keyframe_save_dir",
+        "--save_dir",
         type=str,
         default=None,
-        help="path for save keyframe images. if None, keyframes will save in video_data_path." "Default: None",
+        help="path for save keyframe images or sences. if None, keyframes or sences will save in video_data_path, ."
+        "Default: None",
     )
     parser.add_argument(
         "--task",
@@ -119,13 +133,19 @@ if __name__ == "__main__":
         choices=["ContentDetector", "AdaptiveDetector", "ThresholdDetector"],
         help="scene detection algorithms type." "Default: ContentDetector",
     )
+    parser.add_argument(
+        "--num_processing",
+        type=int,
+        default=1,
+        help="multiple processesr",
+    )
     args = parser.parse_args()
 
     if args.task == "keyframe":
-        keyframe = KeyFrameExtractUtils(args.video_data_path, args.keyframe_save_dir)
+        keyframe = KeyFrameExtractUtils(args.video_data_path, args.save_dir)
         keyframe.save_keyframe()
     elif args.task == "scene":
-        cut_scene = SceneCutUtils(args.video_data_path, args.Detectortype)
-        cut_scene.save_scene()
+        cut_scene = SceneCutUtils(args.video_data_path, args.Detectortype, args.save_dir, args.num_processing)
+        cut_scene.scenes_detect()
     else:
         raise ValueError(f"Unsupported tasks: {args.task}.")
