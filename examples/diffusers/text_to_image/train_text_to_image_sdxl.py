169af2d9827f0be01783150b9af41aa0f195cd4d
townwish4git <tanweixi1@huawei.com>
2024-04-18 09:59:29 +0800

    fix(sdxl): ckpt filename extension

--- examples/diffusers/text_to_image/train_text_to_image_sdxl.py
+++ examples/diffusers/text_to_image/train_text_to_image_sdxl.py
@@ -961,7 +961,7 @@ def main():
             # Get the most recent checkpoint
             dirs = os.listdir(args.output_dir)
             dirs = [d for d in dirs if d.startswith("checkpoint")]
-            dirs = sorted(dirs, key=lambda x: int(x.split("-")[1]))
+            dirs = sorted(dirs, key=lambda x: int(x.split("-")[1].split(".")[0]))
             path = dirs[-1] if len(dirs) > 0 else None
 
         if path is None:
@@ -974,7 +974,7 @@ def main():
                 logger.info(f"Resuming from checkpoint {path}")
             state_dict = ms.load_checkpoint(os.path.join(args.output_dir, path))
             ms.load_param_into_net(unet, state_dict)  # todo: what about optimizer and scaler?
-            global_step = int(path.split("-")[1])
+            global_step = int(path.split("-")[1].split(".")[0])
 
             initial_global_step = global_step
             first_epoch = global_step // num_update_steps_per_epoch
@@ -1020,7 +1020,7 @@ def main():
                     if args.checkpoints_total_limit is not None:
                         checkpoints = os.listdir(args.output_dir)
                         checkpoints = [d for d in checkpoints if d.startswith("checkpoint")]
-                        checkpoints = sorted(checkpoints, key=lambda x: int(x.split("-")[1]))
+                        checkpoints = sorted(checkpoints, key=lambda x: int(x.split("-")[1].split(".")[0]))
 
                         # before we save the new checkpoint, we need to have at _most_ `checkpoints_total_limit - 1` checkpoints
                         if len(checkpoints) >= args.checkpoints_total_limit:
