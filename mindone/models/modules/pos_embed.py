76dc3b0099b24399d57e11d2bdbae819251f06d1
fzilan <33061146+Fzilan@users.noreply.github.com>
2024-04-26 14:11:22 +0800

    feat: add mmdit implementation based on sd3 paper (#450)
    * add mmdit
    
    * fix typos
    
    * mmdit and RoPE fix typos
    
    * construct fun combines "with" and "without" cfg
    
    ---------
    
    Co-authored-by: Fzilan <Fzilan@users.noreply.github.com>

--- mindone/models/modules/pos_embed.py
+++ mindone/models/modules/pos_embed.py
@@ -2,7 +2,17 @@ from typing import Optional
 
 import numpy as np
 
-__all__ = ["get_1d_sincos_pos_embed", "get_2d_sincos_pos_embed", "precompute_freqs_cis_2d"]
+import mindspore as ms
+from mindspore import Tensor, ops
+
+__all__ = [
+    "get_1d_sincos_pos_embed",
+    "get_2d_sincos_pos_embed",
+    "precompute_freqs_cis_2d",
+    "apply_rotary_pos_emb",
+    "apply_2d_rotary_pos",
+    "create_sinusoidal_positions",
+]
 
 
 def get_2d_sincos_pos_embed(embed_dim: int, nh: int, nw: Optional[int] = None) -> np.ndarray:
@@ -120,3 +130,38 @@ def _precompute_freqs_cis_1d_from_grid(
     b = np.sin(freqs)  # represent for a + ib
     freqs_cis = np.stack([a, b], axis=-1)
     return freqs_cis
+
+
+def create_sinusoidal_positions(num_pos: int, dim: int) -> Tensor:
+    inv_freq = 1.0 / (10000 ** (np.arange(0, dim, 2) / dim))
+    sinusoid_inp = np.einsum("i , j -> i j", np.arange(num_pos), inv_freq)
+    sinusoid_inp = ops.cat((Tensor(sinusoid_inp, dtype=ms.float32), Tensor(sinusoid_inp, dtype=ms.float32)), axis=-1)
+    return ops.cat((ops.sin(sinusoid_inp), ops.cos(sinusoid_inp)), axis=1)
+
+
+def rotate_every_two(x: Tensor):
+    x1 = x[:, :, :, ::2]
+    x2 = x[:, :, :, 1::2]
+    x = ops.stack((-x2, x1), axis=-1)
+    return x.flatten(order="C", start_dim=-2, end_dim=-1)  # in einsum notation: rearrange(x, '... d j -> ... (d j)')
+
+
+def apply_rotary_pos_emb(tensor: Tensor, sin: Tensor, cos: Tensor) -> Tensor:
+    sin = sin.unsqueeze(0).unsqueeze(1)
+    cos = cos.unsqueeze(0).unsqueeze(1)
+    return (tensor * cos) + (rotate_every_two(tensor) * sin)
+
+
+def apply_2d_rotary_pos(q: Tensor, k: Tensor, freqs_cis: Tensor) -> Tensor:
+    sincos_h, sincos_w = freqs_cis
+    sin_h, cos_h = ops.split(sincos_h, sincos_h.shape[-1] // 2, axis=-1)
+    sin_w, cos_w = ops.split(sincos_w, sincos_w.shape[-1] // 2, axis=-1)
+    q1, q2 = q.chunk(2, axis=-1)
+    k1, k2 = k.chunk(2, axis=-1)
+    q1 = apply_rotary_pos_emb(q1, sin_h, cos_h)
+    k1 = apply_rotary_pos_emb(k1, sin_h, cos_h)
+    q2 = apply_rotary_pos_emb(q2, sin_w, cos_w)
+    k2 = apply_rotary_pos_emb(k2, sin_w, cos_w)
+    q = ops.concat([q1, q2], axis=-1)
+    k = ops.concat([k1, k2], axis=-1)
+    return q, k
