030243cc58fcd93a54f05e6bcb6630ec3dfb883b
wtomin <dengdidanwtomin@gmail.com>
2024-04-28 15:00:25 +0800

    refine logging

--- examples/opensora_pku/examples/rec_imvi_vae.py
+++ examples/opensora_pku/examples/rec_imvi_vae.py
@@ -184,7 +184,10 @@ def main(args):
         original_rgb = x_vae.asnumpy().transpose(0, 2, 3, 4, 1)  # (b c t h w) -> (b t h w c)
         save_video_data = np.concatenate([original_rgb, save_video_data], axis=3) if args.grid else save_video_data
         save_videos(save_video_data, save_fp, loop=0)
-    logger.info(f"Save reconstructed data to {save_fp}")
+    if args.grid:
+        logger.info(f"Save original vs. reconstructed data to {save_fp}")
+    else:
+        logger.info(f"Save reconstructed data to {save_fp}")
 
 
 if __name__ == "__main__":
