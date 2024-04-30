20d019151acc6c1e7d7037c8c0ab1e0ac8c5b485
Didan Deng <dengdidanwtomin@gmail.com>
2024-04-30 15:36:28 +0800

    eval scripts

--- examples/opensora_pku/opensora/models/ae/videobase/modeling_videobase.py
+++ examples/opensora_pku/opensora/models/ae/videobase/modeling_videobase.py
@@ -11,17 +11,6 @@ class VideoBaseAE(ModelMixin, ConfigMixin):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
-    # @classmethod
-    # def load_from_checkpoint(cls, model_path):
-    #     with open(os.path.join(model_path, "config.json"), "r") as file:
-    #         config = json.load(file)
-    #     state_dict = torch.load(os.path.join(model_path, "pytorch_model.bin"), map_location="cpu")
-    #     if 'state_dict' in state_dict:
-    #         state_dict = state_dict['state_dict']
-    #     model = cls(config=cls.CONFIGURATION_CLS(**config))
-    #     model.load_state_dict(state_dict)
-    #     return model
-
     @classmethod
     def download_and_load_model(cls, model_name, cache_dir=None):
         pass
