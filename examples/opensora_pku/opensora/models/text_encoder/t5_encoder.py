b317848db837d484d0e6167777e3a16248a0db09
Didan Deng <dengdidanwtomin@gmail.com>
2024-05-06 16:56:49 +0800

    fix error

--- examples/opensora_pku/opensora/models/text_encoder/t5_encoder.py
+++ examples/opensora_pku/opensora/models/text_encoder/t5_encoder.py
@@ -600,7 +600,7 @@ class T5Stack(T5PreTrainedModel):
 
         # We can provide a self-attention mask of dimensions [batch_size, from_seq_length, to_seq_length]
         # ourselves in which case we just need to make it broadcastable to all heads.
-        extended_attention_mask = self.get_extended_attention_mask(attention_mask, input_shape)
+        extended_attention_mask = self.get_extended_attention_mask(attention_mask, input_shape, self.is_decoder)
 
         # If a 2D or 3D attention mask is provided for the cross-attention
         # we need to make broadcastable to [batch_size, num_heads, seq_length, seq_length]
