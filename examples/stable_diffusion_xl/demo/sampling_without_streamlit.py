d008a71d670a79ccb18a1da9d9b1434ee3858bce
zhanghuiyao <1814619459@qq.com>
2024-05-11 09:17:59 +0800

    update sdxl readme (#461)
    * update sdxl readme
    
    * fix CI pylint
    
    * refresh ci

--- examples/stable_diffusion_xl/demo/sampling_without_streamlit.py
+++ examples/stable_diffusion_xl/demo/sampling_without_streamlit.py
@@ -97,7 +97,6 @@ def get_parser_sample():
         default=1.0,
         help="For DynamicThresholding. Valid only when thresholding=True.",
     )
-
     parser.add_argument(
         "--discretization",
         type=str,
@@ -139,8 +138,8 @@ def get_parser_sample():
         "--init_noise_scheduler_path",
         type=str,
         default=None,
-        help="path to initial sampler noise (npy file). If not None, seed will not make effect and the initial noise"
-        " will be used for sampling. Currently only supported in the EulerA sampler.",
+        help="path to initial sampler noise (npy file). If not None, seed will not make effect and the initial noise will be used "
+        "for sampling. Currently only supported in the EulerA sampler.",
     )
     parser.add_argument("--precision_keep_origin_dtype", type=ast.literal_eval, default=False)
     parser.add_argument("--save_path", type=str, default="outputs/demo/", help="save dir")
@@ -543,8 +542,11 @@ if __name__ == "__main__":
     if args.deterministic:
         # get same results for each run
         ms.context.set_context(deterministic="ON")
-        # FIXME: Bug on MindSpore 2.2.11
+
+        # FIXME: Bug on MindSpore 2.2.11, If not setting `pynative_synchronize=True`, there may be a relatively small
+        #  probability(~1%) of the result not meeting expectations.
         ms.context.set_context(pynative_synchronize=True)
+
     if args.precision_keep_origin_dtype:
         ms.context.set_context(ascend_config=dict(precision_mode="must_keep_origin_dtype"))
 
