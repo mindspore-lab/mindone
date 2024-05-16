1a35c78e9ef35c3e6ecd60c9de5eb29ba7ffdac4
Chaoran Wei <77485245+wcrzlh@users.noreply.github.com>
2024-05-16 11:39:55 +0800

    T5ForConditionalGeneration: Graph Mode and Pynative Mode

--- mindone/transformers/models/__init__.py
+++ mindone/transformers/models/__init__.py
@@ -1,2 +1,2 @@
 from .clip import CLIPModel, CLIPTextModel, CLIPTextModelWithProjection, CLIPVisionModel, CLIPVisionModelWithProjection
-from .t5 import T5EncoderModel, T5Model
+from .t5 import T5EncoderModel, T5ForConditionalGeneration, T5Model
