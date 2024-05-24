a34c20feb55b0f44d4b9ebe3512942d1ea89e857
Didan Deng <dengdidanwtomin@gmail.com>
2024-05-24 14:19:39 +0800

    update the training command

--- examples/opensora_pku/scripts/text_condition/train_videoae_65x512x512.sh
+++ examples/opensora_pku/scripts/text_condition/train_videoae_65x512x512.sh
@@ -1,3 +1,22 @@
+export ASCEND_RT_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export MS_ENABLE_NUMA=0
+export MS_MEMORY_STATISTIC=1
+
+# enable kbk
+export MS_ENABLE_ACLNN=1
+export GRAPH_OP_RUN=1
+export GLOG_v=2
+
+# hyper-parameters
+image_size=512
+use_image_num=16
+num_frames=65
+model_dtype="fp16"
+enable_flash_attention="True"
+batch_size=2
+lr="2e-05"
+output_dir=t2v-f$num_frames-$image_size-img$use_image_num-videovae488-$model_dtype-FA$enable_flash_attention-bs$batch_size-t5
+
 python opensora/train/train_t2v.py \
     --model LatteT2V-XL/122 \
     --text_encoder_name DeepFloyd/t5-v1_1-xxl \
@@ -8,22 +27,22 @@ python opensora/train/train_t2v.py \
     --video_folder /remote-home1/dataset/data_split_tt \
     --text_embed_folder /path/to/text-embed-folder \
     --sample_rate 1 \
-    --num_frames 65 \
-    --max_image_size 512 \
+    --num_frames $num_frames \
+    --max_image_size $image_size \
     --use_recompute True \
-    --enable_flash_attention True \
-    --batch_size=2 \
+    --enable_flash_attention $enable_flash_attention \
+    --batch_size=$batch_size \
     --num_parallel_workers 10 \
     --gradient_accumulation_steps=1 \
     --max_train_steps=1000000 \
-    --start_learning_rate=2e-05 \
+    --start_learning_rate=$lr \
     --lr_scheduler="constant" \
     --lr_warmup_steps=0 \
-    --precision="fp16" \
+    --precision=$model_dtype \
     --checkpointing_steps=500 \
-    --output_dir="t2v-f65-512-img16-videovae488-bf16-ckpt-xformers-bs2-lr2e-5-t5" \
+    --output_dir=$output_dir \
     --pretrained path/to/ckpt/from/last/stage \
     --model_max_length 300 \
-    --use_image_num 16 \
+    --use_image_num $use_image_num \
     --use_img_from_vid \
     --enable_tiling \
