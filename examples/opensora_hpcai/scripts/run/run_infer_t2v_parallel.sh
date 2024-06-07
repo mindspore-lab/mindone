ceeb6fbbc2342e777ad20798263b3204d5d23e11
HaoyangLee <417493727@qq.com>
2024-06-07 09:46:49 +0800

    fix

--- examples/opensora_hpcai/scripts/run/run_infer_t2v_parallel.sh
+++ examples/opensora_hpcai/scripts/run/run_infer_t2v_parallel.sh
@@ -5,7 +5,7 @@ export ASCEND_RT_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 # export MS_ENABLE_ACLNN=1
 # export GRAPH_OP_RUN=1
 
-# msrun --master_port=8200 --worker_num=4 --local_worker_num=4 --log_dir=logs_t5_cache  \
+# msrun --master_port=8200 --worker_num=8 --local_worker_num=8 --log_dir=log_output  \
 mpirun --allow-run-as-root -n 8 --output-filename log_output --merge-stderr-to-stdout \
     python scripts/inference.py \
     --config configs/opensora/inference/stdit_512x512x64.yaml \
--- examples/opensora_hpcai/scripts/run/run_infer_vae_parallel.sh
+++ examples/opensora_hpcai/scripts/run/run_infer_vae_parallel.sh
@@ -1,6 +1,6 @@
 export ASCEND_RT_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
-# msrun --master_port=8200 --worker_num=8 --local_worker_num=4 --log_dir=logs_t5_cache  \
+# msrun --master_port=8200 --worker_num=8 --local_worker_num=8 --log_dir=log_output  \
 mpirun --allow-run-as-root -n 8 --output-filename log_output --merge-stderr-to-stdout \
 python scripts/infer_vae.py \
     --csv_path datasets/sora_overfitting_dataset_0410/vcg_200.csv \
