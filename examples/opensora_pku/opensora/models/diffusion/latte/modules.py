fix fa attention mask

examples/opensora_pku/opensora/models/diffusion/latte/modules.py
@@ -56,7 +56,9 @@ class Attention(nn.Cell):
         if self.upcast_softmax:
             sim = sim.astype(ms.float32)
         if mask is not None:
-            sim += mask
+            # (b*h 1 n_k)
+            mask = mask.to(ms.bool_)
+            sim = ops.masked_fill(sim, mask, -ms.numpy.inf)  # FIXME: whether to use -inf or -10000.0 as torch repo?
 
         # use fp32 for exponential inside
         attn = self.softmax(sim).astype(v.dtype)
@@ -217,13 +219,10 @@ class MultiHeadAttention(nn.Cell):
         k_b, k_n, _ = k.shape
         v_b, v_n, _ = v.shape
 
-        # # convert sequence mask to attention mask: (b, q_n) to (b, q_n, k_n)
-        # if mask is not None:
-        #     mask = self.reshape(mask, (mask.shape[0], -1))
-        #     attn_mask = ops.zeros((q_b, q_n, k_n), self.dtype)
-        #     mask = ops.expand_dims(mask, axis=1)  # (q_b, 1, k_n)
-        #     attn_mask = attn_mask.masked_fill(~mask, -ms.numpy.inf)
-        #     mask = attn_mask
+        # 2+: mask adaptation for multi-head attention
+        if mask is not None:
+            # flip mask, since ms FA treats 1 as discard, 0 as retain.
+            mask = 1 - mask
 
         if self.enable_flash_attention:
             # reshape qkv shape ((b n h*d) -> (b h n d))and mask dtype for FA input format