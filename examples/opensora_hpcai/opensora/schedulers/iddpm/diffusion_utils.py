bd3618e9598264a1ad461c30bf59b575b1f73824
Rustam Khadipash <16683750+hadipash@users.noreply.github.com>
2024-06-12 17:18:00 +0800

    remove numpy operations from under `construct`

--- examples/opensora_hpcai/opensora/schedulers/iddpm/diffusion_utils.py
+++ examples/opensora_hpcai/opensora/schedulers/iddpm/diffusion_utils.py
@@ -184,7 +184,7 @@ def approx_standard_normal_cdf(x):
     A fast approximation of the cumulative distribution function of the
     standard normal.
     """
-    return 0.5 * (1.0 + ops.tanh(Tensor(np.sqrt(2.0 / np.pi)) * (x + 0.044715 * ops.pow(x, 3))))
+    return 0.5 * (1.0 + ops.tanh(ms.numpy.sqrt(2.0 / ms.numpy.pi) * (x + 0.044715 * ops.pow(x, 3))))
 
 
 def continuous_gaussian_log_likelihood(x, *, means, log_scales):
