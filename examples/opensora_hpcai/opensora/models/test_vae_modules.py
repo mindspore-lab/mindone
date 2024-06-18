269b8b6acddb9496cb21836030a0b05ce5721f56
Samit <285365963@qq.com>
2024-06-18 23:35:37 +0800

    add vae 3d enc-dec

---
+++ examples/opensora_hpcai/opensora/models/test_vae_modules.py
@@ -0,0 +1,252 @@
+import sys
+import os
+import numpy as np
+
+sys.path.append(".")
+import numpy as np
+import torch
+from vae.vae_temporal import CausalConv3d, ResBlock, Encoder, Decoder
+
+import mindspore as ms
+
+bs, cin, T, H, W = 1, 9, 8, 256, 256
+cout = 16  # hidden size, actually 128
+x = np.random.normal(size=(bs, cin, T, H, W))
+
+
+def test_cconv3d():
+    ms.set_context(mode=0)
+    conv_in = CausalConv3d(cin, cout, kernel_size=3, stride=1, padding=1)
+    xo = conv_in(ms.Tensor(x, dtype=ms.float32))
+
+    print("out shape: ", xo.shape)
+
+
+def _diff_res(ms_val, pt_val, eps=1e-8):
+    abs_diff = np.fabs(ms_val - pt_val)
+    mae = abs_diff.mean()
+    max_ae = abs_diff.max()
+
+    rel_diff = abs_diff / (np.fabs(pt_val) + eps)
+    mre = rel_diff.mean()
+    max_re = rel_diff.max()
+
+    return dict(mae=mae, max_ae=max_ae, mre=mre, max_re=max_re)
+
+
+def compare_cconv3d(copy_weights=True):
+    import torch
+    # pt_code_path = "/Users/Samit/Data/Work/HW/ms_kit/aigc/Open-Sora"
+    # sys.path.append(pt_code_path)
+    # from opensora.models.vae.vae_temporal import CausalConv3d as CConv3d_PT
+
+    from pt_vae_temporal import CausalConv3d as CConv3d_PT
+
+    cc3_pt = CConv3d_PT(cin, cout, kernel_size=3)
+    out_pt = cc3_pt(torch.Tensor(x))
+    out_pt = out_pt.detach().numpy()
+    print("PT: ", out_pt.shape, out_pt.mean(), out_pt.sum())
+
+    if copy_weights:
+        torch.save(
+            {
+                "model_state_dict": cc3_pt.state_dict(),
+            },
+            "cc3.pth",
+        )
+
+        target_data = []
+        for k in cc3_pt.state_dict():
+            target_data.append({"name": k, "data": ms.Tensor(cc3_pt.state_dict()[k].detach().numpy())})
+        ms.save_checkpoint(target_data, "cc3.ckpt")
+
+    cc3_ms = CausalConv3d(cin, cout, kernel_size=3)
+
+    if copy_weights:
+        ms.load_checkpoint("cc3.ckpt", net=cc3_ms)
+
+    out_ms = cc3_ms(ms.Tensor(x, dtype=ms.float32))
+    out_ms = out_ms.asnumpy()
+    print("MS: ", out_ms.shape, out_ms.mean(), out_ms.sum())
+    print("Diff: ", _diff_res(out_pt, out_ms))
+
+
+def _convert_ckpt(pt_ckpt, name, contain_gn=False):
+    target_data = []
+    sd = torch.load(pt_ckpt)
+    for k in sd:
+        if "." not in k and contain_gn:
+            # only for GroupNorm
+            ms_name = k.replace("weight", "gamma").replace("bias", "beta")
+        else:
+            if "norm" in k:
+                ms_name = k.replace(".weight", ".gamma").replace(".bias", ".beta")
+            else:
+                ms_name = k
+        target_data.append({"name": ms_name, "data": ms.Tensor(sd[k].detach().numpy())})
+
+    save_fn = f"{name}.ckpt"
+    ms.save_checkpoint(target_data, save_fn)
+    return save_fn
+
+
+def compare_ResBlock(pdb_debug=False, backend='ms'):
+    # pt_code_path = "/data3/hyx/Open-Sora-Plan-cc41/"
+    # sys.path.append(pt_code_path)
+    # from opensora.models.ae.videobase.causal_vae.modeling_causalvae import Encoder as Enc_PT
+    from pt_vae_temporal import ResBlock as Res_PT
+
+    # NOTE: after vae 2d, h w compressed to 1/8, z=4
+    bs, cin, T, H, W = 1, 128, 9, 64, 64
+    # cout = 16
+    # net_kwargs = dict(in_channels=cin, out_channels=cout)
+
+    inp = 'res_input.npy'
+    if os.path.exists(inp):
+        print('load input from ', inp)
+        x = np.load(inp)
+    else:
+        x = np.random.normal(size=(bs, cin, T, H, W))
+        np.save(inp, x)
+    print("Input sum: ", x.sum())
+
+    kwargs = dict(in_channels=128, filters=128)
+    net_pt = Res_PT(**kwargs)
+    # import torch
+    # net_pt = torch.nn.Conv3d(**net_kwargs,kernel_size= (3, 3, 3), stride=(2,1,1), padding=(0,1,1))
+    # net_pt = torch.nn.Conv3d(**net_kwargs,kernel_size= (3, 3, 3), stride=(1,1,1), padding=0)
+
+    name = 'res'
+    pt_ckpt = f'{name}.pth'
+    if os.path.exists(pt_ckpt):
+        print('load ckpt from ', pt_ckpt)
+        net_pt.load_state_dict(torch.load(pt_ckpt))
+    else:
+        torch.save(net_pt.state_dict(), pt_ckpt)
+
+    if pdb_debug and backend=='pt':
+        import pdb; pdb.set_trace()
+    res_pt = net_pt(torch.Tensor(x))
+    res_pt = res_pt.detach().numpy()
+    print("PT output shape: ", res_pt.shape)
+
+    ms_ckpt = _convert_ckpt(pt_ckpt, name=name)
+    ms.set_context(mode=0)
+    net_ms = ResBlock(**kwargs)
+    # net_ms = ms.nn.Conv3d(**net_kwargs, kernel_size=(3,3,3), stride=(2,1,1), pad_mode="pad", padding=(0,0,1,1,1,1), has_bias=True)
+    # net_ms = ms.nn.Conv3d(**net_kwargs, kernel_size=(3,3,3), stride=(1,1,1), pad_mode="valid", has_bias=True)
+    ms.load_checkpoint(ms_ckpt, net_ms)
+
+    if pdb_debug and backend=='ms':
+        import pdb; pdb.set_trace()
+    res_ms = net_ms(ms.Tensor(x, dtype=ms.float32))
+    res_ms = res_ms.asnumpy()
+
+    print("MS output shape: ", res_ms.shape)
+    print("Diff: ", _diff_res(res_ms, res_pt))
+
+
+def compare_Encoder(pdb_debug=False, backend='ms'):
+    # pt_code_path = "/data3/hyx/Open-Sora-Plan-cc41/"
+    # sys.path.append(pt_code_path)
+    # from opensora.models.ae.videobase.causal_vae.modeling_causalvae import Encoder as Enc_PT
+    from pt_vae_temporal import Encoder as Enc_PT
+
+    # NOTE: after vae 2d, h w compressed to 1/8, z=4
+    bs, cin, T, H, W = 1, 4, 9, 64, 64
+    # net_kwargs = dict(in_channels=cin, out_channels=cout)
+
+    inp = 'enc_input.npy'
+    if os.path.exists(inp):
+        print('load input from ', inp)
+        x = np.load(inp)
+    else:
+        x = np.random.normal(size=(bs, cin, T, H, W))
+        np.save(inp, x)
+    print("Input sum: ", x.sum())
+
+    net_pt = Enc_PT()
+
+    name = 'enc'
+    pt_ckpt = f'{name}.pth'
+    if os.path.exists(pt_ckpt):
+        print('load ckpt from ', pt_ckpt)
+        net_pt.load_state_dict(torch.load(pt_ckpt))
+    else:
+        torch.save(net_pt.state_dict(), pt_ckpt)
+
+    if pdb_debug and backend=='pt':
+        import pdb; pdb.set_trace()
+    res_pt = net_pt(torch.Tensor(x))
+    res_pt = res_pt.detach().numpy()
+    print("PT output shape: ", res_pt.shape)
+
+    ms_ckpt = _convert_ckpt(pt_ckpt, name=name)
+    ms.set_context(mode=0)
+    net_ms = Encoder()
+    ms.load_checkpoint(ms_ckpt, net_ms)
+
+    if pdb_debug and backend=='ms':
+        import pdb; pdb.set_trace()
+    res_ms = net_ms(ms.Tensor(x, dtype=ms.float32))
+    res_ms = res_ms.asnumpy()
+
+    print("MS output shape: ", res_ms.shape)
+    print("Diff: ", _diff_res(res_ms, res_pt))
+
+
+def compare_Decoder(pdb_debug=False, backend='ms'):
+    # pt_code_path = "/data3/hyx/Open-Sora-Plan-cc41/"
+    # sys.path.append(pt_code_path)
+    # from opensora.models.ae.videobase.causal_vae.modeling_causalvae import Encoder as Enc_PT
+    from pt_vae_temporal import Decoder as Dec_PT
+
+    latent_embed_dim = 256
+    bs, cin, T, H, W = 1, latent_embed_dim, 2, 64, 64
+    # net_kwargs = dict(in_channels=cin, out_channels=cout)
+
+    inp = 'dec_input.npy'
+    if os.path.exists(inp):
+        print('load input from ', inp)
+        x = np.load(inp)
+    else:
+        x = np.random.normal(size=(bs, cin, T, H, W))
+        np.save(inp, x)
+    print("Input sum: ", x.sum())
+
+    net_pt = Dec_PT(latent_embed_dim=latent_embed_dim)
+
+    name = 'dec'
+    pt_ckpt = f'{name}.pth'
+    if os.path.exists(pt_ckpt):
+        print('load ckpt from ', pt_ckpt)
+        net_pt.load_state_dict(torch.load(pt_ckpt))
+    else:
+        torch.save(net_pt.state_dict(), pt_ckpt)
+
+    if pdb_debug and backend=='pt':
+        import pdb; pdb.set_trace()
+    res_pt = net_pt(torch.Tensor(x))
+    res_pt = res_pt.detach().numpy()
+    print("PT output shape: ", res_pt.shape)
+
+    ms_ckpt = _convert_ckpt(pt_ckpt, name=name)
+    ms.set_context(mode=0)
+    net_ms = Decoder(latent_embed_dim=latent_embed_dim)
+    ms.load_checkpoint(ms_ckpt, net_ms)
+
+    if pdb_debug and backend=='ms':
+        import pdb; pdb.set_trace()
+    res_ms = net_ms(ms.Tensor(x, dtype=ms.float32))
+    res_ms = res_ms.asnumpy()
+
+    print("MS output shape: ", res_ms.shape)
+    print("Diff: ", _diff_res(res_ms, res_pt))
+
+
+if __name__ == "__main__":
+    # compare_cconv3d()
+    # compare_ResBlock()
+    # compare_Encoder()
+    compare_Decoder()
+
---
+++ examples/opensora_hpcai/opensora/models/vae/vae_temporal.py
@@ -0,0 +1,373 @@
+import logging
+from typing import Tuple, Union
+
+import numpy as np
+
+import mindspore as ms
+from mindspore import nn, ops
+
+
+def divisible_by(num, den):
+    return (num % den) == 0
+
+def is_odd(n):
+    return not divisible_by(n, 2)
+
+def cast_tuple(t, length=1):
+    return t if isinstance(t, tuple) else ((t,) * length)
+
+class CausalConv3d(nn.Cell):
+    # default values aligned to torch
+    def __init__(
+        self,
+        chan_in,
+        chan_out,
+        kernel_size: Union[int, Tuple[int, int, int]],
+        pad_mode="constant",
+        strides=None,  # allow custom stride
+        has_bias=True,
+        **kwargs,
+    ):
+        super().__init__()
+        kernel_size = cast_tuple(kernel_size, 3)
+
+        time_kernel_size, height_kernel_size, width_kernel_size = kernel_size
+
+        assert is_odd(height_kernel_size) and is_odd(width_kernel_size)
+        assert pad_mode == 'constant'
+
+        dilation = kwargs.pop("dilation", 1)
+        stride = strides[0] if strides is not None else kwargs.pop("stride", 1)
+
+        self.pad_mode = pad_mode
+        time_pad = dilation * (time_kernel_size - 1) + (1 - stride)
+        height_pad = height_kernel_size // 2
+        width_pad = width_kernel_size // 2
+
+        self.time_pad = time_pad
+
+        # diff from torch: torch pad order whc. ms pad order chw. need to reverse
+        # self.time_causal_padding = (width_pad, width_pad, height_pad, height_pad, time_pad, 0)  # torch pad
+
+        stride = strides if strides is not None else (stride, 1, 1)
+        dilation = (dilation, 1, 1)
+        self.time_causal_padding = (time_pad, 0, height_pad, height_pad, width_pad, width_pad)
+        # integarte pad in conv can be faster
+        self.conv = nn.Conv3d(chan_in,
+                              chan_out,
+                              kernel_size,
+                              stride=stride,
+                              dilation=dilation,
+                              has_bias=has_bias,
+                              pad_mode="pad",
+                              padding=self.time_causal_padding,
+                              **kwargs)
+
+    def construct(self, x):
+        # x: (B, C, T, H, W)
+        # x = ops.pad(x, self.time_causal_padding, mode=self.pad_mode)
+        x = self.conv(x)
+        return x
+
+# TODO: remove it after ms2.3-0525 which support 5D input
+class GroupNorm(nn.GroupNorm):
+    # GroupNorm supporting tensors with more than 4 dim
+    def construct(self, x):
+        x_shape = x.shape
+        if x.ndim >= 5:
+            x = x.view(x_shape[0], x_shape[1], x_shape[2], -1)
+        y = super().construct(x)
+        return y.view(x_shape)
+
+
+class ResBlock(nn.Cell):
+    def __init__(
+        self,
+        in_channels,  # SCH: added
+        filters,
+        conv_fn=CausalConv3d,
+        activation_fn=nn.SiLU,
+        use_conv_shortcut=False,
+        num_groups=32,
+    ):
+        super().__init__()
+        self.in_channels = in_channels
+        self.filters = filters
+        self.activate = activation_fn()
+        self.use_conv_shortcut = use_conv_shortcut
+
+        # SCH: MAGVIT uses GroupNorm by default
+        self.norm1 = GroupNorm(num_groups, in_channels)
+        self.conv1 = conv_fn(in_channels, self.filters, kernel_size=(3, 3, 3), has_bias=False)
+        self.norm2 = GroupNorm(num_groups, self.filters)
+        self.conv2 = conv_fn(self.filters, self.filters, kernel_size=(3, 3, 3), has_bias=False)
+        if in_channels != filters:
+            if self.use_conv_shortcut:
+                self.conv3 = conv_fn(in_channels, self.filters, kernel_size=(3, 3, 3), has_bias=False)
+            else:
+                self.conv3 = conv_fn(in_channels, self.filters, kernel_size=(1, 1, 1), has_bias=False)
+
+    def construct(self, x):
+        residual = x
+        x = self.norm1(x)
+        x = self.activate(x)
+        x = self.conv1(x)
+        x = self.norm2(x)
+        x = self.activate(x)
+        x = self.conv2(x)
+        if self.in_channels != self.filters:  # SCH: ResBlock X->Y
+            residual = self.conv3(residual)
+        return x + residual
+
+
+def get_activation_fn(activation):
+    if activation == "relu":
+        activation_fn = nn.ReLU
+    elif activation == "swish":
+        activation_fn = nn.SiLU
+    else:
+        raise NotImplementedError
+    return activation_fn
+
+
+class Encoder(nn.Cell):
+    """Encoder Blocks."""
+
+    def __init__(
+        self,
+        in_out_channels=4,
+        latent_embed_dim=512,  # num channels for latent vector
+        filters=128,
+        num_res_blocks=4,
+        channel_multipliers=(1, 2, 2, 4),
+        temporal_downsample=(False, True, True),
+        num_groups=32,  # for nn.GroupNorm
+        activation_fn="swish",
+    ):
+        super().__init__()
+        self.filters = filters
+        self.num_res_blocks = num_res_blocks
+        self.num_blocks = len(channel_multipliers)
+        self.channel_multipliers = channel_multipliers
+        self.temporal_downsample = temporal_downsample
+        self.num_groups = num_groups
+        self.embedding_dim = latent_embed_dim
+
+        self.activation_fn = get_activation_fn(activation_fn)
+        self.activate = self.activation_fn()
+        # NOTE: don't switch to Conv2d, unless adjust the default value of padding mode and has_hias for mindspore
+        self.conv_fn = CausalConv3d
+
+        self.block_args = dict(
+            conv_fn=self.conv_fn,
+            activation_fn=self.activation_fn,
+            use_conv_shortcut=False,
+            num_groups=self.num_groups,
+        )
+
+        # first layer conv
+        self.conv_in = self.conv_fn(
+            in_out_channels,
+            filters,
+            kernel_size=(3, 3, 3),
+            has_bias=False,
+        )
+
+        # ResBlocks and conv downsample
+        # TODO: need auto_prefix?
+        self.block_res_blocks = nn.CellList(auto_prefix=False)
+        self.conv_blocks = nn.CellList(auto_prefix=False)
+
+        filters = self.filters
+        prev_filters = filters  # record for in_channels
+        for i in range(self.num_blocks):
+            filters = self.filters * self.channel_multipliers[i]
+            block_items = nn.CellList()
+            for _ in range(self.num_res_blocks):
+                block_items.append(ResBlock(prev_filters, filters, **self.block_args))
+                prev_filters = filters  # update in_channels
+            self.block_res_blocks.append(block_items)
+
+            if i < self.num_blocks - 1:
+                if self.temporal_downsample[i]:
+                    t_stride = 2 if self.temporal_downsample[i] else 1
+                    s_stride = 1
+                    self.conv_blocks.append(
+                        self.conv_fn(
+                            prev_filters, filters, kernel_size=(3, 3, 3), strides=(t_stride, s_stride, s_stride)
+                        )
+                    )
+                    prev_filters = filters  # update in_channels
+                else:
+                    # if no t downsample, don't add since this does nothing for pipeline models
+                    self.conv_blocks.append(nn.Identity())  # Identity
+                    prev_filters = filters  # update in_channels
+
+            self.block_res_blocks.update_parameters_name(prefix=self.param_prefix + f"block_res_blocks.")
+            self.conv_blocks.update_parameters_name(prefix=self.param_prefix + f"conv_blocks.")
+
+        # last layer res block
+        self.res_blocks = nn.CellList(auto_prefix=False)
+        for i in range(self.num_res_blocks):
+            self.res_blocks.append(ResBlock(prev_filters, filters, **self.block_args))
+            prev_filters = filters  # update in_channels
+
+            self.res_blocks.update_parameters_name(prefix=self.param_prefix + f"res_blocks.")
+
+        # MAGVIT uses Group Normalization
+        self.norm1 = GroupNorm(self.num_groups, prev_filters)
+
+        self.conv2 = self.conv_fn(prev_filters, self.embedding_dim, kernel_size=(1, 1, 1))  # NOTE: since it is 1x1x1 conv, there is no need to set padding=same
+
+    def construct(self, x):
+        # input x: (b, z, t, h, w), z = 4
+        # output: (b, embeddim_dim, t//4, h, w)
+        x = self.conv_in(x)
+
+        for i in range(self.num_blocks):
+            for j in range(self.num_res_blocks):
+                x = self.block_res_blocks[i][j](x)
+            if i < self.num_blocks - 1:
+                x = self.conv_blocks[i](x)
+        for i in range(self.num_res_blocks):
+            x = self.res_blocks[i](x)
+
+        x = self.norm1(x)
+        x = self.activate(x)
+        x = self.conv2(x)
+        return x
+
+
+class Decoder(nn.Cell):
+    """Decoder Blocks."""
+    def __init__(
+        self,
+        in_out_channels=4,
+        latent_embed_dim=512,
+        filters=128,
+        num_res_blocks=4,
+        channel_multipliers=(1, 2, 2, 4),
+        temporal_downsample=(False, True, True),  # FIXME: better name temporal_upsample
+        num_groups=32,  # for nn.GroupNorm
+        activation_fn="swish",
+    ):
+        super().__init__()
+        self.filters = filters
+        self.num_res_blocks = num_res_blocks
+        self.num_blocks = len(channel_multipliers)
+        self.channel_multipliers = channel_multipliers
+        self.temporal_downsample = temporal_downsample
+        self.num_groups = num_groups
+        self.embedding_dim = latent_embed_dim
+        self.s_stride = 1
+        assert self.s_stride == 1
+
+        self.activation_fn = get_activation_fn(activation_fn)
+        self.activate = self.activation_fn()
+        self.conv_fn = CausalConv3d
+        self.block_args = dict(
+            conv_fn=self.conv_fn,
+            activation_fn=self.activation_fn,
+            use_conv_shortcut=False,
+            num_groups=self.num_groups,
+        )
+
+        filters = self.filters * self.channel_multipliers[-1]
+        prev_filters = filters
+
+        # last conv
+        self.conv1 = self.conv_fn(self.embedding_dim, filters, kernel_size=(3, 3, 3), has_bias=True)
+
+        # last layer res block
+        self.res_blocks = nn.CellList(auto_prefix=False)
+        for _ in range(self.num_res_blocks):
+            self.res_blocks.append(ResBlock(filters, filters, **self.block_args))
+
+            self.res_blocks.update_parameters_name(prefix=self.param_prefix + f"res_blocks.")
+
+        # ResBlocks and conv upsample
+        self.block_res_blocks = nn.CellList()
+        self.num_blocks = len(self.channel_multipliers)
+        self.conv_blocks = nn.CellList()
+        # reverse to keep track of the in_channels, but append also in a reverse direction
+        for i in reversed(range(self.num_blocks)):
+            filters = self.filters * self.channel_multipliers[i]
+            # resblock handling
+            block_items = nn.CellList()
+            for _ in range(self.num_res_blocks):
+                block_items.append(ResBlock(prev_filters, filters, **self.block_args))
+                prev_filters = filters  # SCH: update in_channels
+            self.block_res_blocks.insert(0, block_items)  # SCH: append in front
+
+            # conv blocks with upsampling
+            if i > 0:
+                if self.temporal_downsample[i - 1]:
+                    t_stride = 2 if self.temporal_downsample[i - 1] else 1
+                    # SCH: T-Causal Conv 3x3x3, f -> (t_stride * 2 * 2) * f, depth to space t_stride x 2 x 2
+                    self.conv_blocks.insert(
+                        0,
+                        self.conv_fn(
+                            prev_filters, prev_filters * t_stride * self.s_stride * self.s_stride, kernel_size=(3, 3, 3)
+                        ),
+                    )
+                else:
+                    self.conv_blocks.insert(
+                        0,
+                        nn.Identity(),
+                    )
+
+            self.block_res_blocks.update_parameters_name(prefix=self.param_prefix + f"block_res_blocks.")
+            self.conv_blocks.update_parameters_name(prefix=self.param_prefix + f"conv_blocks.")
+
+        self.norm1 = GroupNorm(self.num_groups, prev_filters)
+
+        self.conv_out = self.conv_fn(filters, in_out_channels, 3)
+
+    def rearrange(x, ts, hs=1, ws=1):
+        # "B (C ts hs ws) T H W -> B C (T ts) (H hs) (W ws)",
+        # TODO: due to hs, ws is fixed to 1, it's equivalent to:
+        #       B (C ts) T H W -> B C (T ts) H W )
+        ''' for hs ws != 1
+        B, C_ts_hs_ws, T, H, W = x.shape
+        C = C_ts_hs_ws // (ts * hs * ws)
+        x = ops.reshape(x, (B, C, ts, hs, ws, T, H, W))
+        x = ops.transpose(x, (0, 1, 5, 2, 6, 3, 7, 4))
+        x = ops.reshape(x, (B, C, T * ts, H * hs, W * ws))
+        '''
+
+        B, C_ts, T, H, W = x.shape
+        C = C_ts // ts
+        x = ops.reshape(x, (B, C, ts, T, H, W))
+        # (B, C, ts, T, H, W) -> (B, C, T, ts, H, W)
+        x = ops.transpose(x, (0, 1, 3, 2, 4, 5))
+        x = ops.reshape(x, (B, C, T * ts, H, W))
+
+        return x
+
+    def construct(self, x):
+        # input: (b, embedding_dim, t//4, h, w)
+        # output: (b, z, t, h, w)
+
+        x = self.conv1(x)
+        for i in range(self.num_res_blocks):
+            x = self.res_blocks[i](x)
+        # for i in reversed(range(self.num_blocks)):
+        for i in list(range(self.num_blocks))[::-1]:
+            for j in range(self.num_res_blocks):
+                x = self.block_res_blocks[i][j](x)
+            if i > 0:
+                t_stride = 2 if self.temporal_downsample[i - 1] else 1
+                x = self.conv_blocks[i - 1](x)
+                x = self.rearrange(
+                    x,
+                    ts=t_stride,
+                    # hs=self.s_stride,
+                    # ws=self.s_stride,
+                )
+
+        x = self.norm1(x)
+        x = self.activate(x)
+        x = self.conv_out(x)
+        return x
+
+
