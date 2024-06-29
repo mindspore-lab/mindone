2f7d3b741dc75df895b4012780cb1feaa27bce70
Yang Chen <isyangc2@gmail.com>
2024-06-29 11:21:46 +0800

    fix jit bug

--- examples/stable_diffusion_v2/common.py
+++ examples/stable_diffusion_v2/common.py
@@ -46,6 +46,20 @@ def init_env(
         _logger.warning("Debug mode is on, switching execution mode to PyNative.")
         mode = ms.PYNATIVE_MODE
 
+    try:
+        if jit_level in ["O0", "O1", "O2"]:
+            ms.set_context(jit_config={"jit_level": jit_level})
+            _logger.info(f"set jit_level: {jit_level}.")
+        else:
+            _logger.warning(
+                f"Unsupport jit_level: {jit_level}. The framework automatically selects the execution method"
+            )
+    except Exception:
+        _logger.warning(
+            "The current jit_level is not suitable because current MindSpore version or mode does not match,"
+            "please ensure the MindSpore version >= ms2.3_0615, and use GRAPH_MODE."
+        )
+
     if distributed:
         device_id = int(os.getenv("DEVICE_ID"))
         ms.set_context(
@@ -81,20 +95,6 @@ def init_env(
             device_id=device_id,
             ascend_config={"precision_mode": "allow_fp32_to_fp16"},  # Only effective on Ascend 910*
             pynative_synchronize=debug,
-        )
-
-    try:
-        if jit_level in ["O0", "O1", "O2"]:
-            ms.set_context(jit_config={"jit_level": jit_level})
-            _logger.info(f"set jit_level: {jit_level}.")
-        else:
-            _logger.warning(
-                f"Unsupport jit_level: {jit_level}. The framework automatically selects the execution method"
-            )
-    except Exception:
-        _logger.warning(
-            "The current jit_level is not suitable because current MindSpore version or mode does not match,"
-            "please ensure the MindSpore version >= ms2.3_0615, and use GRAPH_MODE."
         )
 
     return device_id, rank_id, device_num
