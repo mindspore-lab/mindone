fix softmax in attentionblock3d fiox

examples/opensora_pku/opensora/models/ae/videobase/modules/attention.py
@@ -73,6 +73,7 @@ class AttnBlock3DFix(nn.Cell):
         self.k = CausalConv3d(in_channels, in_channels, kernel_size=1, stride=1)
         self.v = CausalConv3d(in_channels, in_channels, kernel_size=1, stride=1)
         self.proj_out = CausalConv3d(in_channels, in_channels, kernel_size=1, stride=1)
+        self.softmax = nn.Softmax(axis=2)
 
     def construct(self, x):
         # q shape: (b c t h w)
@@ -98,7 +99,7 @@ class AttnBlock3DFix(nn.Cell):
         w_ = self.bmm(q, k)  # b,hw,hw    w[b,i,j]=sum_c q[b,i,c]k[b,c,j]
         w_ = w_ * (int(c) ** (-0.5))
         # FIXME: cast w_ to FP32 in amp
-        w_ = ops.Softmax(axis=2)(w_)
+        w_ = self.softmax(w_)
 
         # attend to values
         # v: (b c t h w) -> (b t c h w) -> (bt c hw)