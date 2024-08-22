2af21d150af785a6d6ab41b2febda41f7751460f
Mike Cheung <zhtmike@gmail.com>
2024-08-22 18:07:46 +0800

    use KBK for normal loss value

--- examples/pixart_sigma/train.py
+++ examples/pixart_sigma/train.py
@@ -49,7 +49,7 @@ logger = logging.getLogger(__name__)
 
 def init_env(args) -> Tuple[int, int]:
     set_random_seed(args.seed)
-    ms.set_context(mode=args.mode, device_target=args.device_target, jit_config=dict(jit_level="O1"))
+    ms.set_context(mode=args.mode, device_target=args.device_target, jit_config=dict(jit_level="O0"))
     if args.use_parallel:
         init()
         device_num = get_group_size()
