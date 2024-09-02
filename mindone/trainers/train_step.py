260e7da7814496ab286a2fc22edd21e254e4b967
Didan Deng <33117903+wtomin@users.noreply.github.com>
2024-09-02 15:04:15 +0800

    zero optimizer from zhaoting

--- mindone/trainers/train_step.py
+++ mindone/trainers/train_step.py
@@ -117,8 +117,7 @@ class TrainOneStepWrapper(nn.TrainOneStepWithLossScaleCell):
         grads = self.grad(self.network, weights)(*inputs, scaling_sens_filled)
 
         # Gradient communication
-        if self.zero_helper is not None:
-            grads = self.zero_helper.cal_gradients(grads)
+        grads = self.zero_helper.cal_gradients(grads)
 
         if self.accum_steps == 1:
             grads = self.grad_reducer(grads)
