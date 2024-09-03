4c0cee8b484b417896892ed4d779d39badc49394
zhaoting <zhaoting23@huawei.com>
2024-09-03 14:52:05 +0800

    update

--- mindone/models/modules/parallel/__init__.py
+++ mindone/models/modules/parallel/__init__.py
@@ -3,6 +3,7 @@ from mindspore import nn
 from .conv import Conv1d, Conv2d, Conv3d
 from .dense import Dense
 
+# {Original MindSpore Cell: New Cell in ZeRO3}
 PARALLEL_MODULES = {
     nn.Conv1d: Conv1d,
     nn.Conv2d: Conv2d,
