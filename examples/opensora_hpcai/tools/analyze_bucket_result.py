a9e7126e4bd7eb3d2210ae3aef6426179d8e1248
samithuang <285365963@qq.com>
2024-09-13 15:36:06 +0800

    impr bucket analysis

--- examples/opensora_hpcai/tools/analyze_bucket_result.py
+++ examples/opensora_hpcai/tools/analyze_bucket_result.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import argparse
+import math
 
 # result_path = 'outputs/analyze_os1.2_stage2_vcg200_ms231/merged_result.log'
 
@@ -14,7 +15,8 @@ def analyze(result_path, save_path):
 
     # filter out outliers
     data = data[data['train_time(s)']<max_step_time]
-
+    global_avg_step_time = data['train_time(s)'].mean() 
+    num_steps = data.shape[0]
 
     res = data.groupby('shape')['train_time(s)'].agg(['mean', 'std', 'count']).reset_index()
 
@@ -23,10 +25,32 @@ def analyze(result_path, save_path):
     res['std_step_time'].fillna(0, inplace=True)
 
     res_sorted = res.sort_values(by='mean_step_time', ascending=False)
+    
+    percent_col = []
+    bs_col = []
+    sug_bs_col = []
+    rnd_bs_col = []
+    tot_occur = res_sorted['occurence'].sum()
+    # for shape_str in res_sorted['shape'].tolist():
+    for idx, row in res_sorted.iterrows():
+        percent_col.append(row['occurence'] / tot_occur)
+        bs = int(row['shape'].split(',')[0][1:])
+        bs_col.append(bs)
+
+        suggest_bs = (global_avg_step_time / row['mean_step_time']) * bs 
+        rounded_bs = max(1, math.floor(suggest_bs))
+        sug_bs_col.append(suggest_bs)
+        rnd_bs_col.append(rounded_bs)
+    res_sorted['occ_percent'] = percent_col
+    res_sorted['bs'] = bs_col
+    res_sorted['suggested_bs'] = sug_bs_col
+    res_sorted['rounded_bs'] = rnd_bs_col
 
     res_sorted.to_csv(save_path, index=False)
 
     print(res_sorted)
+    print(f"\nAverage step time(s) (in {num_steps} steps starting from step {warmup_steps}): ", global_avg_step_time)
+    print('Analysis csv saved in', save_path)
 
 
 if __name__ == "__main__":
