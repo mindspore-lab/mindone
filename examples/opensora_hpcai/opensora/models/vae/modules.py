941298224ba6bddd68af1c890a70baabe9e5c8c9
Samit <285365963@qq.com>
2024-10-14 22:28:44 +0800

    vae encoder lazy inline

--- examples/opensora_hpcai/opensora/models/vae/modules.py
+++ examples/opensora_hpcai/opensora/models/vae/modules.py
@@ -178,7 +178,7 @@ def make_attn(in_channels, attn_type="vanilla"):
 
 # used in vae
 class Encoder(nn.Cell):
-    # @ms.lazy_inline()
+    @ms.lazy_inline()
     def __init__(
         self,
         *,
@@ -300,7 +300,7 @@ class Encoder(nn.Cell):
 
 
 class Decoder(nn.Cell):
-    # @ms.lazy_inline()
+    @ms.lazy_inline()
     def __init__(
         self,
         *,
