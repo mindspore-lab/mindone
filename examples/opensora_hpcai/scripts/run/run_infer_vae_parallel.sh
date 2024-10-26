66b5bbe1d0e466199c4def466e11e44c1a71d2cf
HaoyangLI <417493727@qq.com>
2024-10-26 09:46:55 +0800

    Update opensora-hpcai inference scripts (#704)
    * Update opensora-hpcai inference scripts
    
    * unset RANK_TABLE_FILE
    
    * fix typo
    
    * align format
    
    * state unset RANK_TABLE_FILE on README

--- examples/opensora_hpcai/scripts/run/run_infer_vae_parallel.sh
+++ examples/opensora_hpcai/scripts/run/run_infer_vae_parallel.sh
@@ -3,9 +3,9 @@ export ASCEND_RT_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 # msrun --master_port=8200 --worker_num=8 --local_worker_num=8 --log_dir=log_output  \
 mpirun --allow-run-as-root -n 8 --output-filename log_output --merge-stderr-to-stdout \
 python scripts/infer_vae.py \
-    --csv_path datasets/sora_overfitting_dataset_0410/vcg_200.csv \
-    --video_folder datasets/sora_overfitting_dataset_0410 \
-    --output_path datasets/sora_overfitting_dataset_0410_vae_512x512 \
+    --csv_path /path/to/video_caption.csv \
+    --video_folder /path/to/video \
+    --output_path /path/to/vae_cache \
     --vae_checkpoint models/sd-vae-ft-ema.ckpt \
     --image_size 576 1024 \
     --transform_name crop_resize \
