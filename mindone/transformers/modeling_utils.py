cccf2bd2dd4aca870b0d305a3bcc3d45ebb17202
zhanghuiyao <1814619459@qq.com>
2024-10-26 11:59:46 +0800

    fix PreTrainedModel loaded_keys

--- mindone/transformers/modeling_utils.py
+++ mindone/transformers/modeling_utils.py
@@ -1968,7 +1968,7 @@ class MSPreTrainedModel(nn.Cell, ModuleUtilsMixin, GenerationMixin, PushToHubMix
         # Mapping loaded_keys from pt to ms
         pt2ms_mappings = _get_pt2ms_mappings(model)
         loaded_keys = [
-            _get_pt2ms_mapped_kv(pt2ms_mappings, s, None, f"{model.base_model_prefix}.")[0] for s in loaded_keys
+            _get_pt2ms_mapped_kv(pt2ms_mappings, s, None, "")[0] for s in loaded_keys
         ]
         # Retrieve missing & unexpected_keys
         model_state_dict = {k: v for k, v in model.parameters_and_names()}
