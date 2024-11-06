5840bbd650a8707b2f2dc578a97fd66aad1eb9f6
zhanghuiyao <1814619459@qq.com>
2024-11-06 20:12:18 +0800

    update run scripts

--- examples/transformers/llama/run_distribute.sh
+++ examples/transformers/llama/run_distribute.sh
@@ -1,10 +1,17 @@
 
+PORT=9000
+
+if [ $# == 1 ]
+then
+    PORT=$1
+fi
+
 local_path=/home_host/zhy/huggingface_weights
 
-msrun --bind_core=True --worker_num=8 --local_worker_num=8 --master_port=9000 --log_dir=outputs/parallel_logs \
+msrun --bind_core=True --worker_num=8 --local_worker_num=8 --master_port=$PORT --log_dir=outputs/parallel_logs \
 python finetune_with_mindspore_trainer.py \
   --model_path $local_path/meta-llama/Meta-Llama-3-8B \
-  --dataset_path $local_path/Yelp/yelp_review_full \
+  --dataset_path $local_path/yelp_review_full \
   --output_dir ./outputs \
   --per_device_train_batch_size 8 \
   \
