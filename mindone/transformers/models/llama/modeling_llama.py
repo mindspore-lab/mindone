21fc9fd6c8dd2e0c93289de14f52011f3b79d026
zhanghuiyao <1814619459@qq.com>
2024-11-06 21:14:27 +0800

    debug

--- mindone/transformers/models/llama/modeling_llama.py
+++ mindone/transformers/models/llama/modeling_llama.py
@@ -1238,7 +1238,7 @@ class LlamaForSequenceClassification(LlamaPreTrainedModel):
         #         loss = self.loss_fct_single_label_classification(pooled_logits.view(-1, self.num_labels), labels.view(-1).int())
         #     elif problem_type == 2:  #"multi_label_classification"
         #         loss = self.loss_fct_multi_label_classification(pooled_logits, labels)
-        if pooled_logits is not None
+        if pooled_logits is not None:
             loss = self.loss_fct_single_label_classification(pooled_logits.view(-1, self.num_labels), labels.view(-1).int())
         else:
             loss = -1.
