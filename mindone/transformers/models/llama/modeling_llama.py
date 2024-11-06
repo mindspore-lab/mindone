54957c5e3de65d5474c6d19880d6303da67a1f7d
zhanghuiyao <1814619459@qq.com>
2024-11-06 21:07:26 +0800

    delete llama model output_attention

--- mindone/transformers/models/llama/modeling_llama.py
+++ mindone/transformers/models/llama/modeling_llama.py
@@ -359,10 +359,7 @@ class LlamaAttention(nn.Cell):
         else:
             attn_output = self.o_proj(attn_output)
 
-        if not output_attentions:
-            attn_weights = None
-
-        return attn_output, attn_weights, past_key_value
+        return attn_output, past_key_value
 
 
 class LlamaFlashAttention2(LlamaAttention):
@@ -481,7 +478,7 @@ class LlamaFlashAttention2(LlamaAttention):
         else:
             attn_output = self.o_proj(attn_output)
 
-        return attn_output, None, past_key_value
+        return attn_output, past_key_value
 
 
 LLAMA_ATTENTION_CLASSES = {
@@ -539,7 +536,7 @@ class LlamaDecoderLayer(nn.Cell):
         hidden_states = self.input_layernorm(hidden_states)
 
         # Self Attention
-        hidden_states, self_attn_weights, present_key_value = self.self_attn(
+        hidden_states, present_key_value = self.self_attn(
             hidden_states=hidden_states,
             attention_mask=attention_mask,
             position_ids=position_ids,
@@ -558,9 +555,6 @@ class LlamaDecoderLayer(nn.Cell):
         hidden_states = self.output_identity(hidden_states)
 
         outputs = (hidden_states,)
-
-        if output_attentions:
-            outputs += (self_attn_weights,)
 
         if use_cache:
             outputs += (present_key_value,)
@@ -824,10 +818,7 @@ class LlamaModel(LlamaPreTrainedModel):
             hidden_states = layer_outputs[0]
 
             if use_cache:
-                next_caches += (layer_outputs[2 if output_attentions else 1],)
-
-            if output_attentions:
-                all_self_attns += (layer_outputs[1],)
+                next_caches += (layer_outputs[1],)
 
         hidden_states = self.norm(hidden_states)
 
