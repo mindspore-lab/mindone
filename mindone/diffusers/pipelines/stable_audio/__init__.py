74fc26e7566950f403f1ea230c299a099a37cfd8
litingyu <605979840@qq.com>
2024-11-14 18:42:44 +0800

    init

--- mindone/diffusers/pipelines/stable_audio/__init__.py
+++ mindone/diffusers/pipelines/stable_audio/__init__.py
@@ -1,41 +1,17 @@
 from typing import TYPE_CHECKING
 
-from ...utils import (
-    DIFFUSERS_SLOW_IMPORT,
-    OptionalDependencyNotAvailable,
-    _LazyModule,
-    get_objects_from_module,
-    is_torch_available,
-    is_transformers_available,
-    is_transformers_version,
-)
+from ...utils import _LazyModule
 
 
 _dummy_objects = {}
 _import_structure = {}
-
-try:
-    if not (is_transformers_available() and is_torch_available() and is_transformers_version(">=", "4.27.0")):
-        raise OptionalDependencyNotAvailable()
-except OptionalDependencyNotAvailable:
-    from ...utils import dummy_torch_and_transformers_objects
-
-    _dummy_objects.update(get_objects_from_module(dummy_torch_and_transformers_objects))
-else:
-    _import_structure["modeling_stable_audio"] = ["StableAudioProjectionModel"]
-    _import_structure["pipeline_stable_audio"] = ["StableAudioPipeline"]
+_import_structure["modeling_stable_audio"] = ["StableAudioProjectionModel"]
+_import_structure["pipeline_stable_audio"] = ["StableAudioPipeline"]
 
 
-if TYPE_CHECKING or DIFFUSERS_SLOW_IMPORT:
-    try:
-        if not (is_transformers_available() and is_torch_available() and is_transformers_version(">=", "4.27.0")):
-            raise OptionalDependencyNotAvailable()
-    except OptionalDependencyNotAvailable:
-        from ...utils.dummy_torch_and_transformers_objects import *
-
-    else:
-        from .modeling_stable_audio import StableAudioProjectionModel
-        from .pipeline_stable_audio import StableAudioPipeline
+if TYPE_CHECKING:
+    from .modeling_stable_audio import StableAudioProjectionModel
+    from .pipeline_stable_audio import StableAudioPipeline
 
 else:
     import sys
