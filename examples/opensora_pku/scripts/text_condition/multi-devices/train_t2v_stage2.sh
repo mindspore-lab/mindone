53336e10b019e27663deba5efcbd6964c976bda4
Didan Deng <33117903+wtomin@users.noreply.github.com>
2024-11-19 16:57:26 +0800

    update training scripts

--- examples/opensora_pku/scripts/text_condition/multi-devices/train_t2v_stage2.sh
+++ examples/opensora_pku/scripts/text_condition/multi-devices/train_t2v_stage2.sh
@@ -1,56 +1,65 @@
-# Stage 3: 29x480p
-NUM_FRAME=29
-msrun --bind_core=True --worker_num=8 --local_worker_num=8 --master_port=9000 --log_dir="t2v-video3d-${NUM_FRAME}x480p_zero2/parallel_logs" \
+# Stage 2: 93x320x320
+NUM_FRAME=93
+WIDTH=320
+HEIGHT=320
+ASCEND_RT_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+msrun --bind_core=True --worker_num=8 --local_worker_num=8 --master_port=9000 --log_dir="t2v-video-${NUM_FRAME}x${HEIGHT}x${WIDTH}/parallel_logs" \
   opensora/train/train_t2v_diffusers.py \
-    --model OpenSoraT2V-ROPE-L/122 \
-    --text_encoder_name google/mt5-xxl \
+    --model OpenSoraT2V_v1_3-2B/122 \
+    --text_encoder_name_1 google/mt5-xxl \
     --cache_dir "./" \
     --dataset t2v \
-    --data "scripts/train_data/merge_data_mixkit.txt" \
-    --ae CausalVAEModel_D4_4x8x8 \
-    --ae_path "LanguageBind/Open-Sora-Plan-v1.2.0/vae" \
+    --data "scripts/train_data/video_data_v1_2.txt" \
+    --ae WFVAEModel_D8_4x8x8  \
+    --ae_path LanguageBind/Open-Sora-Plan-v1.3.0/vae \
     --sample_rate 1 \
     --num_frames ${NUM_FRAME} \
-    --max_height 480 \
-    --max_width 640 \
+    --max_height ${HEIGHT} \
+    --max_width ${WIDTH} \
     --interpolation_scale_t 1.0 \
     --interpolation_scale_h 1.0 \
     --interpolation_scale_w 1.0 \
-    --attention_mode xformers \
     --gradient_checkpointing \
     --train_batch_size=1 \
     --dataloader_num_workers 8 \
     --gradient_accumulation_steps=1 \
     --max_train_steps=1000000 \
-    --start_learning_rate=1e-4 \
+    --start_learning_rate=2e-5 \
     --lr_scheduler="constant" \
     --seed=10 \
-    --lr_warmup_steps=500 \
+    --lr_warmup_steps=0 \
     --precision="bf16" \
     --checkpointing_steps=1000 \
-    --output_dir="t2v-video3d-${NUM_FRAME}x480p_zero2/" \
+    --output_dir="t2v-video-${NUM_FRAME}x${HEIGHT}x${WIDTH}/" \
     --model_max_length 512 \
     --use_image_num 0 \
     --cfg 0.1 \
     --snr_gamma 5.0 \
+    --rescale_betas_zero_snr \
     --use_ema True\
     --ema_start_step 0 \
     --enable_tiling \
     --tile_overlap_factor 0.125 \
     --clip_grad True \
     --max_grad_norm 1.0 \
-    --use_rope \
     --noise_offset 0.02 \
     --enable_stable_fp32 True\
     --ema_decay 0.999 \
     --speed_factor 1.0 \
-    --drop_short_ratio 1.0 \
-    --pretrained "LanguageBind/Open-Sora-Plan-v1.2.0/1x480p" \
+    --drop_short_ratio 0.0 \
     --use_parallel True \
     --parallel_mode "zero" \
     --zero_stage 2 \
     --max_device_memory "59GB" \
     --jit_syntax_level "lax" \
-    --dataset_sink_mode True \
-    --num_no_recompute 18 \
-    # --group_frame \
+    --dataset_sink_mode False \
+    --prediction_type "v_prediction" \
+    --hw_stride 32 \
+    --sparse1d \
+    --sparse_n 4 \
+    --train_fps 16 \
+    --trained_data_global_step 0 \
+    --group_data \
+    --mode 1 \
+    # --sp_size 8 \
+    # --train_sp_batch_size 1 \
