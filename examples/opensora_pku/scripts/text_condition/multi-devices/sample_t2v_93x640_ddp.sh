fc68b9130183402d5228670e81787017f38f422b
Didan Deng <33117903+wtomin@users.noreply.github.com>
2024-11-21 11:13:44 +0800

    update train scripts

--- examples/opensora_pku/scripts/text_condition/multi-devices/sample_t2v_93x640_ddp.sh
+++ examples/opensora_pku/scripts/text_condition/multi-devices/sample_t2v_93x640_ddp.sh
@@ -1,25 +1,26 @@
-# The DiT model is trained arbitrarily on stride=32. 
-# So keep the resolution of the inference a multiple of 32. Frames needs to be 4n+1, e.g. 93, 77, 61, 45, 29, 1 (image).
 
-export DEVICE_ID=0
-python opensora/sample/sample.py \
+msrun --bind_core=True --worker_num=8 --local_worker_num=8 --master_port=9000 --log_dir="./sample_videos/sora_93x640_mt5_ddp/parallel_logs/" \
+   opensora/sample/sample.py \
     --model_path LanguageBind/Open-Sora-Plan-v1.3.0/any93x640x640 \
     --version v1_3 \
-    --num_frames 29 \
-    --height 704 \
-    --width 1280 \
+    --num_frames 93 \
+    --height 640 \
+    --width 640 \
     --text_encoder_name_1 google/mt5-xxl \
-    --text_prompt examples/prompt_list_0.txt \
+    --text_prompt examples/sora.txt \
     --ae WFVAEModel_D8_4x8x8  \
     --ae_path LanguageBind/Open-Sora-Plan-v1.3.0/vae \
-    --save_img_path "./sample_videos/prompt_list_0_29x1280" \
-    --fps 24 \
+    --save_img_path "./sample_videos/sora_93x640_mt5" \
+    --fps 18 \
     --guidance_scale 7.5 \
     --num_sampling_steps 100 \
     --enable_tiling \
     --max_sequence_length 512 \
     --sample_method EulerAncestralDiscrete \
+    --seed 1234 \
     --num_samples_per_prompt 1 \
     --rescale_betas_zero_snr \
     --prediction_type "v_prediction" \
-    --mode 1
+    --mode 1 \
+    --precision bf16 \
+    --use_parallel True \
