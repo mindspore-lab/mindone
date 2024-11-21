e63bfe933495083d11c19026d94d344e5f259a8b
Didan Deng <33117903+wtomin@users.noreply.github.com>
2024-11-21 15:47:14 +0800

    lazy_inline & update save video func

--- examples/opensora_pku/scripts/text_condition/multi-devices/sample_t2v_93x640_ddp.sh
+++ examples/opensora_pku/scripts/text_condition/multi-devices/sample_t2v_93x640_ddp.sh
@@ -1,4 +1,4 @@
-
+export ASCEND_RT_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 msrun --bind_core=True --worker_num=8 --local_worker_num=8 --master_port=9000 --log_dir="./sample_videos/sora_93x640_mt5_ddp/parallel_logs/" \
    opensora/sample/sample.py \
     --model_path LanguageBind/Open-Sora-Plan-v1.3.0/any93x640x640 \
