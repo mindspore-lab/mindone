b106f9f13e4be7bed8e34a59cff94ad683ed76c9
Didan Deng <33117903+wtomin@users.noreply.github.com>
2024-11-21 19:54:55 +0800

    save path correction

--- examples/opensora_pku/scripts/text_condition/multi-devices/sample_t2v_93x640_sp.sh
+++ examples/opensora_pku/scripts/text_condition/multi-devices/sample_t2v_93x640_sp.sh
@@ -11,7 +11,7 @@ msrun --bind_core=True --worker_num=8 --local_worker_num=8 --master_port=9000 --
     --text_prompt examples/sora.txt \
     --ae WFVAEModel_D8_4x8x8  \
     --ae_path LanguageBind/Open-Sora-Plan-v1.3.0/vae \
-    --save_img_path "./sample_videos/sora_93x640_mt5" \
+    --save_img_path "./sample_videos/sora_93x640_mt5_sp" \
     --fps 18 \
     --guidance_scale 7.5 \
     --num_sampling_steps 100 \
