588cc7cd98f81364c1b570b798a027fc906a7c42
Didan Deng <33117903+wtomin@users.noreply.github.com>
2024-11-30 16:25:09 +0800

    tile_overlap_factor remove

--- examples/opensora_pku/scripts/text_condition/multi-devices/train_t2v_stage3.sh
+++ examples/opensora_pku/scripts/text_condition/multi-devices/train_t2v_stage3.sh
@@ -40,7 +40,6 @@ msrun --bind_core=True --worker_num=8 --local_worker_num=8 --master_port=9000 --
     --use_ema False \
     --ema_start_step 0 \
     --enable_tiling \
-    --tile_overlap_factor 0.125 \
     --clip_grad True \
     --max_grad_norm 1.0 \
     --noise_offset 0.02 \
