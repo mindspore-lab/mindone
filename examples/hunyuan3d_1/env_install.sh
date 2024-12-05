bd3223d307740d27ccd9cc7b5a56aa4f589148f0
chenyingshu <yingshu2008@gmail.com>
2024-12-05 10:49:50 +0800

    update env

--- examples/hunyuan3d_1/env_install.sh
+++ examples/hunyuan3d_1/env_install.sh
@@ -1,5 +1,10 @@
 # in addition to mindone
-pip3 install diffusers transformers
+pip3 install transformers
 pip3 install rembg tqdm omegaconf matplotlib opencv-python imageio jaxtyping 
-pip3 install SentencePiece trimesh xatlas libigl ninja gradio
+# pip3 install libigl # optional not support yet
+pip3 install SentencePiece trimesh xatlas ninja gradio
 pip3 install open3d
+
+# if do rendering locally (maybe unstable)
+pip3 install pyglet
+# conda install anaconda::libglu
