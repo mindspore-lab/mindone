8db2a9f4526346e8f5dd6ae49ab2db5ac32438fe
chenyingshu <yingshu2008@gmail.com>
2024-12-05 17:23:04 +0800

    render mesh gif

---
+++ examples/hunyuan3d_1/svrm/ldm/vis_util.py
@@ -0,0 +1,89 @@
+# 
+# MindONE 
+# render and save images/gif using trimesh in CPU
+# pip install pyglet
+# Only support local rendering with GUI (or if remote to server by ssh -X sucessfully)
+# Usage:
+# python infer/git_render.py --mesh_path mesh.obj --output_gif_path output.gif
+#
+
+import os
+from PIL import Image
+import imageio
+import time
+import trimesh
+from trimesh.transformations import rotation_matrix, concatenate_matrices
+import numpy as np
+import pyglet
+
+
+def render(
+    obj_filename, 
+    elev=0, 
+    azim=None, 
+    resolution=512, 
+    gif_dst_path='', 
+    n_views=120, 
+    fps=30, 
+    rgb=False
+):
+    '''
+        obj_filename: path to obj file
+        gif_dst_path: 
+            if set a path, will render n_views frames, then save it to a gif file
+            if not set, will render single frame, then return PIL.Image instance
+        rgb: if set true, will convert result to rgb image/frame
+    '''
+    
+
+    # load mesh
+    mesh = trimesh.load(obj_filename)
+    
+    # get a scene object containing the mesh, this is equivalent to:
+    # scene = trimesh.scene.Scene(mesh)
+    scene = mesh.scene()
+    
+    # render scene as a PNG in bytes
+    frames = []
+
+    # rotation angles
+    if azim is None:
+        elev = np.linspace(elev, elev, n_views+1)[:-1]
+        azim = np.linspace(0, 360, n_views+1)[:-1]
+
+    xaxis, yaxis, zaxis = [1,0,0], [0,1,0], [0,0,1] # y-up direction
+    scene.camera.fov = (49.1, 49.1)
+    for i, (alpha, beta) in enumerate(zip(elev, azim)):
+        if i < 0: #skip some frames if needed 
+            continue
+
+        # prepare transformation matrix
+        Rx = rotation_matrix(np.radians(alpha), xaxis, point=scene.centroid)
+        Ry = rotation_matrix(np.radians(beta), yaxis, point=scene.centroid)
+        Rz = rotation_matrix(0., zaxis, point=scene.centroid)
+        R = concatenate_matrices(Rx, Ry, Rz)
+
+        transform_M = scene.camera.look_at(mesh.vertices, rotation=R, distance=1.5)
+        scene.camera_transform = transform_M
+
+        file_name = os.path.join(os.path.dirname(gif_dst_path), "frame_%04d.png"%i)
+        while True: # TODO: may fail after some renderings, need to be resolved
+            try:
+                png = scene.save_image(resolution=[resolution, resolution]) # png in bytes
+                with open(file_name, "wb") as f:
+                    f.write(png)
+                    f.close()
+                print("saved %s"%file_name)
+                break
+            except:
+                print("unable to save image %s"%file_name) 
+
+    if gif_dst_path != '': 
+        with imageio.get_writer(uri=gif_dst_path, mode='I', duration=1. / fps * 1000, loop=0) as writer:
+            for i in range(n_views):
+                file_name = os.path.join(os.path.dirname(gif_dst_path), "frame_%04d.png"%i)
+                frame = Image.open(file_name).convert("RGB") if rgb else Image.open(file_name)
+                writer.append_data(frame)
+    return frames
+
+
