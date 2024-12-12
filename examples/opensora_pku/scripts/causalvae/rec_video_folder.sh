51b2c7f40b9c28672c078418dc95ed213cba1a84
Didan Deng <33117903+wtomin@users.noreply.github.com>
2024-12-12 10:45:08 +0800

    allow parallel video reconstruction

--- examples/opensora_pku/scripts/causalvae/rec_video_folder.sh
+++ examples/opensora_pku/scripts/causalvae/rec_video_folder.sh
@@ -1,10 +1,10 @@
-python examples/rec_video_folder.py \
+msrun --bind_core=True --worker_num=8 --local_worker_num=8 --master_port=9000 --log_dir="parallel_logs/" examples/rec_video_folder.py \
     --batch_size 1 \
     --real_video_dir datasets/UCF-101/ \
     --data_file_path datasets/ucf101_test.csv \
     --generated_video_dir recons/ucf101_test/ \
     --device Ascend \
-    --sample_fps 10 \
+    --sample_fps 30 \
     --sample_rate 1 \
     --num_frames 25 \
     --height 256 \
@@ -12,6 +12,7 @@ python examples/rec_video_folder.py \
     --num_workers 8 \
     --ae "WFVAEModel_D8_4x8x8" \
     --ae_path LanguageBind/Open-Sora-Plan-v1.3.0/vae \
-    --enable_tiling \
     --mode 1 \
     --jit_syntax_level lax \
+    --use_parallel True \
+    # --ms_checkpoint path/to/ms/ckpt
