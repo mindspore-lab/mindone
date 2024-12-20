c7ecb58939d4e996efc24e1d58a0083a9bfda416
The-truthh <53842165+The-truthh@users.noreply.github.com>
2024-12-20 09:51:48 +0800

    feat(diffusers/pipelines): add pipelines of animatediff and aura_flow (#764)

--- mindone/diffusers/models/__init__.py
+++ mindone/diffusers/models/__init__.py
@@ -28,10 +28,12 @@ _import_structure = {
     "controlnet": ["ControlNetModel"],
     "controlnet_hunyuan": ["HunyuanDiT2DControlNetModel", "HunyuanDiT2DMultiControlNetModel"],
     "controlnet_sd3": ["SD3ControlNetModel", "SD3MultiControlNetModel"],
+    "controlnet_sparsectrl": ["SparseControlNetModel"],
     "controlnet_xs": ["ControlNetXSAdapter", "UNetControlNetXSModel"],
     "dual_transformer_2d": ["DualTransformer2DModel"],
     "embeddings": ["ImageProjection"],
     "modeling_utils": ["ModelMixin"],
+    "transformers.auraflow_transformer_2d": ["AuraFlowTransformer2DModel"],
     "transformers.cogvideox_transformer_3d": ["CogVideoXTransformer3DModel"],
     "transformers.dit_transformer_2d": ["DiTTransformer2DModel"],
     "transformers.dual_transformer_2d": ["DualTransformer2DModel"],
@@ -71,10 +73,12 @@ if TYPE_CHECKING:
     from .controlnet import ControlNetModel
     from .controlnet_hunyuan import HunyuanDiT2DControlNetModel, HunyuanDiT2DMultiControlNetModel
     from .controlnet_sd3 import SD3ControlNetModel, SD3MultiControlNetModel
+    from .controlnet_sparsectrl import SparseControlNetModel
     from .controlnet_xs import ControlNetXSAdapter, UNetControlNetXSModel
     from .embeddings import ImageProjection
     from .modeling_utils import ModelMixin
     from .transformers import (
+        AuraFlowTransformer2DModel,
         CogVideoXTransformer3DModel,
         DiTTransformer2DModel,
         DualTransformer2DModel,
