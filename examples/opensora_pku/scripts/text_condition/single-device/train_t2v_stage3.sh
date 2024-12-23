48b9c820e552423f74600e4dd537029ee2e585db
Didan Deng <33117903+wtomin@users.noreply.github.com>
2024-12-23 10:24:52 +0800

    set dataset sink mode

--- examples/opensora_pku/scripts/text_condition/single-device/train_t2v_stage3.sh
+++ examples/opensora_pku/scripts/text_condition/single-device/train_t2v_stage3.sh
@@ -44,7 +44,7 @@ python opensora/train/train_t2v_diffusers.py \
     --speed_factor 1.0 \
     --drop_short_ratio 0.0 \
     --max_device_memory "59GB" \
-    --dataset_sink_mode False \
+    --dataset_sink_mode True\
     --prediction_type "v_prediction" \
     --hw_stride 32 \
     --sparse1d \
