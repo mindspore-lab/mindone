2129e28bc9da9ba780d33976542352c989bc5cce
Samit <285365963@qq.com>
2025-01-09 12:34:35 +0800

    add single stream

--- examples/hunyuanvideo/hyvideo/modules/models.py
+++ examples/hunyuanvideo/hyvideo/modules/models.py
@@ -18,6 +18,23 @@ from .embed_layers import TimestepEmbedder, PatchEmbed, TextProjection
 from .token_refiner import SingleTokenRefiner
 '''
 
+def rearrange_qkv(qkv, heads_num):
+    # qkv: shape (B L K*H*D), K=3
+    # B L (K H D) -> B L K H D -> K B L H D
+    # return q/k/v: (B L H D)
+    B, L, KHD = qkv.shape
+    H = heads_num
+    # D = head_dim # KHD // (K * self.heads_num)
+    D = KHD // (3 * H)
+    qkv = ops.reshape(qkv, (B, L, 3, H, D))
+    q, k, v = ops.split(qkv, 1, axis=2)
+    q = ops.squeeze(q, axis=2)
+    k = ops.squeeze(k, axis=2)
+    v = ops.squeeze(v, axis=2)
+
+    return q, k, v
+
+
 class MMDoubleStreamBlock(nn.Cell):
     """
     A multimodal dit block with seperate modulation for
@@ -129,20 +146,6 @@ class MMDoubleStreamBlock(nn.Cell):
     def disable_deterministic(self):
         self.deterministic = False
 
-    def rearrange_qkv(self, qkv, heads_num):
-        # B L (K H D) -> B L K H D -> K B L H D
-        B, L, KHD = qkv.shape
-        H = heads_num
-        # D = head_dim # KHD // (K * self.heads_num)
-        D = KHD // (3 * H)
-        qkv = ops.reshape(qkv, (B, L, 3, H, D))
-        q, k, v = ops.split(qkv, 1, axis=2)
-        q = ops.squeeze(q, axis=2)
-        k = ops.squeeze(k, axis=2)
-        v = ops.squeeze(v, axis=2)
-
-        return q, k, v
-
     def construct(
         self,
         img: ms.Tensor,
@@ -180,7 +183,7 @@ class MMDoubleStreamBlock(nn.Cell):
         )
         img_qkv = self.img_attn_qkv(img_modulated)
         # "B L (K H D) -> K B L H D", K=3, H=self.heads_num
-        img_q, img_k, img_v = self.rearrange_qkv(img_qkv, self.heads_num)
+        img_q, img_k, img_v = rearrange_qkv(img_qkv, self.heads_num)
 
         # Apply QK-Norm if needed
         # TODO: check whether need to cast to dtype of img_v
@@ -203,7 +206,7 @@ class MMDoubleStreamBlock(nn.Cell):
         txt_qkv = self.txt_attn_qkv(txt_modulated)
 
         # "B L (K H D) -> K B L H D", K=3, H=self.heads_num
-        txt_q, txt_k, txt_v = self.rearrange_qkv(txt_qkv, self.heads_num)
+        txt_q, txt_k, txt_v = rearrange_qkv(txt_qkv, self.heads_num)
 
         # Apply QK-Norm if needed.
         txt_q = self.txt_attn_q_norm(txt_q) # .to(txt_v)
@@ -249,3 +252,131 @@ class MMDoubleStreamBlock(nn.Cell):
         )
 
         return img, txt
+
+class MMSingleStreamBlock(nn.Cell):
+    """
+    A DiT block with parallel linear layers as described in
+    https://arxiv.org/abs/2302.05442 and adapted modulation interface.
+    Also refer to (SD3): https://arxiv.org/abs/2403.03206
+                  (Flux.1): https://github.com/black-forest-labs/flux
+    """
+
+    def __init__(
+        self,
+        hidden_size: int,
+        heads_num: int,
+        mlp_width_ratio: float = 4.0,
+        mlp_act_type: str = "gelu_tanh",
+        qk_norm: bool = True,
+        qk_norm_type: str = "rms",
+        qk_scale: float = None,
+        dtype = None,
+    ):
+        factory_kwargs = {"dtype": dtype}
+        super().__init__()
+
+        self.deterministic = False
+        self.hidden_size = hidden_size
+        self.heads_num = heads_num
+        head_dim = hidden_size // heads_num
+        mlp_hidden_dim = int(hidden_size * mlp_width_ratio)
+        self.mlp_hidden_dim = mlp_hidden_dim
+        self.scale = qk_scale or head_dim ** -0.5
+
+        # qkv and mlp_in
+        self.linear1 = nn.Dense(
+            hidden_size, hidden_size * 3 + mlp_hidden_dim,
+        )
+        # proj and mlp_out
+        self.linear2 = nn.Dense(
+            hidden_size + mlp_hidden_dim, hidden_size,
+        )
+
+        qk_norm_layer = get_norm_layer(qk_norm_type)
+        self.q_norm = (
+            qk_norm_layer(head_dim, elementwise_affine=True, eps=1e-6, **factory_kwargs)
+            if qk_norm
+            else nn.Identity()
+        )
+        self.k_norm = (
+            qk_norm_layer(head_dim, elementwise_affine=True, eps=1e-6, **factory_kwargs)
+            if qk_norm
+            else nn.Identity()
+        )
+
+        self.pre_norm = LayerNorm(
+            hidden_size, elementwise_affine=False, eps=1e-6, **factory_kwargs
+        )
+
+        self.mlp_act = get_activation_layer(mlp_act_type)()
+        self.modulation = ModulateDiT(
+            hidden_size,
+            factor=3,
+            act_layer=get_activation_layer("silu"),
+            **factory_kwargs,
+        )
+        self.hybrid_seq_parallel_attn = None
+
+        self.compute_attention = VanillaAttention(head_dim)
+
+    def enable_deterministic(self):
+        self.deterministic = True
+
+    def disable_deterministic(self):
+        self.deterministic = False
+
+    def construct(
+        self,
+        x: ms.Tensor,
+        vec: ms.Tensor,
+        txt_len: int,
+        cu_seqlens_q: Optional[ms.Tensor] = None,
+        cu_seqlens_kv: Optional[ms.Tensor] = None,
+        max_seqlen_q: Optional[int] = None,
+        max_seqlen_kv: Optional[int] = None,
+        freqs_cis: Tuple[ms.Tensor, ms.Tensor] = None,
+    ) -> ms.Tensor:
+        mod_shift, mod_scale, mod_gate = self.modulation(vec).chunk(3, axis=-1)
+        x_mod = modulate(self.pre_norm(x), shift=mod_shift, scale=mod_scale)
+        qkv, mlp = ops.split(
+            self.linear1(x_mod), [3 * self.hidden_size, self.mlp_hidden_dim], axis=-1
+        )
+
+        # q, k, v = rearrange(qkv, "B L (K H D) -> K B L H D", K=3, H=self.heads_num)
+        q, k, v = rearrange_qkv(qkv, heads_num=self.heads_num)
+
+        # Apply QK-Norm if needed.
+        q = self.q_norm(q) # .to(v)
+        k = self.k_norm(k) # .to(v)
+
+        # Apply RoPE if needed.
+        if freqs_cis is not None:
+            img_q, txt_q = q[:, :-txt_len, :, :], q[:, -txt_len:, :, :]
+            img_k, txt_k = k[:, :-txt_len, :, :], k[:, -txt_len:, :, :]
+            img_qq, img_kk = apply_rotary_emb(img_q, img_k, freqs_cis, head_first=False)
+            # assert (
+            #    img_qq.shape == img_q.shape and img_kk.shape == img_k.shape
+            # ), f"img_kk: {img_qq.shape}, img_q: {img_q.shape}, img_kk: {img_kk.shape}, img_k: {img_k.shape}"
+            img_q, img_k = img_qq, img_kk
+            q = ops.concat((img_q, txt_q), axis=1)
+            k = ops.concat((img_k, txt_k), axis=1)
+
+        # Compute attention.
+        # assert (
+        #     cu_seqlens_q.shape[0] == 2 * x.shape[0] + 1
+        # ), f"cu_seqlens_q.shape:{cu_seqlens_q.shape}, x.shape[0]:{x.shape[0]}"
+
+        # attention computation start
+        attn = self.compute_attention(
+            q,
+            k,
+            v,
+        )
+        # TODO: add FA
+        # attention computation end
+
+        # Compute activation in mlp stream, cat again and run second linear layer.
+        output = self.linear2(ops.concat((attn, self.mlp_act(mlp)), axis=2))
+        return x + apply_gate(output, gate=mod_gate)
+
+
--- examples/hunyuanvideo/tests/test_dit_block.py
+++ examples/hunyuanvideo/tests/test_dit_block.py
@@ -1,15 +1,42 @@
-import sys
+import os, sys
 import numpy as np
 from PIL import Image
 import mindspore as ms
 
 sys.path.insert(0, ".")
 
-from hyvideo.modules.models import MMDoubleStreamBlock
+from hyvideo.modules.models import MMDoubleStreamBlock, MMSingleStreamBlock
 from hyvideo.modules.attention import attention
 
 
 np.random.seed(42)
+
+(B, L, N, D) = (1, 32, 6, 48)
+L_txt = 18
+hidden_size = N*D
+
+img_shape = (B, L, N*D)
+txt_shape = (B, L_txt, N*D)
+vec_shape = (B, N*D)
+freqs_cis_shape = (L, D)
+img_ = np.random.normal(size=img_shape).astype(np.float32)
+txt_ = np.random.normal(size=txt_shape).astype(np.float32)
+vec_ = np.random.normal(size=vec_shape).astype(np.float32)
+freqs_cis_cos_ = np.random.normal(size=freqs_cis_shape).astype(np.float32)
+freqs_cis_sin_ = np.random.normal(size=freqs_cis_shape).astype(np.float32)
+
+
+def _diff_res(ms_val, pt_val, eps=1e-8):
+    abs_diff = np.fabs(ms_val - pt_val)
+    mae = abs_diff.mean()
+    max_ae = abs_diff.max()
+
+    rel_diff = abs_diff / (np.fabs(pt_val) + eps)
+    mre = rel_diff.mean()
+    max_re = rel_diff.max()
+
+    return dict(mae=mae, max_ae=max_ae, mre=mre, max_re=max_re)
+
 
 def test_attn():
     x_shape = (B, L, N, D) = (1, 32, 6, 48)
@@ -26,24 +53,10 @@ def test_attn():
 
 
 def test_dualstream_block():
-    (B, L, N, D) = (1, 32, 6, 48)
-    L_txt = 18
-    hidden_size = N*D
-
-    img_shape = (B, L, N*D)
-    txt_shape = (B, L_txt, N*D)
-    vec_shape = (B, N*D)
-    freqs_cis_shape = (L, D)
-    img = np.random.normal(size=img_shape).astype(np.float32)
-    txt = np.random.normal(size=txt_shape).astype(np.float32)
-    vec = np.random.normal(size=vec_shape).astype(np.float32)
-    freqs_cis_cos = np.random.normal(size=freqs_cis_shape).astype(np.float32)
-    freqs_cis_sin = np.random.normal(size=freqs_cis_shape).astype(np.float32)
-
-    img = ms.Tensor(img)
-    txt = ms.Tensor(txt)
-    vec = ms.Tensor(vec)
-    freqs_cis = (ms.Tensor(freqs_cis_cos), ms.Tensor(freqs_cis_sin))
+    img = ms.Tensor(img_)
+    txt = ms.Tensor(txt_)
+    vec = ms.Tensor(vec_)
+    freqs_cis = (ms.Tensor(freqs_cis_cos_), ms.Tensor(freqs_cis_sin_))
     # freqs_cis_cos = ms.Tensor(freqs_cis_cos)
     # freqs_cis_sin = ms.Tensor(freqs_cis_sin)
 
@@ -58,12 +71,38 @@ def test_dualstream_block():
     img_out, txt_out = block(img, txt, vec, freqs_cis=freqs_cis)
     # out = block(img, txt, vec, freqs_cis_cos=freqs_cis_cos, freqs_cis_sin=freqs_cis_sin)
     print(img_out.shape)
-    print(img_out.mean(), img_out.std())  # -0.00013358534 1.0066756
+    print(img_out.mean(), img_out.std())  # -0.00013358534 1.0066756 for fp32
     print(txt.shape)
     print(txt_out.mean(), txt_out.std())
 
 
+def test_singlestream_block(pt_fp: str=None):
+    img = ms.Tensor(img_)
+    txt = ms.Tensor(txt_)
+    x = ms.ops.concat([img, txt], 1)
+    vec = ms.Tensor(vec_)
+    freqs_cis = (ms.Tensor(freqs_cis_cos_), ms.Tensor(freqs_cis_sin_))
+
+    print('input sum: ', x.sum())
+    block = MMSingleStreamBlock(
+        hidden_size = hidden_size,
+        heads_num=N,
+        mlp_width_ratio=1,
+    )
+
+    out = block(x, vec, L_txt, freqs_cis=freqs_cis)
+
+    print(out.shape)
+    print(out.mean(), out.std())
+
+    if pt_fp:
+        pt_out = np.load(pt_fp)
+        diff = _diff_res(out.asnumpy(), pt_out)
+        print(diff)
+
+
 if __name__ == "__main__":
     ms.set_context(mode=0, jit_syntax_level=ms.STRICT)
-    test_dualstream_block()
     # test_attn()
+    # test_dualstream_block()
+    test_singlestream_block('tests/pt_single_stream.npy')
