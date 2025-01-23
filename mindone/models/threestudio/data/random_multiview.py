fix nram leakage during recompute

mindone/models/threestudio/data/random_multiview.py
@@ -25,15 +25,13 @@ class RandomMultiviewCameraIterableDataset(RandomCameraIterableDataset):
         self.output_columns = [
             "rays_o",
             "rays_d",
-            "mvp_mtx",
-            "camera_positions",
             "c2w",
-            "light_positions",
             "elevation",
             "azimuth",
             "camera_distances",
             "fovy",
         ]
+        self._export_light = False
 
     def __len__(self):
         return self.cfg.n_view
@@ -225,13 +223,13 @@ class RandomMultiviewCameraIterableDataset(RandomCameraIterableDataset):
         )  # FIXME: hard-coded near and far
         mvp_mtx: np.array = get_mvp_matrix(c2w, proj_mtx)
 
+        if self._export_light:
+            return (light_positions.astype(np.float32), mvp_mtx.astype(np.float32))
+
         return (
             rays_o.astype(np.float32),
             rays_d.astype(np.float32),
-            mvp_mtx.astype(np.float32),
-            camera_positions.astype(np.float32),
             c2w.astype(np.float32),
-            light_positions.astype(np.float32),
             elevation_deg.astype(np.float32),
             azimuth_deg.astype(np.float32),
             camera_distances.astype(np.float32),

mindone/models/threestudio/models/renderers/base.py
@@ -1,73 +1,34 @@
 from dataclasses import dataclass
 from typing import Any, Dict
 
-from threestudio.models.background.base import BaseBackground
-from threestudio.models.geometry.base import BaseImplicitGeometry
-from threestudio.models.materials.base import BaseMaterial
-from threestudio.utils.base import BaseModule
-
 import mindspore as ms
-from mindspore import Tensor
+from mindspore import Tensor, nn
 
 
-class Renderer(BaseModule):
+class Renderer(nn.Cell):
     @dataclass
-    class Config(BaseModule.Config):
+    class Config:
         radius: float = 1.0
 
     cfg: Config
 
-    def configure(
+    def __init__(
         self,
-        geometry: BaseImplicitGeometry,
-        material: BaseMaterial,
-        background: BaseBackground,
+        radius: float,
     ) -> None:
-        # keep references to submodules using namedtuple, avoid being registered as modules
-        @dataclass
-        class SubModules:
-            geometry: BaseImplicitGeometry
-            material: BaseMaterial
-            background: BaseBackground
-
-        self.sub_modules = SubModules(geometry, material, background)
+        # original renderer base cannot get recompute correct, nram leakage, replace the parent class with Cell
 
         # set up bounding box
-        self.bbox: Tensor
-        self.register_buffer_ms(
-            "bbox",
-            Tensor(
-                [
-                    [-self.cfg.radius, -self.cfg.radius, -self.cfg.radius],
-                    [self.cfg.radius, self.cfg.radius, self.cfg.radius],
-                ],
-                dtype=ms.float32,
-            ),
+        self.bbox = Tensor(
+            [
+                [-self.cfg.radius, -self.cfg.radius, -self.cfg.radius],
+                [self.cfg.radius, self.cfg.radius, self.cfg.radius],
+            ],
+            dtype=ms.float32,
         )
 
     def construct(self, *args, **kwargs) -> Dict[str, Any]:
         raise NotImplementedError
-
-    @property
-    def geometry(self) -> BaseImplicitGeometry:
-        return self.sub_modules.geometry
-
-    @property
-    def material(self) -> BaseMaterial:
-        return self.sub_modules.material
-
-    @property
-    def background(self) -> BaseBackground:
-        return self.sub_modules.background
-
-    def set_geometry(self, geometry: BaseImplicitGeometry) -> None:
-        self.sub_modules.geometry = geometry
-
-    def set_material(self, material: BaseMaterial) -> None:
-        self.sub_modules.material = material
-
-    def set_background(self, background: BaseBackground) -> None:
-        self.sub_modules.background = background
 
 
 class VolumeRenderer(Renderer):