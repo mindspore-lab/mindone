e461e427987ad1573195340b8a7cdf2cadc0cc17
Rustam Khadipash <16683750+hadipash@users.noreply.github.com>
2025-02-12 09:34:32 +0800

    fix weight init

--- examples/moviegen/mg/models/text_encoders/text_projector.py
+++ examples/moviegen/mg/models/text_encoders/text_projector.py
@@ -3,8 +3,6 @@ from typing import Type
 import mindspore as ms
 from mindspore import Tensor, mint, nn
 
-from mindone.models.utils import normal_, zeros_
-
 
 class TextProjector(nn.Cell):
     def __init__(
@@ -15,8 +13,6 @@ class TextProjector(nn.Cell):
         out_features: int = 6144,
         layer_norm: Type[nn.Cell] = mint.nn.LayerNorm,
         norm_eps: float = 1e-5,
-        initializer_range: float = 0.02,
-        post_init_weight: bool = True,
         dtype: ms.Type = ms.float32,
     ):
         super().__init__()
@@ -29,24 +25,6 @@ class TextProjector(nn.Cell):
 
         self.byt5_linear = nn.Dense(byt5_in_features, out_features, has_bias=False, dtype=dtype)
         self.byt5_layernorm = layer_norm((out_features,), eps=norm_eps, dtype=dtype)
-
-        self.initializer_range = initializer_range
-
-        # post-init
-        if post_init_weight:
-            self.initializer_range = initializer_range
-            self.init_weights()
-
-    def init_weights(self):
-        std = self.initializer_range
-
-        def _init_weights(module):
-            if isinstance(module, nn.Dense):
-                normal_(module.weight, mean=0.0, std=std)
-                if module.bias is not None:
-                    zeros_(module.weight)
-
-        self.apply(_init_weights)
 
     def construct(self, ul2_emb: Tensor, metaclip_emb: Tensor, byt5_emb: Tensor) -> Tensor:
         ul2_hidden_states = self.ul2_layernorm(self.ul2_linear(ul2_emb))
