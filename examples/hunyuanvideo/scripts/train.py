9596a7de73e8d9e2eb2a95e81918adb42a2cab80
Didan Deng <33117903+wtomin@users.noreply.github.com>
2025-02-13 16:16:13 +0800

    impr logging

--- examples/hunyuanvideo/scripts/train.py
+++ examples/hunyuanvideo/scripts/train.py
@@ -16,7 +16,6 @@ mindone_lib_path = os.path.abspath(os.path.join(__dir__, "../../../"))
 sys.path.append(mindone_lib_path)
 sys.path.append(os.path.join(__dir__, ".."))
 from hyvideo.acceleration import create_parallel_group
-from hyvideo.constants import PRECISION_TO_TYPE
 from hyvideo.dataset import ImageVideoDataset, bucket_split_function
 from hyvideo.diffusion.pipelines import DiffusionWithLoss
 from hyvideo.diffusion.schedulers import RFlowEvalLoss, RFlowLossWrapper
@@ -101,7 +100,7 @@ def main(args):
             **args.vae,
         )
         # vae_kwargs = {"s_ratio": s_ratio, "t_ratio": t_ratio}
-        vae_dtype = PRECISION_TO_TYPE(args.vae.precision)
+        vae_dtype = args.vae.precision
 
     else:
         logger.info("vae latent folder provided. Skipping vae initialization.")
@@ -110,7 +109,7 @@ def main(args):
     # 2.2 Llama 3
     logger.info("Transformer init")
     network = init_model(resume=args.train.resume_ckpt is not None, **args.model)
-    model_dtype = PRECISION_TO_TYPE(args.model.factor_kwargs["dtype"])
+    model_dtype = args.model.factor_kwargs["dtype"]
     if network.guidance_embed:
         embed_cfg_scale = 6.0
     else:
