7c4d85af712d4f656fdd393704f774feaf014ec0
Didan Deng <33117903+wtomin@users.noreply.github.com>
2025-02-13 17:29:51 +0800

    update zero3 script

--- examples/hunyuanvideo/scripts/hyvideo/train_t2v_zero3.sh
+++ examples/hunyuanvideo/scripts/hyvideo/train_t2v_zero3.sh
@@ -3,6 +3,7 @@ msrun --bind_core=True --worker_num=8 --local_worker_num=8 --master_port=8000 --
   scripts/train.py \
    --config configs/train/stage1_t2v_256px.yaml \
    --env.mode 1 \
+   --env.distributed True \
    --model.name "HYVideo-T/2-cfgdistill" \
    --train.settings.zero_stage 3 \
    --dataset.csv_path datasets/mixkit-100videos/video_caption_train.csv \
@@ -12,4 +13,3 @@ msrun --bind_core=True --worker_num=8 --local_worker_num=8 --master_port=8000 --
    --valid.dataset.csv_path datasets/mixkit-100videos/video_caption_test.csv \
    --valid.dataset.video_folder datasets/mixkit-100videos/mixkit \
    --valid.dataset.text_emb_folder datasets/mixkit-100videos/text_embed \
-   --use_parallel True \
