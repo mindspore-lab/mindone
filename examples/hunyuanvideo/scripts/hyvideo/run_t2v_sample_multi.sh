4cc16b98f4e6ade73beb1377b7dd41c09a074366
Didan Deng <33117903+wtomin@users.noreply.github.com>
2025-02-17 11:05:42 +0800

    use num_frames 29

--- examples/hunyuanvideo/scripts/hyvideo/run_t2v_sample_multi.sh
+++ examples/hunyuanvideo/scripts/hyvideo/run_t2v_sample_multi.sh
@@ -2,7 +2,7 @@
 msrun --bind_core=True --worker_num=8 --local_worker_num=8 --master_port=9000 --log_dir="./sample_videos/parallel_logs/" \
    sample_video.py \
     --video-size 256 256 \
-    --video-length 16 \
+    --video-length 29 \
     --infer-steps 50 \
     --prompt "The video features a black and white dog sitting upright against a plain background." \
     --flow-reverse \
