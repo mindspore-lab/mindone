correct scaling factor

examples/hunyuanvideo/hyvideo/dataset/dataset.py
@@ -185,9 +185,8 @@ class ImageVideoDataset(BaseDataset):
 
             latent_mean, latent_std = latent_mean[batch_index], latent_std[batch_index]
             vae_latent = np.random.normal(latent_mean, latent_std).astype(np.float32)
-            vae_latent = vae_latent / self._vae_scale_factor
-            if self._vae_shift_factor is not None:
-                vae_latent = vae_latent + self._vae_shift_factor
+
+            vae_latent = vae_latent * self._vae_scale_factor
             data["video"] = vae_latent
         else:
             if data["video"].lower().endswith(IMAGE_EXT):

examples/hunyuanvideo/hyvideo/diffusion/pipelines/train_pipelines.py
@@ -67,10 +67,7 @@ class DiffusionWithLoss(nn.Cell):
             return video_tokens
         with no_grad():  # (B, C, T, H, W)
             video_emb = ops.stop_gradient(self.vae.encode(video_tokens)).to(ms.float32)
-            if hasattr(self.vae.config, "shift_factor") and self.vae.config.shift_factor:
-                video_emb = video_emb / self.vae.config.scaling_factor + self.vae.config.shift_factor
-            else:
-                video_emb = video_emb / self.vae.config.scaling_factor
+            video_emb = video_emb * self.vae.config.scaling_factor
         return video_emb
 
     def construct(