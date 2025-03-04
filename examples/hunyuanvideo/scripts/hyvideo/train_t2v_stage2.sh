d55e516c16abc54211f6388ade461ca698c1d463
Didan Deng <33117903+wtomin@users.noreply.github.com>
2025-03-04 17:33:53 +0800

    update training configs

--- examples/hunyuanvideo/scripts/hyvideo/train_t2v_stage2.sh
+++ examples/hunyuanvideo/scripts/hyvideo/train_t2v_stage2.sh
@@ -1,10 +1,8 @@
 export ASCEND_RT_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
-msrun --bind_core=True --worker_num=8 --local_worker_num=8 --master_port=8000 --log_dir="./parallel_logs" \
+msrun --bind_core=True --worker_num=8 --local_worker_num=8 --master_port=8000 --log_dir="./stage2_parallel_logs" \
   scripts/train.py \
-   --config configs/train/stage1_t2v_256px.yaml \
-   --env.mode 0 \
+   --config configs/train/stage2_t2v_512px.yaml \
    --env.distributed True \
-   --model.name "HYVideo-T/2-cfgdistill" \
    --train.settings.zero_stage 3 \
    --dataset.csv_path datasets/mixkit-100videos/video_caption_train.csv \
    --dataset.video_folder datasets/mixkit-100videos/mixkit \
