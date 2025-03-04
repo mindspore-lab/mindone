e1501809796db583e9417682035d69173764f3d5
Chaoran Wei <77485245+wcrzlh@users.noreply.github.com>
2025-03-04 16:30:31 +0800

    feat(examples/janus): support pure_text training and generation pipe

--- examples/janus/inference_pure_text.py
+++ examples/janus/inference_pure_text.py
@@ -1,5 +1,8 @@
+import os
+import sys
+
 import urllib3
-import os, sys
+
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 import mindspore as ms
@@ -8,25 +11,26 @@ __dir__ = os.path.dirname(os.path.abspath(__file__))
 mindone_lib_path = os.path.abspath(os.path.join(__dir__, "../../"))
 sys.path.insert(0, mindone_lib_path)
 
-from transformers import AutoConfig, AutoTokenizer
-from mindone.transformers import LlamaForCausalLM
 from janus.models import VLChatProcessor
+from transformers import AutoConfig, AutoTokenizer
+
+from mindone.transformers import LlamaForCausalLM
 
 tokenizer = AutoTokenizer.from_pretrained("deepseek-ai/Janus-Pro-1B")
 
 system_prompt = (
-        "You are a helpful language and vision assistant. "
-        "You are able to understand the visual content that the user provides, "
-        "and assist the user with a variety of tasks using natural language."
-    )
+    "You are a helpful language and vision assistant. "
+    "You are able to understand the visual content that the user provides, "
+    "and assist the user with a variety of tasks using natural language."
+)
 input_text = "Do mitochondria play a role in remodelling lace plant leaves during programmed cell death?"
 conversations = [
-            {
-                "role": "<|User|>",
-                "content": input_text,
-            },
-            {"role": "<|Assistant|>", "content": ""},
-        ]
+    {
+        "role": "<|User|>",
+        "content": input_text,
+    },
+    {"role": "<|Assistant|>", "content": ""},
+]
 vl_chat_processor = VLChatProcessor = VLChatProcessor.from_pretrained("deepseek-ai/Janus-Pro-1B")
 sft_format = vl_chat_processor.apply_sft_template_for_multi_turn_prompts(
     conversations=conversations,
@@ -44,21 +48,21 @@ language_config = config.language_config
 language_config._attn_implementation = "eager"
 model = LlamaForCausalLM.from_pretrained(
     "results_graphs/checkpoint-10000",
-    trust_remote_code = True,
+    trust_remote_code=True,
 )
 
 inputs_embeds = model.get_input_embeddings()(inputs["input_ids"])
 
 outputs = model.generate(
-        inputs_embeds=inputs_embeds,
-        attention_mask=inputs["attention_mask"],
-        pad_token_id=tokenizer.eos_token_id,
-        bos_token_id=tokenizer.bos_token_id,
-        eos_token_id=tokenizer.eos_token_id,
-        max_new_tokens=1024,
-        do_sample=False,
-        use_cache=True,
-    )
+    inputs_embeds=inputs_embeds,
+    attention_mask=inputs["attention_mask"],
+    pad_token_id=tokenizer.eos_token_id,
+    bos_token_id=tokenizer.bos_token_id,
+    eos_token_id=tokenizer.eos_token_id,
+    max_new_tokens=1024,
+    do_sample=False,
+    use_cache=True,
+)
 
 answer = tokenizer.decode(outputs[0].asnumpy().tolist(), skip_special_tokens=True)
 
--- examples/janus/train_pure_text.py
+++ examples/janus/train_pure_text.py
@@ -1,31 +1,36 @@
-from datasets import load_dataset
+import os
+import sys
 
 import urllib3
-import os, sys
+from datasets import load_dataset
+
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 import numpy as np
+
 import mindspore as ms
 
 __dir__ = os.path.dirname(os.path.abspath(__file__))
 mindone_lib_path = os.path.abspath(os.path.join(__dir__, "../../"))
 sys.path.insert(0, mindone_lib_path)
 
+from janus.models import MultiModalityCausalLM, VLChatProcessor
 from transformers import AutoConfig
+
 from mindone.transformers.trainer import Trainer
 from mindone.transformers.training_args import TrainingArguments
-from janus.models import MultiModalityCausalLM, VLChatProcessor
 
-ms.set_context(mode = 0)
+ms.set_context(mode=0)
 
 dataset = load_dataset("pubmed_qa", "pqa_labeled")
 system_prompt = (
-        "You are a helpful language and vision assistant. "
-        "You are able to understand the visual content that the user provides, "
-        "and assist the user with a variety of tasks using natural language."
-    )
+    "You are a helpful language and vision assistant. "
+    "You are able to understand the visual content that the user provides, "
+    "and assist the user with a variety of tasks using natural language."
+)
 vl_chat_processor = VLChatProcessor = VLChatProcessor.from_pretrained("deepseek-ai/Janus-Pro-1B")
 tokenizer = vl_chat_processor.tokenizer
+
 
 def preprocess(examples, BatchInfo):
     for example in examples:
@@ -40,16 +45,16 @@ def preprocess(examples, BatchInfo):
             {"role": "<|Assistant|>", "content": answer},
         ]
         sft_format = vl_chat_processor.apply_sft_template_for_multi_turn_prompts(
-                conversations=conversations,
-                system_prompt=system_prompt,
-            )
+            conversations=conversations,
+            system_prompt=system_prompt,
+        )
 
         inputs = tokenizer(
-                    sft_format,
-                    padding="max_length",
-                    max_length=512,
-                    truncation=True,
-                 )
+            sft_format,
+            padding="max_length",
+            max_length=512,
+            truncation=True,
+        )
 
         answer_tokens = tokenizer(
             answer,
@@ -74,35 +79,36 @@ def preprocess(examples, BatchInfo):
 
     yield {"input_ids": inputs["input_ids"], "attention_mask": inputs["attention_mask"], "labels": labels}
 
+
 config = AutoConfig.from_pretrained("deepseek-ai/Janus-Pro-1B")
 language_config = config.language_config
 language_config._attn_implementation = "eager"
 model = MultiModalityCausalLM.from_pretrained(
     "deepseek-ai/Janus-Pro-1B",
-    language_config = language_config,
-    trust_remote_code = True,
-    revision = "refs/pr/1",
+    language_config=language_config,
+    trust_remote_code=True,
+    revision="refs/pr/1",
 )
 
 model = model.language_model
 model.set_train()
 
 training_args = TrainingArguments(
-    output_dir = "results_graphs",
-    per_device_train_batch_size = 1,
-    num_train_epochs = 10,
-    weight_decay = 0.01,
-    learning_rate = 1e-5,
-    logging_dir = "./logs_graphs",
-    remove_unused_columns = False,
+    output_dir="results_graphs",
+    per_device_train_batch_size=1,
+    num_train_epochs=10,
+    weight_decay=0.01,
+    learning_rate=1e-5,
+    logging_dir="./logs_graphs",
+    remove_unused_columns=False,
 )
 
 trainer = Trainer(
-    model = model,
-    args = training_args,
-    train_dataset = dataset["train"],
-    tokenizer = tokenizer,
-    data_collator = preprocess,
+    model=model,
+    args=training_args,
+    train_dataset=dataset["train"],
+    tokenizer=tokenizer,
+    data_collator=preprocess,
 )
 
 trainer.train()
