e97df7d14d63dcd55d69ae9d54d2ab886a032cfd
Didan Deng <33117903+wtomin@users.noreply.github.com>
2025-03-10 15:14:59 +0800

    run vae reconstruction

--- examples/hunyuanvideo-i2v/scripts/vae/recon_image.sh
+++ examples/hunyuanvideo-i2v/scripts/vae/recon_image.sh
@@ -1,7 +1,7 @@
 python scripts/run_vae.py \
     --input-type image \
-    --image-path "path/to/input_image.jpg" \
-    --output-path "path/to/output_directory" \
+    --image-path "./assets/demo/i2v/imgs/0.jpg" \
+    --output-path "outputs/" \
     --rec-path "reconstructed_image.jpg" \
     --height 336 \
     --width 336 \
