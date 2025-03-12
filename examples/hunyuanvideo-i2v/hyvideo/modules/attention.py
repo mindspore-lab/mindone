980f98afb8662751b885fbec176442c79791357e
Didan Deng <33117903+wtomin@users.noreply.github.com>
2025-03-12 15:53:12 +0800

    FA fix bf16

--- examples/hunyuanvideo-i2v/hyvideo/modules/attention.py
+++ examples/hunyuanvideo-i2v/hyvideo/modules/attention.py
@@ -73,11 +73,13 @@ class FlashAttention(nn.Cell):
         q = ops.transpose(q, (0, 2, 1, 3))
         k = ops.transpose(k, (0, 2, 1, 3))
         v = ops.transpose(v, (0, 2, 1, 3))
-
+        ori_dtype = q.dtype
         # in ms FA, 0 - retain, 1 - discard. shape: `(B, N1, S1, S2)` or `(B, 1, S1, S2)`, dtype: bool or uint8
         if mask is not None:
             mask = ops.logical_not(mask)
-        _, _, _, out = self.flash_attention(q, k, v, None, None, None, mask)
+        _, _, _, out = self.flash_attention(
+            q.to(ms.bfloat16), k.to(ms.bfloat16), v.to(ms.bfloat16), None, None, None, mask
+        )
 
         # (B N S D) -> (B S N D)
         out = ops.transpose(out, (0, 2, 1, 3))
@@ -85,7 +87,7 @@ class FlashAttention(nn.Cell):
         B, S, N, D = out.shape
         out = out.reshape(B, S, -1)
 
-        return out
+        return out.to(ori_dtype)
 
 
 class FlashAttentionVarLen(nn.Cell):
@@ -122,9 +124,9 @@ class FlashAttentionVarLen(nn.Cell):
         v = v.reshape((-1, heads_num, head_dim))
 
         _, _, _, out = self.flash_attention(
-            q.to(ms.bloat16),
-            k.to(ms.bloat16),
-            v.to(ms.bloat16),
+            q.to(ms.bfloat16),
+            k.to(ms.bfloat16),
+            v.to(ms.bfloat16),
             actual_seq_qlen=actual_seq_qlen,
             actual_seq_kvlen=actual_seq_kvlen,
         )
