7fb0b808f50c03cbfb21f6d80976344212872140
Chaoran Wei <77485245+wcrzlh@users.noreply.github.com>
2025-03-19 09:55:38 +0800

    fix(transformers): fix bugs for transformers upgrade

--- mindone/transformers/__init__.py
+++ mindone/transformers/__init__.py
@@ -52,7 +52,15 @@ from .models.glm import (
     GlmModel,
     GlmPreTrainedModel,
 )
-from .models.gpt2 import GPT2LMHeadModel
+from .models.gpt2 import (
+    GPT2DoubleHeadsModel,
+    GPT2ForQuestionAnswering,
+    GPT2ForSequenceClassification,
+    GPT2ForTokenClassification,
+    GPT2LMHeadModel,
+    GPT2Model,
+    GPT2PreTrainedModel,
+)
 from .models.llama import LlamaForCausalLM, LlamaForSequenceClassification, LlamaModel, LlamaPreTrainedModel
 from .models.llava import LlavaConfig, LlavaForConditionalGeneration
 from .models.mt5 import (
