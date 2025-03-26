71b029857e845bee3c83a2bb03050e6b0f170f39
Didan Deng <33117903+wtomin@users.noreply.github.com>
2025-03-26 16:26:57 +0800

    use nn.GELU

--- mindone/models/dit.py
+++ mindone/models/dit.py
@@ -28,7 +28,7 @@ __all__ = [
 ]
 
 
-class GELU(mint.nn.GELU):
+class GELU(nn.GELU):
     def __init__(self, approximate: str = "none"):
         if approximate == "none":
             super().__init__(False)
