aa8b8334f8241fbb40a471a347d76a9b129cdf40
Didan Deng <33117903+wtomin@users.noreply.github.com>
2025-03-27 12:01:50 +0800

    reset parallel init

--- mindone/models/modules/parallel/__init__.py
+++ mindone/models/modules/parallel/__init__.py
@@ -9,28 +9,9 @@ PARALLEL_MODULES = {
     nn.Conv2d: Conv2d,
     nn.Conv3d: Conv3d,
     nn.Dense: Dense,
+    mint.nn.Conv2d: Mint_Conv2d,
+    mint.nn.Conv3d: Mint_Conv3d,
+    mint.nn.Linear: Linear,
 }
-
-try:
-    from mindspore.mint.nn import Conv2d
-
-    PARALLEL_MODULES[mint.nn.Conv2d] = Mint_Conv2d
-except ImportError:
-    pass
-
-try:
-    from mindspore.mint.nn import Conv3d
-
-    PARALLEL_MODULES[mint.nn.Conv3d] = Mint_Conv3d
-except ImportError:
-    pass
-
-try:
-    from mindspore.mint.nn import Linear as m_Linear
-
-    PARALLEL_MODULES[mint.nn.Linear] = Linear
-except ImportError:
-    pass
-
 
 __all__ = ["Conv1d", "Conv2d", "Conv3d", "Mint_Conv2d", "Mint_Conv3d", "Dense", "Linear"]
