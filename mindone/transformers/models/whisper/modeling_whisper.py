8f6129c808f61a5b5d17750412c8418c16ba74e5
litingyu <605979840@qq.com>
2025-04-10 09:37:06 +0800

    fix

--- mindone/transformers/models/whisper/modeling_whisper.py
+++ mindone/transformers/models/whisper/modeling_whisper.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""PyTorch Whisper model."""
+"""Mindspore Whisper model."""
 
 import math
 from typing import Optional, Tuple, Union
@@ -21,8 +21,6 @@ import numpy as np
 from transformers.models.whisper.configuration_whisper import WhisperConfig
 
 import mindspore
-
-# import torch.utils.checkpoint
 from mindspore import Parameter, mint, nn, ops
 from mindspore.mint.nn import CrossEntropyLoss
 
@@ -379,119 +377,10 @@ class WhisperAttention(nn.Cell):
         return attn_output, attn_weights_reshaped, past_key_value
 
 
-class WhisperSdpaAttention(WhisperAttention):
-    # Copied from transformers.models.bart.modeling_bart.BartSdpaAttention.forward with BART->whisper, Bart->Whisper
-    def construct(
-        self,
-        hidden_states: mindspore.tensor,
-        key_value_states: Optional[mindspore.tensor] = None,
-        past_key_value: Optional[Tuple[mindspore.tensor]] = None,
-        attention_mask: Optional[mindspore.tensor] = None,
-        layer_head_mask: Optional[mindspore.tensor] = None,
-        output_attentions: bool = False,
-    ) -> Tuple[mindspore.tensor, Optional[mindspore.tensor], Optional[Tuple[mindspore.tensor]]]:
-        """Input shape: Batch x Time x Channel"""
-        if output_attentions or layer_head_mask is not None:
-            # TODO: Improve this warning with e.g. `model.config._attn_implementation = "manual"` once this is implemented.
-            logger.warning_once(
-                "WhisperModel is using WhisperSdpaAttention, but `torch.nn.functional.scaled_dot_product_attention` does not support\
-                      `output_attentions=True` or `layer_head_mask` not None. Falling back to the manual attention"
-                ' implementation, but specifying the manual implementation will be required from Transformers version v5.0.0 onwards.\
-                      This warning can be removed using the argument `attn_implementation="eager"` when loading the model.'
-            )
-            return super().forward(
-                hidden_states,
-                key_value_states=key_value_states,
-                past_key_value=past_key_value,
-                attention_mask=attention_mask,
-                layer_head_mask=layer_head_mask,
-                output_attentions=output_attentions,
-            )
-
-        # if key_value_states are provided this layer is used as a cross-attention layer
-        # for the decoder
-        is_cross_attention = key_value_states is not None
-
-        bsz, tgt_len, _ = hidden_states.shape
-
-        # get query proj
-        query_states = self.q_proj(hidden_states)
-        # get key, value proj
-        # `past_key_value[0].shape[2] == key_value_states.shape[1]`
-        # is checking that the `sequence_length` of the `past_key_value` is the same as
-        # the provided `key_value_states` to support prefix tuning
-        if (
-            is_cross_attention
-            and past_key_value is not None
-            and past_key_value[0].shape[2] == key_value_states.shape[1]
-        ):
-            # reuse k,v, cross_attentions
-            key_states = past_key_value[0]
-            value_states = past_key_value[1]
-        elif is_cross_attention:
-            # cross_attentions
-            key_states = self._shape(self.k_proj(key_value_states), -1, bsz)
-            value_states = self._shape(self.v_proj(key_value_states), -1, bsz)
-        elif past_key_value is not None:
-            # reuse k, v, self_attention
-            key_states = self._shape(self.k_proj(hidden_states), -1, bsz)
-            value_states = self._shape(self.v_proj(hidden_states), -1, bsz)
-            key_states = mint.cat([past_key_value[0], key_states], dim=2)
-            value_states = mint.cat([past_key_value[1], value_states], dim=2)
-        else:
-            # self_attention
-            key_states = self._shape(self.k_proj(hidden_states), -1, bsz)
-            value_states = self._shape(self.v_proj(hidden_states), -1, bsz)
-
-        if self.is_decoder:
-            # if cross_attention save Tuple(mindspore.tensor, mindspore.tensor) of all cross attention key/value_states.
-            # Further calls to cross_attention layer can then reuse all cross-attention
-            # key/value_states (first "if" case)
-            # if uni-directional self-attention (decoder) save Tuple(mindspore.tensor, mindspore.tensor) of
-            # all previous decoder key/value_states. Further calls to uni-directional self-attention
-            # can concat previous decoder key/value_states to current projected key/value_states (third "elif" case)
-            # if encoder bi-directional self-attention `past_key_value` is always `None`
-            past_key_value = (key_states, value_states)
-
-        query_states = self._shape(query_states, tgt_len, bsz)
-
-        # We dispatch to SDPA's Flash Attention or Efficient kernels via this `is_causal` if statement instead of an inline conditional assignment
-        # in SDPA to support both torch.compile's dynamic shapes and full graph options. An inline conditional prevents dynamic shapes from compiling.
-        # The tgt_len > 1 is necessary to match with AttentionMaskConverter.to_causal_4d that does not create a causal mask in case tgt_len == 1.
-        is_causal = True if self.is_causal and attention_mask is None and tgt_len > 1 else False
-
-        # NOTE: SDPA with memory-efficient backend is currently (torch==2.1.2) bugged when using non-contiguous inputs and a custom attn_mask,
-        # but we are fine here as `_shape` do call `.contiguous()`. Reference: https://github.com/pytorch/pytorch/issues/112577
-        attn_output = mint.nn.functional.scaled_dot_product_attention(
-            query_states,
-            key_states,
-            value_states,
-            attn_mask=attention_mask,
-            dropout_p=self.dropout if self.training else 0.0,
-            is_causal=is_causal,
-        )
-
-        if attn_output.shape != (bsz, self.num_heads, tgt_len, self.head_dim):
-            raise ValueError(
-                f"`attn_output` should be of size {(bsz, self.num_heads, tgt_len, self.head_dim)}, but is"
-                f" {attn_output.shape}"
-            )
-
-        attn_output = attn_output.transpose(1, 2)
-
-        # Use the `embed_dim` from the config (stored in the class) rather than `hidden_state` because `attn_output` can be
-        # partitioned across GPUs when using tensor-parallelism.
-        attn_output = attn_output.reshape(bsz, tgt_len, self.embed_dim)
-
-        attn_output = self.out_proj(attn_output)
-
-        return attn_output, None, past_key_value
-
-
 WHISPER_ATTENTION_CLASSES = {
     "eager": WhisperAttention,
     # "flash_attention_2": WhisperFlashAttention2,
-    "sdpa": WhisperSdpaAttention,
+    # "sdpa": WhisperSdpaAttention,
 }
 
 
@@ -696,6 +585,9 @@ class WhisperPreTrainedModel(MSPreTrainedModel):
     _supports_flash_attn_2 = True
     _supports_sdpa = True
 
+    def _supports_default_dynamic_cache(self):
+        return True
+
     def _init_weights(self, module):
         std = self.config.init_std
         if isinstance(module, (mint.nn.Linear, nn.Conv1d)):
@@ -707,7 +599,6 @@ class WhisperPreTrainedModel(MSPreTrainedModel):
             if module.padding_idx is not None:
                 module.embedding_table.data[module.padding_idx].zero_()
         elif isinstance(module, WhisperEncoder):
-            # with torch.no_grad():
             embed_positions = module.embed_positions.embedding_table
             embed_positions.copy_(sinusoids(*embed_positions.shape))
 
@@ -1218,8 +1109,8 @@ class WhisperModel(WhisperPreTrainedModel):
 
         Example:
          ```python
-         >>> import torch
-         >>> from transformers import AutoFeatureExtractor, WhisperModel
+         >>> import mindspore
+         >>> from mindone.transformers import AutoFeatureExtractor, WhisperModel
          >>> from datasets import load_dataset
 
          >>> model = WhisperModel.from_pretrained("openai/whisper-base")
@@ -1353,7 +1244,7 @@ class WhisperForConditionalGeneration(WhisperGenerationMixin, WhisperPreTrainedM
 
         ```python
         >>> import mindspore
-        >>> from mindone.transformers import AutoProcessor, WhisperForConditionalGeneration
+        >>> from mindone.transformers import WhiperProcessor, WhisperForConditionalGeneration
         >>> from datasets import load_dataset
 
         >>> processor = AutoProcessor.from_pretrained("openai/whisper-large-v3")
@@ -1364,9 +1255,9 @@ class WhisperForConditionalGeneration(WhisperGenerationMixin, WhisperPreTrainedM
         >>> inputs = processor(ds[0]["audio"]["array"], return_tensors="np")
         >>> input_features = inputs.input_features
 
-        >>> generated_ids = model.generate(inputs=input_features)
+        >>> generated_ids = model.generate(inputs=mindspore.tensor(input_features))
 
-        >>> transcription = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+        >>> transcription = processor.batch_decode(generated_ids.numpy(), skip_special_tokens=True)[0]
         >>> transcription
         ' Mr. Quilter is the apostle of the middle classes, and we are glad to welcome his gospel.'
         ```"""
@@ -1591,8 +1482,8 @@ class WhisperForCausalLM(WhisperPreTrainedModel):
         Example:
 
         ```python
-        >>> from transformers import WhisperForCausalLM, WhisperForConditionalGeneration, WhisperProcessor
-        >>> import torch
+        >>> from mindone.transformers import WhisperForCausalLM, WhisperForConditionalGeneration, WhisperProcessor
+        >>> import mindspore
         >>> from datasets import load_dataset
 
         >>> processor = WhisperProcessor.from_pretrained("openai/whisper-large-v2")
@@ -1743,8 +1634,8 @@ class WhisperForAudioClassification(WhisperPreTrainedModel):
         Example:
 
         ```python
-        >>> import torch
-        >>> from transformers import AutoFeatureExtractor, WhisperForAudioClassification
+        >>> import mindspore
+        >>> from mindone.transformers import AutoFeatureExtractor, WhisperForAudioClassification
         >>> from datasets import load_dataset
 
         >>> feature_extractor = AutoFeatureExtractor.from_pretrained("sanchit-gandhi/whisper-medium-fleurs-lang-id")
@@ -1758,10 +1649,9 @@ class WhisperForAudioClassification(WhisperPreTrainedModel):
         ... )
         >>> input_features = inputs.input_features
 
-        >>> with torch.no_grad():
-        ...     logits = model(input_features).logits
+        >>> logits = model(input_features).logits
 
-        >>> predicted_class_ids = torch.argmax(logits).item()
+        >>> predicted_class_ids = mindspore.mint.argmax(logits).item()
         >>> predicted_label = model.config.id2label[predicted_class_ids]
         >>> predicted_label
         'Afrikaans'
