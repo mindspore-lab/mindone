cd8b8582f67ab778241587e2f859638d1e8756db
Mark-Zhou <65532315+Mark-ZhouWX@users.noreply.github.com>
2025-05-14 16:08:51 +0800

    Move transformers from mindway to mindone (#998)
    * move transformers from mindway
    
    * fix pre-commit check
    
    * add qwen2-vl ut
    
    * fix naming typo
    
    * fix gemma ut
    
    * fix random seed for ut
    
    * fix levit load weight in UT
    
    * revise threshold for ut after fix random seed
    
    * fix bugs
    
    ---------
    
    Co-authored-by: Mark-ZhouWX <zhouwuxing000@gmail.com>
    Co-authored-by: chenyingshu <yingshu2008@gmail.com>

--- mindone/transformers/modeling_outputs.py
+++ mindone/transformers/modeling_outputs.py
@@ -608,6 +608,55 @@ class CausalLMOutputWithCrossAttentions(ModelOutput):
 
 
 @dataclass
+class ImageClassifierOutputWithNoAttention(ModelOutput):
+    """
+    Base class for outputs of image classification models.
+
+    Args:
+        loss (`ms.Tensor` of shape `(1,)`, *optional*, returned when `labels` is provided):
+            Classification (or regression if config.num_labels==1) loss.
+        logits (`ms.Tensor` of shape `(batch_size, config.num_labels)`):
+            Classification (or regression if config.num_labels==1) scores (before SoftMax).
+        hidden_states (`tuple(ms.Tensor)`, *optional*, returned when `output_hidden_states=True` is passed or when `config.output_hidden_states=True`):
+            Tuple of `ms.Tensor` (one for the output of the embeddings, if the model has an embedding layer, +
+            one for the output of each stage) of shape `(batch_size, num_channels, height, width)`. Hidden-states (also
+            called feature maps) of the model at the output of each stage.
+    """
+
+    loss: Optional[ms.Tensor] = None
+    logits: Optional[ms.Tensor] = None
+    hidden_states: Optional[Tuple[ms.Tensor, ...]] = None
+
+
+@dataclass
+class ImageClassifierOutput(ModelOutput):
+    """
+    Base class for outputs of image classification models.
+
+    Args:
+        loss (`ms.Tensor` of shape `(1,)`, *optional*, returned when `labels` is provided):
+            Classification (or regression if config.num_labels==1) loss.
+        logits (`ms.Tensor` of shape `(batch_size, config.num_labels)`):
+            Classification (or regression if config.num_labels==1) scores (before SoftMax).
+        hidden_states (`tuple(ms.Tensor)`, *optional*, returned when `output_hidden_states=True` is passed or when `config.output_hidden_states=True`):
+            Tuple of `ms.Tensor` (one for the output of the embeddings, if the model has an embedding layer, +
+            one for the output of each stage) of shape `(batch_size, sequence_length, hidden_size)`. Hidden-states
+            (also called feature maps) of the model at the output of each stage.
+        attentions (`tuple(ms.Tensor)`, *optional*, returned when `output_attentions=True` is passed or when `config.output_attentions=True`):
+            Tuple of `ms.Tensor` (one for each layer) of shape `(batch_size, num_heads, patch_size,
+            sequence_length)`.
+
+            Attentions weights after the attention softmax, used to compute the weighted average in the self-attention
+            heads.
+    """
+
+    loss: Optional[ms.Tensor] = None
+    logits: Optional[ms.Tensor] = None
+    hidden_states: Optional[Tuple[ms.Tensor, ...]] = None
+    attentions: Optional[Tuple[ms.Tensor, ...]] = None
+
+
+@dataclass
 class DepthEstimatorOutput(ModelOutput):
     """
     Base class for outputs of depth estimation models.
