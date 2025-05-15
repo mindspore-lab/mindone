c26de13b03620e84fb15ea58ea1df5941f457414
liuchuting <liuchuting1@huawei.com>
2025-05-15 11:56:35 +0800

    Add bigbird&bigbird_pegasus

--- mindone/transformers/__init__.py
+++ mindone/transformers/__init__.py
@@ -29,6 +29,26 @@ from .models.bert import (
     BertModel,
     BertPreTrainedModel,
 )
+from .models.big_bird import (
+    BigBirdForCausalLM,
+    BigBirdForMaskedLM,
+    BigBirdForMultipleChoice,
+    BigBirdForPreTraining,
+    BigBirdForQuestionAnswering,
+    BigBirdForSequenceClassification,
+    BigBirdForTokenClassification,
+    BigBirdLayer,
+    BigBirdModel,
+    BigBirdPreTrainedModel,
+)
+from .models.bigbird_pegasus import (
+    BigBirdPegasusForCausalLM,
+    BigBirdPegasusForConditionalGeneration,
+    BigBirdPegasusForQuestionAnswering,
+    BigBirdPegasusForSequenceClassification,
+    BigBirdPegasusModel,
+    BigBirdPegasusPreTrainedModel,
+)
 from .models.bit import BitBackbone
 from .models.blip_2 import (
     Blip2ForConditionalGeneration,
---
+++ mindone/transformers/models/big_bird/__init__.py
@@ -0,0 +1,1 @@
+from .modeling_big_bird import *
