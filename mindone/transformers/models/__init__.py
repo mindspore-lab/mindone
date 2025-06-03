19034dba7c95c1e82aab940af25cfd89ebbee01b
liuchuting <liuchuting1@huawei.com>
2025-06-03 18:02:08 +0800

    Add camembert model

--- mindone/transformers/models/__init__.py
+++ mindone/transformers/models/__init__.py
@@ -3,6 +3,7 @@ from . import (
     bert,
     bit,
     blip_2,
+    camembert,
     clap,
     clip,
     dpt,
---
+++ mindone/transformers/models/camembert/__init__.py
@@ -0,0 +1,10 @@
+from .modeling_camembert import (
+    CamembertForCausalLM,
+    CamembertForMaskedLM,
+    CamembertForMultipleChoice,
+    CamembertForQuestionAnswering,
+    CamembertForSequenceClassification,
+    CamembertForTokenClassification,
+    CamembertModel,
+    CamembertPreTrainedModel,
+)
