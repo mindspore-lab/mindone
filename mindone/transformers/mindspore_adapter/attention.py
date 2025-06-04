702496b7a53400660a819fee6b86b0a1872352d7
liuchuting <liuchuting1@huawei.com>
2025-06-04 15:47:27 +0800

    Fix flash attention

--- mindone/transformers/mindspore_adapter/attention.py
+++ mindone/transformers/mindspore_adapter/attention.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 import mindspore as ms
-from mindspore import nn, ops
+from mindspore import mint, nn, ops
 from mindspore.ops.operations.nn_ops import FlashAttentionScore as _FlashAttention
 
 DTYPE_FP16_MIN = float(np.finfo(np.float16).min)
@@ -15,9 +15,8 @@ def scaled_dot_product_attention(query, key, value, attn_mask=None, dtype=None):
 
     if attn_mask is not None:
         if attn_mask.dtype == ms.bool_:
-            attn_mask = attn_mask.to(ms.float32)
-            attn_mask = attn_mask.masked_fill((1 - attn_mask).to(ms.bool_), DTYPE_FP16_MIN)
-        attn_mask = attn_mask.to(query.dtype)
+            attn_mask = mint.logical_not(attn_mask).to(query.dtype)
+            attn_mask = attn_mask.masked_fill(attn_mask.to(ms.bool_), DTYPE_FP16_MIN)
 
         attn_weight = ops.softmax(
             ops.matmul(query, key.swapaxes(-2, -1)) / (query.shape[-1] ** 0.5) + attn_mask, axis=-1, dtype=ms.float32
