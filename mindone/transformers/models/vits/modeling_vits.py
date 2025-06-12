ce42e6ef99729099be1cdee494e6aa7083b90374
litingyu <605979840@qq.com>
2025-06-12 09:57:16 +0800

    fix

--- mindone/transformers/models/vits/modeling_vits.py
+++ mindone/transformers/models/vits/modeling_vits.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""PyTorch VITS model."""
+"""MindSpore VITS model."""
 
 import math
 from dataclasses import dataclass
