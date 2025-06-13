10ef0bfc791949c16fd7d34aea2db7480f799136
Didan Deng <33117903+wtomin@users.noreply.github.com>
2025-06-13 16:54:29 +0800

    mask predict

---
+++ examples/sam2/predict_mask.py
@@ -0,0 +1,65 @@
+"""
+Before running this script, please download the images via:
+
+mkdir -p images
+wget -P images https://raw.githubusercontent.com/facebookresearch/sam2/main/notebooks/images/cars.jpg
+
+Then run `python predict_mask.py`
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+from sam2.automatic_mask_generator import SAM2AutomaticMaskGenerator
+from sam2.build_sam import build_sam2
+
+np.random.seed(3)
+
+
+def show_anns(anns, borders=True):
+    if len(anns) == 0:
+        return
+    sorted_anns = sorted(anns, key=(lambda x: x["area"]), reverse=True)
+    ax = plt.gca()
+    ax.set_autoscale_on(False)
+
+    img = np.ones((sorted_anns[0]["segmentation"].shape[0], sorted_anns[0]["segmentation"].shape[1], 4))
+    img[:, :, 3] = 0
+    for ann in sorted_anns:
+        m = ann["segmentation"]
+        color_mask = np.concatenate([np.random.random(3), [0.5]])
+        img[m] = color_mask
+        if borders:
+            import cv2
+
+            contours, _ = cv2.findContours(m.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+            # Try to smooth contours
+            contours = [cv2.approxPolyDP(contour, epsilon=0.01, closed=True) for contour in contours]
+            cv2.drawContours(img, contours, -1, (0, 0, 1, 0.4), thickness=1)
+
+    ax.imshow(img)
+
+
+def main(args):
+    sam2_checkpoint = "../checkpoints/sam2.1_hiera_large.pt"
+    model_cfg = "configs/sam2.1/sam2.1_hiera_l.yaml"
+
+    sam2 = build_sam2(model_cfg, sam2_checkpoint, apply_postprocessing=False)
+
+    mask_generator = SAM2AutomaticMaskGenerator(sam2)
+    image_path = args.image_path
+    image = Image.open(image_path)
+    image = np.array(image.convert("RGB"))
+
+    masks = mask_generator.generate(image)
+    print(len(masks))
+    print(masks[0].keys())
+
+    plt.figure(figsize=(20, 20))
+    plt.imshow(image)
+    show_anns(masks)
+    plt.axis("off")
+    fig_path = image_path.replace(".jpg", "_mask.jpg")
+    plt.savefig(fig_path)
+    print(f"save to {fig_path}")
+    plt.show()
