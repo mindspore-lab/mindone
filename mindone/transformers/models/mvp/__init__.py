44be12375667700c5486bd3c62c85d81102ed632
liuchuting <liuchuting1@huawei.com>
2025-06-16 16:53:45 +0800

    Add mvp model
    clean code
    
    Fix

---
+++ mindone/transformers/models/mvp/__init__.py
@@ -0,0 +1,8 @@
+from .modeling_mvp import (
+    MvpForCausalLM,
+    MvpForConditionalGeneration,
+    MvpForQuestionAnswering,
+    MvpForSequenceClassification,
+    MvpModel,
+    MvpPreTrainedModel,
+)
