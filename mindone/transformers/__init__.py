ad6aa2db87e11aa5e12fd232e01481f81a2dadf1
liuchuting <liuchuting1@huawei.com>
2025-06-16 16:57:29 +0800

    Add stablelm model
    clean code
    
    fix

--- mindone/transformers/__init__.py
+++ mindone/transformers/__init__.py
@@ -282,6 +282,13 @@ from .models.speecht5 import (
     SpeechT5Model,
     SpeechT5PreTrainedModel,
 )
+from .models.stablelm import (
+    StableLmForCausalLM,
+    StableLmForSequenceClassification,
+    StableLmForTokenClassification,
+    StableLmModel,
+    StableLmPreTrainedModel,
+)
 from .models.switch_transformers import (
     SwitchTransformersEncoderModel,
     SwitchTransformersForConditionalGeneration,
