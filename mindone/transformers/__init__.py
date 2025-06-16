35574e58a9c7577fa3b6ed6d3a76a81cb8ec520e
YMC <61680846+iugoood@users.noreply.github.com>
2025-06-16 15:58:38 +0800

    add persimmon model and fuyu model (#1055)

--- mindone/transformers/__init__.py
+++ mindone/transformers/__init__.py
@@ -117,6 +117,7 @@ from .models.deberta_v2 import (
     DebertaV2PreTrainedModel,
 )
 from .models.dpt import DPTForDepthEstimation
+from .models.fuyu import FuyuForCausalLM, FuyuPreTrainedModel
 from .models.gemma import (
     GemmaForCausalLM,
     GemmaForSequenceClassification,
@@ -215,6 +216,13 @@ from .models.mt5 import (
     MT5ForConditionalGeneration,
     MT5Model,
     MT5PreTrainedModel,
+)
+from .models.persimmon import (
+    PersimmonForCausalLM,
+    PersimmonForSequenceClassification,
+    PersimmonForTokenClassification,
+    PersimmonModel,
+    PersimmonPreTrainedModel,
 )
 from .models.phi3 import (
     Phi3ForCausalLM,
