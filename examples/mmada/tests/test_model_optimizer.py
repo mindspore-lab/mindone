15369e971453b1da0c8e3057d529a76e016db68c
Didan Deng <33117903+wtomin@users.noreply.github.com>
2025-06-17 17:22:22 +0800

    fix

--- examples/mmada/tests/test_model_optimizer.py
+++ examples/mmada/tests/test_model_optimizer.py
@@ -103,7 +103,7 @@ def test_model_and_optimizer_initialization():
         tokenizer = AutoTokenizer.from_pretrained(config.model.mmada.pretrained_model_path, padding_side="left")
         vq_model = get_vq_model_class(config.model.vq_model.type)
         vq_model = vq_model.from_pretrained(config.model.vq_model.vq_model_name)
-        vq_modelset_train(False)
+        vq_model.set_train(False)
         vq_model.requires_grad = False
 
         base_config = AutoConfig.from_pretrained(config.model.mmada.pretrained_model_path).to_dict()
