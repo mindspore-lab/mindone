5e82e3479ed0b0df33ff8cf138c9ba0053e6ef95
SamitHuang <285365963@qq.com>
2025-06-17 17:12:28 +0800

    linting

--- tests/modeling_test_utils.py
+++ tests/modeling_test_utils.py
@@ -8,7 +8,7 @@ from diffusers.utils import BaseOutput
 from ml_dtypes import bfloat16
 
 import mindspore as ms
-from mindspore import mint, nn, ops
+from mindspore import nn, ops
 
 logger = logging.getLogger("ModelingsUnitTest")
 
