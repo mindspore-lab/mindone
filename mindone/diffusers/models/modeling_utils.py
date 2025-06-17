4c75ef1c0b2292602dafe3fe1b34232f63135e6a
jijiarong <jijiarong@huawei.com>
2025-06-17 15:37:56 +0800

    unidiffuser bugfix

--- mindone/diffusers/models/modeling_utils.py
+++ mindone/diffusers/models/modeling_utils.py
@@ -927,6 +927,11 @@ class ModelMixin(nn.Cell, PushToHubMixin):
             if f"{path}.proj_attn.bias" in state_dict:
                 state_dict[f"{path}.to_out.0.bias"] = state_dict.pop(f"{path}.proj_attn.bias")
 
+        # TODO : MindSpore 2.6 share weight bug. Unable to load WTE and LM-Head layer weights properly. It will be
+        #  deleted until fixed load_state_dict_into_model and parameters_and_names。
+        if hasattr(self, "wte_lm_share") and self.wte_lm_share:
+            self.text_decoder.transformer.transformer.wte.embedding_table = self.text_decoder.transformer.lm_head.weight
+
     def get_submodule(self, target: str) -> nn.Cell:
         """Return the submodule given by ``target`` if it exists, otherwise throw an error.
 
--- mindone/diffusers/pipelines/unidiffuser/pipeline_unidiffuser.py
+++ mindone/diffusers/pipelines/unidiffuser/pipeline_unidiffuser.py
@@ -127,8 +127,7 @@ class UniDiffuserPipeline(DiffusionPipeline):
             self.text_intermediate_dim = self.text_decoder.prefix_hidden_dim
 
         self.mode = None
-        # Shared weight
-        self.text_decoder.transformer.transformer.wte.embedding_table = self.text_decoder.transformer.lm_head.weight
+        self.text_decoder.set_share_weight(True)
 
         # TODO: handle safety checking?
         self.safety_checker = None
