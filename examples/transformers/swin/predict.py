24563127d9d6b412dbc046552daeeab55efc0a24
Didan Deng <33117903+wtomin@users.noreply.github.com>
2025-06-17 11:22:06 +0800

    allow fp16

--- examples/transformers/swin/predict.py
+++ examples/transformers/swin/predict.py
@@ -6,17 +6,18 @@ from mindspore import ops
 
 from mindone.transformers import AutoImageProcessor, SwinForMaskedImageModeling
 
+dtype = ms.float16
 url = "http://images.cocodataset.org/val2017/000000039769.jpg"
 image = Image.open(requests.get(url, stream=True).raw)
 
 image_processor = AutoImageProcessor.from_pretrained("microsoft/swin-base-simmim-window6-192")
-model = SwinForMaskedImageModeling.from_pretrained("microsoft/swin-base-simmim-window6-192")
+model = SwinForMaskedImageModeling.from_pretrained("microsoft/swin-base-simmim-window6-192", mindspore_dtype=dtype)
 
 num_patches = (model.config.image_size // model.config.patch_size) ** 2
 pixel_values = ms.Tensor(image_processor(images=image, return_tensors="np").pixel_values)
 # create random boolean mask of shape (batch_size, num_patches)
 bool_masked_pos = ops.randint(low=0, high=2, size=(1, num_patches)).bool()
 
-outputs = model(pixel_values, bool_masked_pos=bool_masked_pos)
+outputs = model(pixel_values.to(dtype), bool_masked_pos=bool_masked_pos)
 loss, reconstructed_pixel_values = outputs.loss, outputs.logits
 print(list(reconstructed_pixel_values.shape))
