9bb14b526ae7be5463695accb17f5d12c4da6ae2
liuchuting <liuchuting1@huawei.com>
2025-06-19 17:24:37 +0800

    Add ernir model

--- tests/transformers_tests/models/ernie/test_modeing_ernie.py
+++ tests/transformers_tests/models/ernie/test_modeing_ernie.py
@@ -27,7 +27,7 @@ from tests.modeling_test_utils import (
 from tests.transformers_tests.models.modeling_common import ids_numpy, random_attention_mask
 
 # CrossEntropyLoss not support bf16
-DTYPE_AND_THRESHOLDS = {"fp32": 5e-4, "fp16": 5e-3, "bf16": 1e-2}
+DTYPE_AND_THRESHOLDS = {"fp32": 5e-4, "fp16": 5e-3, "bf16": 5e-2}
 MODES = [1]
 
 
