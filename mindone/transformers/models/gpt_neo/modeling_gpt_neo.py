e142fc8d1b8d4612741fbf34281baa4711b695de
liuchuting <liuchuting1@huawei.com>
2025-06-25 16:11:41 +0800

    f

--- mindone/transformers/models/gpt_neo/modeling_gpt_neo.py
+++ mindone/transformers/models/gpt_neo/modeling_gpt_neo.py
@@ -270,12 +270,6 @@ class GPTNeoFlashAttention2(GPTNeoSelfAttention):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        # TODO: Should be removed once Flash Attention for RoCm is bumped to 2.1.
-        # flash_attn<2.1 generates top-left aligned causal mask, while what is needed here is bottom-right alignment, that was made default for flash_attn>=2.1.
-        # This attribute is used to handle this difference. Reference: https://github.com/Dao-AILab/flash-attention/releases/tag/v2.1.0.
-        # Beware that with flash_attn<2.1, using q_seqlen != k_seqlen (except for the case q_seqlen == 1) produces a wrong mask (top-left).
-        self._flash_attn_uses_top_left_mask = not is_flash_attn_greater_or_equal_2_10()
-
     def construct(
         self,
         hidden_states,
@@ -326,7 +320,7 @@ class GPTNeoFlashAttention2(GPTNeoSelfAttention):
             value,
             attention_mask,
             dropout=attn_dropout,
-            scaling=self.scaling,
+            scaling=1.0,
         )
 
         attn_weights_reshaped = attn_output.reshape(bsz, query_length, self.num_heads * self.head_dim)
