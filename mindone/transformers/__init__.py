f56739407353a7f6e7f786f80f480718ceae9493
litingyu <605979840@qq.com>
2025-07-03 18:23:51 +0800

    fix

--- mindone/transformers/__init__.py
+++ mindone/transformers/__init__.py
@@ -238,12 +238,7 @@ from .models.hiera import (
     HieraModel,
     HieraPreTrainedModel,
 )
-from .models.hubert import(
-    HubertForCTC,
-    HubertForSequenceClassification,
-    HubertModel,
-    HubertPreTrainedModel,
-)
+from .models.hubert import HubertForCTC, HubertForSequenceClassification, HubertModel, HubertPreTrainedModel
 from .models.idefics3 import (
     Idefics3ForConditionalGeneration,
     Idefics3Model,
