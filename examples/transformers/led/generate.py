64b69c5a8085bfcfea5a47c76da10313be6ef54f
Didan Deng <33117903+wtomin@users.noreply.github.com>
2025-07-14 18:38:22 +0800

    fix error

--- examples/transformers/led/generate.py
+++ examples/transformers/led/generate.py
@@ -15,7 +15,7 @@ TXT = "My friends are <mask> but they eat too many carbs."
 # load from modelscope (run `modelscope download --model allenai/led-base-16384` ahead)
 checkpoint_path = "/home/user/.cache/modelscope/hub/models/allenai/led-base-16384"
 tokenizer = AutoTokenizer.from_pretrained(checkpoint_path)
-model = LEDForConditionalGeneration.from_pretrained(checkpoint_path, mindspore_dtype=ms.float16)
+model = LEDForConditionalGeneration.from_pretrained(checkpoint_path, mindspore_dtype=ms.float32)
 
 
 input_ids = Tensor(tokenizer([TXT], return_tensors="np")["input_ids"])
