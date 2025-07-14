a0bca4eabf2ee3a7fffaee79f9e8ccc1c87df06d
Didan Deng <33117903+wtomin@users.noreply.github.com>
2025-07-14 16:37:11 +0800

    safetensors

--- examples/transformers/led/generate.py
+++ examples/transformers/led/generate.py
@@ -1,14 +1,21 @@
 # Adapted from https://huggingface.co/docs/transformers/model_doc/led#transformers.LEDForConditionalGeneration.forward.example-2
 from transformers import AutoTokenizer
 
+import mindspore as ms
 from mindspore import Tensor
 
 from mindone.transformers import LEDForConditionalGeneration
 
-tokenizer = AutoTokenizer.from_pretrained("allenai/led-base-16384")
 TXT = "My friends are <mask> but they eat too many carbs."
+# load from huggingface
+# tokenizer = AutoTokenizer.from_pretrained("allenai/led-base-16384")
+# model = LEDForConditionalGeneration.from_pretrained("allenai/led-base-16384", revision="refs/pr/4")
 
-model = LEDForConditionalGeneration.from_pretrained("allenai/led-base-16384", revision="refs/pr/4")
+
+# load from modelscope (run `modelscope download --model allenai/led-base-16384` ahead)
+checkpoint_path = "~/.cache/modelscope/hub/models/allenai/led-base-16384"
+tokenizer = AutoTokenizer.from_pretrained(checkpoint_path)
+model = LEDForConditionalGeneration.from_pretrained(checkpoint_path, mindspore_dtype=ms.float16)
 input_ids = Tensor(tokenizer([TXT], return_tensors="np")["input_ids"])
 
 prediction = model.generate(input_ids)[0]
