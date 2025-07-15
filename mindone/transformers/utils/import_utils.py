1795b3fb0f5c8bfc249101f08d41ba6d1c0d6aff
liuchuting <liuchuting1@huawei.com>
2025-07-15 17:17:16 +0800

    Add git model

--- mindone/transformers/utils/import_utils.py
+++ mindone/transformers/utils/import_utils.py
@@ -111,8 +111,15 @@ VISION_IMPORT_ERROR = """
 `pip install pillow`. Please note that you may need to restart your runtime after installation.
 """
 
+# docstyle-ignore
+SCIPY_IMPORT_ERROR = """
+{0} requires the scipy library but it was not found in your environment. You can install it with pip:
+`pip install scipy`. Please note that you may need to restart your runtime after installation.
+"""
+
 BACKENDS_MAPPING = OrderedDict(
     [
+        ("scipy", (is_scipy_available, SCIPY_IMPORT_ERROR)),
         ("mindspore", (is_mindspore_available, MINDSPORE_IMPORT_ERROR_WITH_TF)),
         ("vision", (is_vision_available, VISION_IMPORT_ERROR)),
     ]
