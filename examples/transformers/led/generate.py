1c298abf07dbe889ed89c20dd721190e16ef376c
Didan Deng <33117903+wtomin@users.noreply.github.com>
2025-07-16 19:33:16 +0800

    led load from huggingface

--- examples/transformers/led/generate.py
+++ examples/transformers/led/generate.py
@@ -1,21 +1,25 @@
 # Adapted from https://huggingface.co/docs/transformers/model_doc/led#transformers.LEDForConditionalGeneration.forward.example-2
 from transformers import AutoTokenizer
 
-import mindspore as ms
 from mindspore import Tensor
 
 from mindone.transformers import LEDForConditionalGeneration
 
 TXT = "My friends are <mask> but they eat too many carbs."
 # load from huggingface
-# tokenizer = AutoTokenizer.from_pretrained("allenai/led-base-16384")
-# model = LEDForConditionalGeneration.from_pretrained("allenai/led-base-16384", revision="refs/pr/4")
+tokenizer = AutoTokenizer.from_pretrained("allenai/led-base-16384")
+model = LEDForConditionalGeneration.from_pretrained("allenai/led-base-16384", revision="refs/pr/4")
 
+# load from modelscope
+# run `modelscope download --model allenai/led-base-16384` ahead
+# python convert_to_safetensors.py \
+# --model_path ~/.cache/modelscope/hub/models/allenai/led-base-16384/pytorch_model.bin \
+# --output_path ~/.cache/modelscope/hub/models/allenai/led-base-16384/model.safetensors
 
-# load from modelscope (run `modelscope download --model allenai/led-base-16384` ahead)
-checkpoint_path = "/home/user/.cache/modelscope/hub/models/allenai/led-base-16384"
-tokenizer = AutoTokenizer.from_pretrained(checkpoint_path)
-model = LEDForConditionalGeneration.from_pretrained(checkpoint_path, mindspore_dtype=ms.float32)
+# uncomment the following lines to load from modelscope
+# checkpoint_path = "/home/user/.cache/modelscope/hub/models/allenai/led-base-16384"
+# tokenizer = AutoTokenizer.from_pretrained(checkpoint_path)
+# model = LEDForConditionalGeneration.from_pretrained(checkpoint_path, mindspore_dtype=ms.float32)
 
 
 input_ids = Tensor(tokenizer([TXT], return_tensors="np")["input_ids"])
