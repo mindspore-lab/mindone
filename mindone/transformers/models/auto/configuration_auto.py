3357316b5bfb54f0776071dc1c7fdd3f41d32ce1
chenyingshu <yingshu2008@gmail.com>
2025-07-18 17:16:40 +0800

    linting

--- mindone/transformers/models/auto/configuration_auto.py
+++ mindone/transformers/models/auto/configuration_auto.py
@@ -234,6 +234,7 @@ if version.parse(transformers.__version__) >= version.parse("4.53.0"):
     CONFIG_MAPPING_NAMES.update({"vjepa2": "VJEPA2Model"})
     MODEL_NAMES_MAPPING.update({"vjepa2": "VJEPA2Model"})
 
+
 def model_type_to_module_name(key):
     """Converts a config key to the corresponding module."""
     # Special treatment
