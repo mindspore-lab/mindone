3077e9db44fc96aecc8b2f05d2e6afff35db139b
Didan Deng <33117903+wtomin@users.noreply.github.com>
2025-07-18 19:13:36 +0800

    updates

--- tests/transformers_tests/models/swin/test_modeling_swin.py
+++ tests/transformers_tests/models/swin/test_modeling_swin.py
@@ -18,7 +18,7 @@ from tests.modeling_test_utils import (
 )
 
 DTYPE_AND_THRESHOLDS = {"fp32": 5e-4, "fp16": 5e-3, "bf16": 5e-3}
-MODES = [0, 1]  # 0: graph mode, 1: pynative mode
+MODES = [1]  # 0: graph mode, 1: pynative mode
 
 
 class SwinModelTester:
