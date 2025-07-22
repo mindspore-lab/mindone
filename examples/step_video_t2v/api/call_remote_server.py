8aa07dbca2358a015e18ef045016c0a8849fa161
qiuyufeng <qiuyufeng3@huawei.com>
2025-07-22 17:44:21 +0800

    Add ssl verification and ban 0.0.0.0 sever ip

--- examples/step_video_t2v/api/call_remote_server.py
+++ examples/step_video_t2v/api/call_remote_server.py
@@ -207,7 +207,7 @@ class RemoteServer(object):
                 resource_class_args=[self.caption_pipeline],
             )
 
-    def run(self, host="0.0.0.0", port=8080):
+    def run(self, host="127.0.0.1", port=8080):
         if self.enable_vae:
             port = 5001
             print(f"enable vae, port setting to {port}")
@@ -233,4 +233,4 @@ if __name__ == "__main__":
     )
 
     flask_server = RemoteServer(args)
-    flask_server.run(host="0.0.0.0", port=args.port)
+    flask_server.run(host="127.0.0.1", port=args.port)
