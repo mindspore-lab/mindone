1887ee90621092142449ed7d61a104863721df4a
chenyingshu <yingshu2008@gmail.com>
2025-07-22 15:26:02 +0800

    add copyright

--- tests/transformers_tests/models/vit/test_modeling_vit.py
+++ tests/transformers_tests/models/vit/test_modeling_vit.py
@@ -1,6 +1,9 @@
 # coding=utf-8
 # Copyright 2021 The HuggingFace Inc. team. All rights reserved.
 #
+# This code is adapted from https://github.com/huggingface/transformers
+# with modifications to run transformers on mindspore.
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -12,7 +15,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Testing suite for the PyTorch ViT model."""
+"""Testing suite for the MindSpore ViT model."""
 import inspect
 
 import numpy as np
