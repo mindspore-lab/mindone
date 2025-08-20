f8e13d31aa1289268d2c62067c45c5a8e8d42b93
Didan Deng <33117903+wtomin@users.noreply.github.com>
2025-08-20 10:30:01 +0800

    fix mint.nn.batchnorm dtype error

--- tests/modeling_test_utils.py
+++ tests/modeling_test_utils.py
@@ -151,6 +151,8 @@ def get_pt2ms_mappings(m):
                 mappings[f"{name}.running_mean"] = f"{name}.moving_mean", lambda x: x
                 mappings[f"{name}.running_var"] = f"{name}.moving_variance", lambda x: x
                 mappings[f"{name}.num_batches_tracked"] = None, lambda x: x
+        elif isinstance(cell, (mint.nn.BatchNorm1d, mint.nn.BatchNorm2d, mint.nn.BatchNorm3d)):
+            pass
     return mappings
 
 
