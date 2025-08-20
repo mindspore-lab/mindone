061deb073b9b78326343a4378b580474612ff1f7
Didan Deng <33117903+wtomin@users.noreply.github.com>
2025-08-20 10:31:00 +0800

    adjust bf16 threshold

--- tests/transformers_tests/models/align/test_modeling_align.py
+++ tests/transformers_tests/models/align/test_modeling_align.py
@@ -25,7 +25,7 @@ from tests.modeling_test_utils import (
 )
 from tests.transformers_tests.models.modeling_common import ids_numpy
 
-DTYPE_AND_THRESHOLDS = {"fp32": 5e-4, "fp16": 5e-3, "bf16": 5e-3}
+DTYPE_AND_THRESHOLDS = {"fp32": 5e-4, "fp16": 5e-3, "bf16": 6e-3}
 MODES = [1]  # 0: graph mode, 1: pynative mode
 
 
