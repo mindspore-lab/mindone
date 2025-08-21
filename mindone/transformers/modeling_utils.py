fd769b1ca66bbb25832b450a8a58d378ff74c458
Chaoran Wei <77485245+wcrzlh@users.noreply.github.com>
2025-08-21 14:34:22 +0800

    fix get_type_hints problem

--- mindone/transformers/modeling_utils.py
+++ mindone/transformers/modeling_utils.py
@@ -959,27 +959,8 @@ class PreTrainedModel(nn.Cell, EmbeddingAccessMixin, ModuleUtilsMixin, Generatio
 
     def __init_subclass__(cls, **kwargs):
         super().__init_subclass__(**kwargs)
-        # For BC we keep the original `config_class` definition in case
-        # there is a `config_class` attribute (e.g. remote code models),
-        # otherwise we derive it from the annotated `config` attribute.
-
-        # defined in this particular subclass
-        child_annotation = cls.__dict__.get("__annotations__", {}).get("config", None)
-        child_attribute = cls.__dict__.get("config_class", None)
-
-        # defined in the class (this subclass or any parent class)
-        full_annotation = get_type_hints(cls).get("config", None)
-        full_attribute = cls.config_class
-
-        # priority (child class_config -> child annotation -> global class_config -> global annotation)
-        if child_attribute is not None:
-            cls.config_class = child_attribute
-        elif child_annotation is not None:
-            cls.config_class = child_annotation
-        elif full_attribute is not None:
-            cls.config_class = full_attribute
-        elif full_annotation is not None:
-            cls.config_class = full_annotation
+        # fixme get_type_hints would encounter a problem during typing check
+        pass
 
     def __init__(self, config: PretrainedConfig, *inputs, **kwargs):
         super().__init__()
