f32b7cb169a710d8a6957d9d92cd1d90450ab3f2
Didan Deng <33117903+wtomin@users.noreply.github.com>
2025-08-25 11:49:54 +0800

    load module dynamically from mindone/transformers

--- mindone/transformers/processing_utils.py
+++ mindone/transformers/processing_utils.py
@@ -1358,29 +1358,20 @@ class ProcessorMixin(PushToHubMixin):
 
     @staticmethod
     def get_possibly_dynamic_module(module_name):
-        if hasattr(transformers_module, module_name):
-            return getattr(transformers_module, module_name)
-        lookup_locations = [
-            transformers_module.IMAGE_PROCESSOR_MAPPING,
-            transformers_module.VIDEO_PROCESSOR_MAPPING,
-            transformers_module.TOKENIZER_MAPPING,
-            transformers_module.FEATURE_EXTRACTOR_MAPPING,
-            transformers_module.MODEL_FOR_AUDIO_TOKENIZATION_MAPPING,
-        ]
-        for lookup_location in lookup_locations:
-            for custom_class in lookup_location._extra_content.values():
-                if isinstance(custom_class, tuple):
-                    for custom_subclass in custom_class:
-                        if custom_subclass is not None and custom_subclass.__name__ == module_name:
-                            return custom_subclass
-                elif custom_class is not None and custom_class.__name__ == module_name:
-                    return custom_class
+
+        if  "ImageProcess" in module_name:
+            sub_path = os.path.abspath(os.path.dirname(__file__))
+            sub_path = str(Path(sub_path).parent)
+            sys.path.insert(0, sub_path)
+            mindone_transformers_module = importlib.import_module("mindone.transformers")
+            if not hasattr(mindone_transformers_module, module_name):
+                raise ValueError(f"Expect to have `{module_name}` registered in `mindone.transformers`, but failed to load it!")
+            return getattr(mindone_transformers_module, module_name)
         else:
-            raise ValueError(
-                f"Could not find module {module_name} in `transformers`. If this is a custom class, "
-                f"it should be registered using the relevant `AutoClass.register()` function so that "
-                f"other functions can find it!"
-            )
+            if hasattr(transformers_module, module_name)
+                return getattr(transformers_module, module_name)
+            else:
+                raise ValueError(f"Expect to have `{module_name}` registered in `transformers`, but failed to load it!")
 
     @property
     def model_input_names(self):
