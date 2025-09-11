2282e49596dffbad8e810c53cc92fce35fd5cf03
Mike Cheung <zhtmike@gmail.com>
2025-09-11 11:42:30 +0800

    add license

--- mindone/transformers/models/donut/feature_extraction_donut.py
+++ mindone/transformers/models/donut/feature_extraction_donut.py
@@ -7,6 +7,9 @@
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
+# This code is adapted from https://github.com/huggingface/transformers
+# with modifications to run transformers on mindspore.
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
