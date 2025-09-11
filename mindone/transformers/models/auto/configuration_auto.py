429fb183639e4e295f26f537f62cae46f289a2c0
Didan Deng <33117903+wtomin@users.noreply.github.com>
2025-09-11 11:12:25 +0800

    config

--- mindone/transformers/models/auto/configuration_auto.py
+++ mindone/transformers/models/auto/configuration_auto.py
@@ -36,6 +36,7 @@ CONFIG_MAPPING_NAMES = OrderedDict(
     [
         # Add configs here
         ("albert", "AlbertConfig"),
+        ("align", "AlignConfig"),
         ("aria", "AriaConfig"),
         ("aria_text", "AriaTextConfig"),
         ("bert", "BertConfig"),
@@ -135,6 +136,7 @@ MODEL_NAMES_MAPPING = OrderedDict(
     [
         # Add full (and cased) model names here
         ("albert", "ALBERT"),
+        ("align", "ALIGN"),
         ("aria", "Aria"),
         ("aria_text", "AriaText"),
         ("m2m_100", "M2M100"),
