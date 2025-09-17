d8eae561cd9d66da7b5629cd5b140d7d660763b3
Mike Cheung <zhtmike@gmail.com>
2025-09-17 16:31:32 +0800

    fix typo

--- mindone/transformers/models/lxmert/modeling_lxmert.py
+++ mindone/transformers/models/lxmert/modeling_lxmert.py
@@ -913,10 +913,10 @@ class LxmertForPreTraining(LxmertPreTrainedModel):
             num_labels (`int`, *optional*):
                 New number of labels in the linear layer weight matrix. Increasing the size will add newly initialized
                 weights at the end. Reducing the size will remove weights from the end. If not provided or `None`, just
-                returns a pointer to the qa labels ``mint.mint.nn.Linear``` module of the model without doing anything.
+                returns a pointer to the qa labels ``mint.nn.Linear``` module of the model without doing anything.
 
         Return:
-            `mint.mint.nn.Linear`: Pointer to the resized Linear layer or the old Linear layer
+            `mint.nn.Linear`: Pointer to the resized Linear layer or the old Linear layer
         """
 
         cur_qa_logit_layer = self.get_qa_logit_layer()
@@ -1134,10 +1134,10 @@ class LxmertForQuestionAnswering(LxmertPreTrainedModel):
             num_labels (`int`, *optional*):
                 New number of labels in the linear layer weight matrix. Increasing the size will add newly initialized
                 weights at the end. Reducing the size will remove weights from the end. If not provided or `None`, just
-                returns a pointer to the qa labels ``mint.mint.nn.Linear``` module of the model without doing anything.
+                returns a pointer to the qa labels ``mint.nn.Linear``` module of the model without doing anything.
 
         Return:
-            `mint.mint.nn.Linear`: Pointer to the resized Linear layer or the old Linear layer
+            `mint.nn.Linear`: Pointer to the resized Linear layer or the old Linear layer
         """
 
         cur_qa_logit_layer = self.get_qa_logit_layer()
