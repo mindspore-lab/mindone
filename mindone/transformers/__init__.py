3f6261606348254bd09f4694abecf8e38ad12a8f
Dong1017 <xwdong1998@163.com>
2025-09-17 16:01:48 +0800

    seamless_m4t fastUTs pass

--- mindone/transformers/__init__.py
+++ mindone/transformers/__init__.py
@@ -587,6 +587,13 @@ from .models.roberta import (
     RobertaModel,
     RobertaPreTrainedModel,
 )
+from .models.seamless_m4t import (
+    SeamlessM4TForSpeechToSpeech,
+    SeamlessM4TForSpeechToText,
+    SeamlessM4TForTextToSpeech,
+    SeamlessM4TForTextToText,
+    SeamlessM4TModel,
+)
 from .models.rwkv import RwkvForCausalLM, RwkvModel, RwkvPreTrainedModel
 from .models.segformer import (
     SegformerDecodeHead,
