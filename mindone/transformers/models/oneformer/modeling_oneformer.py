097dc4f531d8ba6e1b196b09c5eb2f09876bb81b
Didan Deng <33117903+wtomin@users.noreply.github.com>
2025-09-19 11:28:23 +0800

    update init weight

--- mindone/transformers/models/oneformer/modeling_oneformer.py
+++ mindone/transformers/models/oneformer/modeling_oneformer.py
@@ -35,6 +35,7 @@ from transformers.utils import (
 import mindspore as ms
 from mindspore import Tensor, mint, nn
 
+from mindone.models.utils import constant_, normal_, trunc_normal_, xavier_uniform_
 from mindone.transformers.mindspore_adapter.utils import _DTYPE_2_MAX
 
 from ...activations import ACT2FN
@@ -2837,14 +2838,14 @@ class OneFormerPreTrainedModel(PreTrainedModel):
             if module.input_projections is not None:
                 for input_projection in module.input_projections:
                     if not isinstance(input_projection, ms.nn.SequentialCell):
-                        nn.init.xavier_uniform_(input_projection.weight, gain=xavier_std)
-                        nn.init.constant_(input_projection.bias, 0)
+                        xavier_uniform_(input_projection.weight, gain=xavier_std)
+                        constant_(input_projection.bias, 0)
         elif isinstance(module, OneFormerTransformerDecoder):
-            nn.init.xavier_uniform_(module.query_input_projection.weight, gain=xavier_std)
-            nn.init.constant_(module.query_input_projection.bias, 0)
+            xavier_uniform_(module.query_input_projection.weight, gain=xavier_std)
+            constant_(module.query_input_projection.bias, 0)
             module.query_input_projection._is_hf_initialized = True
         elif isinstance(module, OneFormerPixelDecoderEncoderMultiscaleDeformableAttention):
-            nn.init.constant_(module.sampling_offsets.weight.data, 0.0)
+            constant_(module.sampling_offsets.weight, 0.0)
             thetas = mint.arange(module.n_heads, dtype=ms.int64).float() * (2.0 * math.pi / module.n_heads)
             grid_init = mint.stack([thetas.cos(), thetas.sin()], -1)
             grid_init = (
@@ -2856,88 +2857,88 @@ class OneFormerPreTrainedModel(PreTrainedModel):
                 grid_init[:, :, i, :] *= i + 1
             with ms._no_grad():
                 module.sampling_offsets.bias = ms.Parameter(grid_init.view(-1))
-            nn.init.constant_(module.attention_weights.weight.data, 0.0)
-            nn.init.constant_(module.attention_weights.bias.data, 0.0)
-            nn.init.xavier_uniform_(module.value_proj.weight.data)
-            nn.init.constant_(module.value_proj.bias.data, 0.0)
-            nn.init.xavier_uniform_(module.output_proj.weight.data)
-            nn.init.constant_(module.output_proj.bias.data, 0.0)
+            constant_(module.attention_weights.weight, 0.0)
+            constant_(module.attention_weights.bias, 0.0)
+            xavier_uniform_(module.value_proj.weight)
+            constant_(module.value_proj.bias, 0.0)
+            xavier_uniform_(module.output_proj.weight)
+            constant_(module.output_proj.bias, 0.0)
         elif isinstance(module, OneFormerPixelDecoderEncoderOnly):
             for p in module.parameters():
                 if p.dim() > 1:
-                    nn.init.xavier_uniform_(p)
+                    xavier_uniform_(p)
         elif isinstance(module, OneFormerPixelDecoder):
             for p in module.parameters():
                 if p.dim() > 1:
-                    nn.init.xavier_uniform_(p)
-            nn.init.normal_(module.level_embed, std=0)
+                    xavier_uniform_(p)
+            normal_(module.level_embed, std=0)
         elif isinstance(module, OneFormerTransformerDecoderSelfAttentionLayer):
             for p in module.parameters():
                 if p.dim() > 1:
-                    nn.init.xavier_uniform_(p, gain=xavier_std)
+                    xavier_uniform_(p, gain=xavier_std)
         elif isinstance(module, OneFormerTransformerDecoderCrossAttentionLayer):
             for p in module.parameters():
                 if p.dim() > 1:
-                    nn.init.xavier_uniform_(p, gain=xavier_std)
+                    xavier_uniform_(p, gain=xavier_std)
         elif isinstance(module, OneFormerTransformerDecoderFFNLayer):
             for p in module.parameters():
                 if p.dim() > 1:
-                    nn.init.xavier_uniform_(p, gain=xavier_std)
+                    xavier_uniform_(p, gain=xavier_std)
         elif isinstance(module, OneFormerTransformerDecoderQueryTransformer):
             for p in module.parameters():
                 if p.dim() > 1:
-                    nn.init.xavier_uniform_(p, gain=xavier_std)
+                    xavier_uniform_(p, gain=xavier_std)
         elif isinstance(module, OneFormerPixelLevelModule):
             for submodule in module.modules():
                 if isinstance(submodule, (mint.nn.Conv2d, mint.nn.Linear)):
-                    submodule.weight.data.normal_(mean=0.0, std=std)
+                    normal_(submodule.weight, mean=0.0, std=std)
                     if submodule.bias is not None:
-                        submodule.bias.data.zero_()
+                        constant_(submodule.bias, 0.0)
         elif isinstance(module, OneFormerTextContextDecoder):
             for submodule in module.modules():
                 if isinstance(submodule, mint.nn.Linear):
-                    nn.init.trunc_normal_(submodule.weight, std=0.02)
+                    trunc_normal_(submodule.weight, std=0.02)
                     if isinstance(submodule, mint.nn.Linear) and submodule.bias is not None:
-                        nn.init.constant_(submodule.bias, 0)
+                        constant_(submodule.bias, 0)
                 elif isinstance(submodule, mint.nn.LayerNorm):
-                    nn.init.constant_(submodule.bias, 0)
-                    nn.init.constant_(submodule.weight, 1.0)
+                    constant_(submodule.bias, 0)
+                    constant_(submodule.weight, 1.0)
         elif isinstance(module, OneFormerTextTransformer):
             proj_std = (module.width**-0.5) * ((2 * module.num_layers) ** -0.5)
             attn_std = module.width**-0.5
             fc_std = (2 * module.width) ** -0.5
             for layer in module.layers:
-                nn.init.normal_(layer.self_attn.in_proj_weight, std=attn_std)
-                nn.init.normal_(layer.self_attn.out_proj.weight, std=proj_std)
-                nn.init.normal_(layer.mlp.fc1.weight, std=fc_std)
-                nn.init.normal_(layer.mlp.fc2.weight, std=proj_std)
+                normal_(layer.self_attn.in_proj_weight, std=attn_std)
+                normal_(layer.self_attn.out_proj.weight, std=proj_std)
+                normal_(layer.mlp.fc1.weight, std=fc_std)
+                normal_(layer.mlp.fc2.weight, std=proj_std)
         elif isinstance(module, OneFormerTextEncoder):
-            nn.init.normal_(module.token_embedding.weight, std=0.02)
-            nn.init.normal_(module.positional_embedding, std=0.01)
+            normal_(module.token_embedding.weight, std=0.02)
+            normal_(module.positional_embedding, std=0.01)
         if hasattr(module, "reference_points"):
-            nn.init.xavier_uniform_(module.reference_points.weight.data, gain=1.0)
-            nn.init.constant_(module.reference_points.bias.data, 0.0)
+            xavier_uniform_(module.reference_points.weight, gain=1.0)
+            constant_(module.reference_points.bias, 0.0)
         elif isinstance(module, OneFormerTaskModel):
             for submodule in module.modules():
                 if isinstance(module, OneFormerMLPPredictionHead):
                     for submodule in module.modules():
                         if isinstance(submodule, mint.nn.Linear):
-                            nn.init.xavier_uniform_(submodule.weight, gain=xavier_std)
-                            nn.init.constant_(submodule.bias, 0)
+                            xavier_uniform_(submodule.weight, gain=xavier_std)
+                            constant_(submodule.bias, 0)
                         elif isinstance(module, mint.nn.LayerNorm):
-                            module.bias.data.zero_()
-                            module.weight.data.fill_(1.0)
+                            constant_(module.bias, 0.0)
+                            constant_(module.weight, 1.0)
         elif isinstance(module, nn.MultiheadAttention):
-            module.in_proj_weight.data.normal_(mean=0.0, std=std)
-            module.in_proj_bias.data.zero_()
+            normal_(module.in_proj_weight, mean=0.0, std=std)
+            constant_(module.in_proj_bias, 0.0)
         elif isinstance(module, (mint.nn.Linear, mint.nn.Conv2d, mint.nn.BatchNorm2d)):
-            module.weight.data.normal_(mean=0.0, std=std)
+            normal_(module.weight, mean=0.0, std=std)
             if module.bias is not None:
-                module.bias.data.zero_()
+                constant_(module.bias, 0.0)
         elif isinstance(module, mint.nn.Embedding):
-            module.weight.data.normal_(mean=0.0, std=std)
+            normal_(module.weight, mean=0.0, std=std)
             if module.padding_idx is not None:
-                module.weight.data[module.padding_idx].zero_()
+                module.weight.data[module.padding_idx] = 0.0
 
 
 @add_start_docstrings(
