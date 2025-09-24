4bbe0e74b7b5919ed9a50df1b17a38ceeb4e90f6
Chaoran Wei <77485245+wcrzlh@users.noreply.github.com>
2025-09-24 17:25:42 +0800

    fix ut bugs

--- tests/transformers_tests/models/bark/test_modeling_bark.py
+++ tests/transformers_tests/models/bark/test_modeling_bark.py
@@ -28,192 +28,14 @@ from tests.modeling_test_utils import (
     get_modules,
 )
 from tests.transformers_tests.models.modeling_common import ids_numpy, random_attention_mask
-from ..encodec.test_modeling_encodec import EncodecModelTester
 
 DTYPE_AND_THRESHOLDS = {"fp32": 5e-4, "fp16": 5e-3, "bf16": 5e-2}
 MODES = [1]
 
 
-class BarkSemanticModelTester:
-    def __init__(
-        self,
-        batch_size=3,  # need batch_size != num_hidden_layers
-        seq_length=4,
-        is_training=False,  # for now training is not supported
-        use_input_mask=True,
-        use_labels=True,
-        vocab_size=33,
-        output_vocab_size=33,
-        hidden_size=16,
-        num_hidden_layers=2,
-        num_attention_heads=2,
-        intermediate_size=15,
-        dropout=0.1,
-        window_size=256,
-        initializer_range=0.02,
-        n_codes_total=8,  # for BarkFineModel
-        n_codes_given=1,  # for BarkFineModel
-    ):
-        self.batch_size = batch_size
-        self.seq_length = seq_length
-        self.is_training = is_training
-        self.use_input_mask = use_input_mask
-        self.use_labels = use_labels
-        self.vocab_size = vocab_size
-        self.output_vocab_size = output_vocab_size
-        self.hidden_size = hidden_size
-        self.num_hidden_layers = num_hidden_layers
-        self.num_attention_heads = num_attention_heads
-        self.intermediate_size = intermediate_size
-        self.dropout = dropout
-        self.window_size = window_size
-        self.initializer_range = initializer_range
-        self.bos_token_id = output_vocab_size - 1
-        self.eos_token_id = output_vocab_size - 1
-        self.pad_token_id = output_vocab_size - 1
-
-        self.n_codes_total = n_codes_total
-        self.n_codes_given = n_codes_given
-
-        self.is_encoder_decoder = False
-
-    def prepare_config_and_inputs(self):
-        input_ids = ids_numpy([self.batch_size, self.seq_length], self.vocab_size)
-
-        input_mask = None
-        if self.use_input_mask:
-            input_mask = random_attention_mask([self.batch_size, self.seq_length])
-
-        config = self.get_config()
-
-        head_mask = ids_numpy([self.num_hidden_layers, self.num_attention_heads], 2)
-
-        inputs_dict = {
-            "input_ids": input_ids,
-            "head_mask": head_mask,
-            "attention_mask": input_mask,
-        }
-
-        return config, inputs_dict
-
-    def get_config(self):
-        return BarkSemanticConfig(
-            vocab_size=self.vocab_size,
-            output_vocab_size=self.output_vocab_size,
-            hidden_size=self.hidden_size,
-            num_layers=self.num_hidden_layers,
-            num_heads=self.num_attention_heads,
-            use_cache=True,
-            bos_token_id=self.bos_token_id,
-            eos_token_id=self.eos_token_id,
-            pad_token_id=self.pad_token_id,
-            window_size=self.window_size,
-        )
-
-    def get_pipeline_config(self):
-        config = self.get_config()
-        config.vocab_size = 300
-        config.output_vocab_size = 300
-        return config
-
-    def prepare_config_and_inputs_for_common(self):
-        config, inputs_dict = self.prepare_config_and_inputs()
-        return config, inputs_dict
-
-
-class BarkCoarseModelTester:
-    def __init__(
-        self,
-        parent,
-        batch_size=3,  # need batch_size != num_hidden_layers
-        seq_length=4,
-        is_training=False,  # for now training is not supported
-        use_input_mask=True,
-        use_labels=True,
-        vocab_size=33,
-        output_vocab_size=33,
-        hidden_size=16,
-        num_hidden_layers=2,
-        num_attention_heads=2,
-        intermediate_size=15,
-        dropout=0.1,
-        window_size=256,
-        initializer_range=0.02,
-        n_codes_total=8,  # for BarkFineModel
-        n_codes_given=1,  # for BarkFineModel
-    ):
-        self.parent = parent
-        self.batch_size = batch_size
-        self.seq_length = seq_length
-        self.is_training = is_training
-        self.use_input_mask = use_input_mask
-        self.use_labels = use_labels
-        self.vocab_size = vocab_size
-        self.output_vocab_size = output_vocab_size
-        self.hidden_size = hidden_size
-        self.num_hidden_layers = num_hidden_layers
-        self.num_attention_heads = num_attention_heads
-        self.intermediate_size = intermediate_size
-        self.dropout = dropout
-        self.window_size = window_size
-        self.initializer_range = initializer_range
-        self.bos_token_id = output_vocab_size - 1
-        self.eos_token_id = output_vocab_size - 1
-        self.pad_token_id = output_vocab_size - 1
-
-        self.n_codes_total = n_codes_total
-        self.n_codes_given = n_codes_given
-
-        self.is_encoder_decoder = False
-
-    def prepare_config_and_inputs(self):
-        input_ids = ids_numpy([self.batch_size, self.seq_length], self.vocab_size)
-
-        input_mask = None
-        if self.use_input_mask:
-            input_mask = random_attention_mask([self.batch_size, self.seq_length])
-
-        config = self.get_config()
-
-        head_mask = ids_numpy([self.num_hidden_layers, self.num_attention_heads], 2)
-
-        inputs_dict = {
-            "input_ids": input_ids,
-            "head_mask": head_mask,
-            "attention_mask": input_mask,
-        }
-
-        return config, inputs_dict
-
-    def get_config(self):
-        return BarkCoarseConfig(
-            vocab_size=self.vocab_size,
-            output_vocab_size=self.output_vocab_size,
-            hidden_size=self.hidden_size,
-            num_layers=self.num_hidden_layers,
-            num_heads=self.num_attention_heads,
-            use_cache=True,
-            bos_token_id=self.bos_token_id,
-            eos_token_id=self.eos_token_id,
-            pad_token_id=self.pad_token_id,
-            window_size=self.window_size,
-        )
-
-    def get_pipeline_config(self):
-        config = self.get_config()
-        config.vocab_size = 300
-        config.output_vocab_size = 300
-        return config
-
-    def prepare_config_and_inputs_for_common(self):
-        config, inputs_dict = self.prepare_config_and_inputs()
-        return config, inputs_dict
-
-
 class BarkFineModelTester:
     def __init__(
         self,
-        parent,
         batch_size=3,  # need batch_size != num_hidden_layers
         seq_length=4,
         is_training=False,  # for now training is not supported
@@ -231,7 +53,6 @@ class BarkFineModelTester:
         n_codes_total=8,  # for BarkFineModel
         n_codes_given=1,  # for BarkFineModel
     ):
-        self.parent = parent
         self.batch_size = batch_size
         self.seq_length = seq_length
         self.is_training = is_training
@@ -292,88 +113,28 @@ class BarkFineModelTester:
             window_size=self.window_size,
         )
 
-    def get_pipeline_config(self):
-        config = self.get_config()
-        config.vocab_size = 300
-        config.output_vocab_size = 300
-        return config
-
     def prepare_config_and_inputs_for_common(self):
         config, inputs_dict = self.prepare_config_and_inputs()
         return config, inputs_dict
 
 
-class BarkModelTester:
-    def __init__(
-        self,
-        parent,
-        semantic_kwargs=None,
-        coarse_acoustics_kwargs=None,
-        fine_acoustics_kwargs=None,
-        codec_kwargs=None,
-        is_training=False,  # for now training is not supported
-    ):
-        if semantic_kwargs is None:
-            semantic_kwargs = {}
-        if coarse_acoustics_kwargs is None:
-            coarse_acoustics_kwargs = {}
-        if fine_acoustics_kwargs is None:
-            fine_acoustics_kwargs = {}
-        if codec_kwargs is None:
-            codec_kwargs = {}
-
-        self.parent = parent
-        self.semantic_model_tester = BarkSemanticModelTester(parent, **semantic_kwargs)
-        self.coarse_acoustics_model_tester = BarkCoarseModelTester(parent, **coarse_acoustics_kwargs)
-        self.fine_acoustics_model_tester = BarkFineModelTester(parent, **fine_acoustics_kwargs)
-        self.codec_model_tester = EncodecModelTester(parent, **codec_kwargs)
-
-        self.is_training = is_training
-
-    def get_config(self):
-        return BarkConfig.from_sub_model_configs(
-            self.semantic_model_tester.get_config(),
-            self.coarse_acoustics_model_tester.get_config(),
-            self.fine_acoustics_model_tester.get_config(),
-            self.codec_model_tester.get_config(),
-        )
-
-    def get_pipeline_config(self):
-        config = self.get_config()
-
-        # follow the `get_pipeline_config` of the sub component models
-        config.semantic_config.vocab_size = 300
-        config.coarse_acoustics_config.vocab_size = 300
-        config.fine_acoustics_config.vocab_size = 300
-
-        config.semantic_config.output_vocab_size = 300
-        config.coarse_acoustics_config.output_vocab_size = 300
-        config.fine_acoustics_config.output_vocab_size = 300
-
-        return config
-
-
-model_tester = BarkModelTester()
+model_tester = BarkFineModelTester()
 (
     config,
-    input_values,
-    attention_mask,
+    input_dict
 ) = model_tester.prepare_config_and_inputs()
-
 
 BARK_CASES = [
     [
-        "BarkModel",
-        "transformers.BarkModel",
-        "mindone.transformers.BarkModel",
+        "BarkFineModel",
+        "transformers.BarkFineModel",
+        "mindone.transformers.BarkFineModel",
         (config,),
         {},
-        (input_values,),
+        (),
+        input_dict,
         {
-            "attention_mask": attention_mask,
-        },
-        {
-            "last_hidden_state": 0,  # key: torch attribute, value: mindspore idx
+            "logits": 0,
         },
     ],
 ]
