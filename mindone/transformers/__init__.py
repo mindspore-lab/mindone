90e2bd470979195c66fb6f41bf64e97853ae4181
籍家荣 <33412538+JIJIARONGjijiarong@users.noreply.github.com>
2025-09-26 16:20:10 +0800

    feat(transformers/models): add models of nllb moe (#1244)
    * add nllb
    
    * bugfix
    
    * add test
    
    * code clean
    
    * Update __init__.py
    
    * Update test_modeling_nllb_moe.py
    
    * Update __init__.py
    
    ---------
    
    Co-authored-by: jijiarong <jijiarong@huawei.com>

--- mindone/transformers/__init__.py
+++ mindone/transformers/__init__.py
@@ -785,6 +785,13 @@ from .models.mvp import (
     MvpModel,
     MvpPreTrainedModel,
 )
+from .models.nllb_moe import (
+    NllbMoeForConditionalGeneration,
+    NllbMoeModel,
+    NllbMoePreTrainedModel,
+    NllbMoeSparseMLP,
+    NllbMoeTop2Router,
+)
 from .models.nystromformer import (
     NystromformerEncoder,
     NystromformerForMaskedLM,
