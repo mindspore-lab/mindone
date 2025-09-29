d1db24d0c1fb83c277af1397e9a5ee3126799225
Andy Zhou <46011930+AndyZhou952@users.noreply.github.com>
2025-09-29 20:51:07 +0800

    feat(transformers) Add usage examples for models Granite-Vision, MatCha, and DePlot (#1334)
    * add granite-vision example (LLaVA_NEXT base)
    
    * add matcha and deplot example
    
    * add granite-vision example
    
    * linting
    
    * rm generate.py
    
    ---------
    
    Co-authored-by: vigo999 <41234155+vigo999@users.noreply.github.com>

--- mindone/transformers/models/reformer/modeling_reformer.py
+++ mindone/transformers/models/reformer/modeling_reformer.py
@@ -827,6 +827,8 @@ class LSHSelfAttention(mindspore.nn.Cell, EfficientAttentionMixin):
         del self_mask
 
         logits = mindspore.mint.logsumexp(query_key_dots, dim=-1, keepdim=True)
+        # dots shape is `[batch_size, num_attn_heads, num_hashes * seq_len
+        # // chunk_length, chunk_length, chunk_length * (1 + num_chunks_before + num_chunks_after)]`
         # dots shape is `[batch_size, num_attn_heads, num_hashes * seq_len // chunk_length,
         # chunk_length, chunk_length * (1 + num_chunks_before + num_chunks_after)]`
         attention_probs = mindspore.mint.exp(query_key_dots - logits)
@@ -1770,9 +1772,9 @@ class ReformerModelOutput(ModelOutput):
             corresponds to `sequence_length`.
         past_buckets_states (`List[Tuple(ms.Tensor, ms.Tensor)]`, *optional*, returned when `use_cache=True` is passed or when `config.use_cache=True`):
             List of `Tuple(ms.Tensor, ms.Tensor` of length `config.n_layers`, with the first element
+            
             being the previous *buckets* of shape `(batch_size, num_heads, num_hashes, sequence_length)`) and the
             second being the previous *hidden_states* of shape `(batch_size, sequence_length, hidden_size)`).
-
             Contains precomputed buckets and hidden-states that can be used (see `past_buckets_states` input) to speed
             up sequential decoding.
         hidden_states (`tuple(ms.Tensor)`, *optional*, returned when `output_hidden_states=True` is passed or when `config.output_hidden_states=True`):
@@ -1809,6 +1811,7 @@ class ReformerModelWithLMHeadOutput(ModelOutput):
             corresponds to `sequence_length`.
         past_buckets_states (`List[Tuple(ms.Tensor, ms.Tensor)]`, *optional*, returned when `use_cache=True` is passed or when `config.use_cache=True`):
             List of `Tuple(ms.Tensor, ms.Tensor` of length `config.n_layers`, with the first element
+
             being the previous *buckets* of shape `(batch_size, num_heads, num_hashes, sequence_length)`) and the
             second being the previous *hidden_states* of shape `(batch_size, sequence_length, hidden_size)`).
 
