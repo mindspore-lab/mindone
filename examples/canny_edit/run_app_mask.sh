203806f8315ef404aaf10b53546453c23537acc3
Didan Deng <33117903+wtomin@users.noreply.github.com>
2025-10-08 14:30:56 +0800

    sam2 checkpoint path

--- examples/canny_edit/run_app_mask.sh
+++ examples/canny_edit/run_app_mask.sh
@@ -2,5 +2,5 @@ export DEVICE_ID=0
 export no_proxy="localhost,127.0.0.1"
 
 python app_mask.py \
---sam_checkpoint ./checkpoints/sam2.1_hiera_large.pt \
---model_cfg ./configs/sam2.1/sam2.1_hiera_l.yaml
+--sam_checkpoint ../sam2/checkpoints/sam2.1_hiera_large.pt \
+--model_cfg ../sam2/configs/sam2.1/sam2.1_hiera_l.yaml
