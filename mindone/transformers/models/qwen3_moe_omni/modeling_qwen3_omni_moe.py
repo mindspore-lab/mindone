85e5ec8da1934c3b9b150516627c11750ba4455f
Chaoran Wei <77485245+wcrzlh@users.noreply.github.com>
2025-10-29 16:24:34 +0800

    fix hidden_states return bugs

--- mindone/transformers/models/qwen3_moe_omni/modeling_qwen3_omni_moe.py
+++ mindone/transformers/models/qwen3_moe_omni/modeling_qwen3_omni_moe.py
@@ -1641,6 +1641,7 @@ class Qwen3OmniMoeThinkerTextModel(Qwen3OmniMoePreTrainedModel):
         # args for deepstack
         visual_pos_masks: Optional[ms.Tensor] = None,
         deepstack_visual_embeds: Optional[list[ms.Tensor]] = None,
+        output_hidden_states: Optional[bool] = None,
         **kwargs: Unpack[FlashAttentionKwargs],
     ) -> Union[tuple, BaseModelOutputWithPast]:
         r"""
@@ -1689,6 +1690,7 @@ class Qwen3OmniMoeThinkerTextModel(Qwen3OmniMoePreTrainedModel):
 
         # create position embeddings to be shared across the decoder layers
         position_embeddings = self.rotary_emb(hidden_states, position_ids)
+        all_hidden_states = () if output_hidden_states else None
 
         # decoder layers
         for layer_idx, decoder_layer in enumerate(self.layers):
@@ -1713,9 +1715,14 @@ class Qwen3OmniMoeThinkerTextModel(Qwen3OmniMoePreTrainedModel):
 
         hidden_states = self.norm(hidden_states)
 
+        # add hidden states from the last decoder layer
+        if output_hidden_states:
+            all_hidden_states += (hidden_states,)
+
         return BaseModelOutputWithPast(
             last_hidden_state=hidden_states,
             past_key_values=past_key_values,
+            hidden_states=all_hidden_states,
         )
 
     def _deepstack_process(self, hidden_states, visual_pos_masks, visual_embeds):
