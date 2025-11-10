a690e96d061c371cb532ea44d7442fb9ce679eb9
Cui-yshoho <73014084+Cui-yshoho@users.noreply.github.com>
2025-11-10 15:30:33 +0800

    add kandinsky5 (#1388)

--- mindone/diffusers/pipelines/__init__.py
+++ mindone/diffusers/pipelines/__init__.py
@@ -137,6 +137,7 @@ _import_structure = {
         "Kandinsky3Img2ImgPipeline",
         "Kandinsky3Pipeline",
     ],
+    "kandinsky5": ["Kandinsky5T2VPipeline"],
     "kolors": [
         "KolorsPipeline",
         "KolorsImg2ImgPipeline",
@@ -384,6 +385,7 @@ if TYPE_CHECKING:
         KandinskyV22PriorPipeline,
     )
     from .kandinsky3 import Kandinsky3Img2ImgPipeline, Kandinsky3Pipeline
+    from .kandinsky5 import Kandinsky5T2VPipeline
     from .kolors import KolorsImg2ImgPipeline, KolorsPipeline
     from .latent_consistency_models import LatentConsistencyModelImg2ImgPipeline, LatentConsistencyModelPipeline
     from .latent_diffusion import LDMSuperResolutionPipeline, LDMTextToImagePipeline
