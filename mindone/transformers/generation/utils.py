11416a0e87aaa18605a5f3e994c4b14daf654251
Chaoran Wei <77485245+wcrzlh@users.noreply.github.com>
2025-11-13 14:36:57 +0800

    fix compile bug

--- mindone/transformers/generation/utils.py
+++ mindone/transformers/generation/utils.py
@@ -2389,9 +2389,7 @@ class GenerationMixin:
             return False
 
         # Base logic
-        valid_hardware = bool(
-            generation_config.compile_config is not None and generation_config.compile_config._compile_all_devices
-        )
+        valid_hardware = bool(generation_config.compile_config is not None)
         using_compilable_cache = (
             isinstance(model_kwargs.get("past_key_values"), Cache) and model_kwargs["past_key_values"].is_compileable
         )
