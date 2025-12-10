fef3fb30a176570ef03296e129aadf86392bff69
AndyZhou952 <jzhoubc@connect.ust.hk>
2025-12-10 17:37:13 +0800

    add back kwargs

--- mindone/transformers/models/janus/modeling_janus.py
+++ mindone/transformers/models/janus/modeling_janus.py
@@ -216,6 +216,7 @@ def eager_attention_forward(
     attention_mask: Optional[ms.Tensor],
     scaling: float,
     dropout: float = 0.0,
+    **kwargs: Unpack[TransformersKwargs],
 ):
     key_states = repeat_kv(key, module.num_key_value_groups)
     value_states = repeat_kv(value, module.num_key_value_groups)
