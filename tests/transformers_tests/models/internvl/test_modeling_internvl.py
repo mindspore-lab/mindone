f4d864f35c7308f673fce19b9ea8f8ef93148ad3
AndyZhou952 <jzhoubc@connect.ust.hk>
2025-12-11 14:57:19 +0800

    linting & refine test precision

--- tests/transformers_tests/models/internvl/test_modeling_internvl.py
+++ tests/transformers_tests/models/internvl/test_modeling_internvl.py
@@ -16,7 +16,7 @@ from tests.modeling_test_utils import (
 )
 from tests.transformers_tests.models.modeling_common import ids_numpy
 
-DTYPE_AND_THRESHOLDS = {"fp32": 5e-2, "fp16": 5e-2, "bf16": 5e-2}
+DTYPE_AND_THRESHOLDS = {"fp32": 5e-4, "fp16": 5e-2, "bf16": 8e-3}
 MODES = [1]
 
 
